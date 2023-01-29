d955531027c1bc0586c27b694c3014ca4b531f28
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-01-29 22:20:01 +0100

    rename

--- apps/slack/src/lib/no-ssr-wrapper.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from "next/dynamic";
-import React, { PropsWithChildren } from "react";
-
-const Wrapper = (props: PropsWithChildren<{}>) => <React.Fragment>{props.children}</React.Fragment>;
-
-export const NoSSRWrapper = dynamic(() => Promise.resolve(Wrapper), {
-  ssr: false,
-});
