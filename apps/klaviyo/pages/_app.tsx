998e10077db11ca1928da445034258fce83954ee
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-02-10 11:34:42 +0100

    Update colors in Klaviyo

--- apps/klaviyo/pages/_app.tsx
+++ apps/klaviyo/pages/_app.tsx
@@ -1,12 +1,36 @@
-import "../styles/globals.css";
+import "@saleor/apps-shared/src/globals.css";
 
 import { Theme } from "@material-ui/core/styles";
 import { AppBridge, AppBridgeProvider } from "@saleor/app-sdk/app-bridge";
-import { ThemeProvider as MacawUIThemeProvider } from "@saleor/macaw-ui";
+import { dark, light, SaleorThemeColors, ThemeProvider as MacawUIThemeProvider } from "@saleor/macaw-ui";
 import React, { PropsWithChildren, useEffect } from "react";
 
 import { ThemeSynchronizer } from "../hooks/theme-synchronizer";
 import { AppLayoutProps } from "../types";
+
+
+type PalettesOverride = Record<"light" | "dark", SaleorThemeColors>;
+
+/**
+ * Temporary override of colors, to match new dashboard palette.
+ * Long term this will be replaced with Macaw UI 2.x with up to date design tokens
+ */
+const palettes: PalettesOverride = {
+  light: {
+    ...light,
+    background: {
+      default: "#fff",
+      paper: "#fff",
+    },
+  },
+  dark: {
+    ...dark,
+    background: {
+      default: "hsla(211, 42%, 14%, 1)",
+      paper: "hsla(211, 42%, 14%, 1)",
+    },
+  },
+};
 
 const themeOverrides: Partial<Theme> = {
   overrides: {
@@ -31,7 +55,7 @@ const appBridgeInstance = typeof window !== "undefined" ? new AppBridge() : unde
 
 // That's a hack required by Macaw-UI incompatibility with React@18
 const ThemeProvider = MacawUIThemeProvider as React.FC<
-  PropsWithChildren<{ overrides: Partial<Theme>; ssr: boolean }>
+  PropsWithChildren<{ overrides?: Partial<Theme>; ssr: boolean; palettes: PalettesOverride }>
 >;
 
 function SaleorApp({ Component, pageProps }: AppLayoutProps) {
@@ -46,7 +70,7 @@ function SaleorApp({ Component, pageProps }: AppLayoutProps) {
 
   return (
     <AppBridgeProvider appBridgeInstance={appBridgeInstance}>
-      <ThemeProvider overrides={themeOverrides} ssr>
+      <ThemeProvider palettes={palettes} overrides={themeOverrides} ssr>
         <ThemeSynchronizer />
         {getLayout(<Component {...pageProps} />)}
       </ThemeProvider>
