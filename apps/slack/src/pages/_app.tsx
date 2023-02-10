3d32b216afd58b0ba3f9f83d2341c36d97d2bdf4
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-02-10 11:30:04 +0100

    Use external theme provider in slack

--- apps/slack/src/pages/_app.tsx
+++ apps/slack/src/pages/_app.tsx
@@ -3,12 +3,12 @@ import "../styles/globals.css";
 import { Theme } from "@material-ui/core/styles";
 import { AppBridge, AppBridgeProvider } from "@saleor/app-sdk/app-bridge";
 import { RoutePropagator } from "@saleor/app-sdk/app-bridge/next";
-import { ThemeProvider as MacawUIThemeProvider } from "@saleor/macaw-ui";
-import React, { PropsWithChildren, useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { AppLayoutProps } from "../../types";
 import { ThemeSynchronizer } from "../hooks/theme-synchronizer";
 import { NoSSRWrapper } from "../lib/no-ssr-wrapper";
+import { MacawThemeProvider } from "@saleor/apps-shared";
 
 const themeOverrides: Partial<Theme> = {
   overrides: {
@@ -33,11 +33,6 @@ const themeOverrides: Partial<Theme> = {
 const appBridgeInstance =
   typeof window !== "undefined" ? new AppBridge({ autoNotifyReady: false }) : undefined;
 
-// That's a hack required by Macaw-UI incompatibility with React@18
-const ThemeProvider = MacawUIThemeProvider as React.FC<
-  PropsWithChildren<{ overrides: Partial<Theme>; ssr: boolean; defaultTheme?: "light" | "dark" }>
->;
-
 function SaleorApp({ Component, pageProps }: AppLayoutProps) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
@@ -51,11 +46,11 @@ function SaleorApp({ Component, pageProps }: AppLayoutProps) {
   return (
     <NoSSRWrapper>
       <AppBridgeProvider appBridgeInstance={appBridgeInstance}>
-        <ThemeProvider overrides={themeOverrides} ssr>
+        <MacawThemeProvider themeOverrides={themeOverrides}>
           <ThemeSynchronizer />
           <RoutePropagator />
           {getLayout(<Component {...pageProps} />)}
-        </ThemeProvider>
+        </MacawThemeProvider>
       </AppBridgeProvider>
     </NoSSRWrapper>
   );
