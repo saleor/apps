1ccc393f7d71122d7052ebfccc4f2ec5bfa3abd8
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-02-10 11:47:12 +0100

    revert slack to use local theme provider

--- apps/slack/src/pages/_app.tsx
+++ apps/slack/src/pages/_app.tsx
@@ -3,12 +3,12 @@ import "../styles/globals.css";
 import { Theme } from "@material-ui/core/styles";
 import { AppBridge, AppBridgeProvider } from "@saleor/app-sdk/app-bridge";
 import { RoutePropagator } from "@saleor/app-sdk/app-bridge/next";
-import React, { useEffect } from "react";
+import { dark, light, SaleorThemeColors, ThemeProvider as MacawUIThemeProvider } from "@saleor/macaw-ui";
+import React, { PropsWithChildren, useEffect } from "react";
 
 import { AppLayoutProps } from "../../types";
 import { ThemeSynchronizer } from "../hooks/theme-synchronizer";
 import { NoSSRWrapper } from "../lib/no-ssr-wrapper";
-import { MacawThemeProvider } from "@saleor/apps-shared";
 
 const themeOverrides: Partial<Theme> = {
   overrides: {
@@ -33,6 +33,37 @@ const themeOverrides: Partial<Theme> = {
 const appBridgeInstance =
   typeof window !== "undefined" ? new AppBridge({ autoNotifyReady: false }) : undefined;
 
+type PalettesOverride = Record<"light" | "dark", SaleorThemeColors>;
+
+/**
+ * Temporary override of colors, to match new dashboard palette.
+ * Long term this will be replaced with Macaw UI 2.x with up to date design tokens
+ */
+const palettes: PalettesOverride = {
+  light: {
+    ...light,
+    background: {
+      default: "#fff",
+      paper: "#fff",
+    },
+  },
+  dark: {
+    ...dark,
+    background: {
+      default: "hsla(211, 42%, 14%, 1)",
+      paper: "hsla(211, 42%, 14%, 1)",
+    },
+  },
+};
+
+/**
+ * That's a hack required by Macaw-UI incompatibility with React@18
+ */
+const ThemeProvider = MacawUIThemeProvider as React.FC<
+  PropsWithChildren<{ overrides?: Partial<Theme>; ssr: boolean; palettes: PalettesOverride }>
+>;
+
+
 function SaleorApp({ Component, pageProps }: AppLayoutProps) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
@@ -46,11 +77,11 @@ function SaleorApp({ Component, pageProps }: AppLayoutProps) {
   return (
     <NoSSRWrapper>
       <AppBridgeProvider appBridgeInstance={appBridgeInstance}>
-        <MacawThemeProvider themeOverrides={themeOverrides}>
+        <ThemeProvider palettes={palettes} overrides={themeOverrides} ssr>
           <ThemeSynchronizer />
           <RoutePropagator />
           {getLayout(<Component {...pageProps} />)}
-        </MacawThemeProvider>
+        </ThemeProvider>
       </AppBridgeProvider>
     </NoSSRWrapper>
   );
