6656613e547c4936e8942e91faba232b45cdddfd
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-02-21 11:34:30 +0100

    Update UI to match new dashboard style (#138)
    * Extract macaw theme palette to shared package
    
    * Use MacawThemeProvider from shared package
    
    * Fix maca theme provider props
    
    * Update invoices app ui
    
    * Add noSSRwrapper to packages shared
    
    * Use external theme provider in data importer
    
    * Use external theme provider in slack
    
    * Improve margin in TitleBar
    
    * Update colors in Klaviyo
    
    * revert search to use local theme provider
    
    * revert slack to use local theme provider
    
    * Rewrite TitleBar to css modules
    
    * Add unique jss classes prefix for Klaviyo
    
    * Update apps/search/src/pages/_app.tsx
    
    Co-authored-by: Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
    
    * Update shy-gorillas-shop.md
    
    ---------
    
    Co-authored-by: Krzysztof Wolski <krzysztof.k.wolski@gmail.com>

--- apps/invoices/src/pages/_app.tsx
+++ apps/invoices/src/pages/_app.tsx
@@ -1,35 +1,20 @@
-import "../styles/globals.css";
-
-import { Theme } from "@material-ui/core/styles";
+import "@saleor/apps-shared/src/globals.css";
 import { AppBridge, AppBridgeProvider } from "@saleor/app-sdk/app-bridge";
 import { RoutePropagator } from "@saleor/app-sdk/app-bridge/next";
-import { ThemeProvider as MacawUIThemeProvider } from "@saleor/macaw-ui";
-import React, { PropsWithChildren, useEffect } from "react";
+import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 
 import GraphQLProvider from "../providers/GraphQLProvider";
 import { ThemeSynchronizer } from "../lib/theme-synchronizer";
 import { NoSSRWrapper } from "../lib/no-ssr-wrapper";
 import { trpcClient } from "../modules/trpc/trpc-client";
-
-const themeOverrides: Partial<Theme> = {
-  /**
-   * You can override MacawUI theme here
-   */
-};
+import { MacawThemeProvider } from "@saleor/apps-shared";
 
 /**
  * Ensure instance is a singleton.
  * TODO: This is React 18 issue, consider hiding this workaround inside app-sdk
  */
 export const appBridgeInstance = typeof window !== "undefined" ? new AppBridge() : undefined;
-
-/**
- * That's a hack required by Macaw-UI incompatibility with React@18
- */
-const ThemeProvider = MacawUIThemeProvider as React.FC<
-  PropsWithChildren<{ overrides?: Partial<Theme>; ssr: boolean }>
->;
 
 function NextApp({ Component, pageProps }: AppProps) {
   /**
@@ -46,11 +31,11 @@ function NextApp({ Component, pageProps }: AppProps) {
     <NoSSRWrapper>
       <AppBridgeProvider appBridgeInstance={appBridgeInstance}>
         <GraphQLProvider>
-          <ThemeProvider overrides={themeOverrides} ssr={false}>
+          <MacawThemeProvider>
             <ThemeSynchronizer />
             <RoutePropagator />
             <Component {...pageProps} />
-          </ThemeProvider>
+          </MacawThemeProvider>
         </GraphQLProvider>
       </AppBridgeProvider>
     </NoSSRWrapper>
