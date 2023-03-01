d35e28a22154620b2408792a853e76168c4dc858
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-03-01 17:52:59 +0100

    build: :arrow_up: next

--- apps/taxes/next.config.js
+++ apps/taxes/next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: true,
+  },
+  transpilePackages: ["nuvo-react", "@saleor/apps-shared"],
 };
