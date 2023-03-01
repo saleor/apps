3e612cad1ca4d58a0339c534181e025ab048413a
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-03-01 15:24:16 +0100

    Add Sentry to data-importer (#226)

---
+++ .changeset/spicy-scissors-report.md
@@ -0,0 +1,5 @@
+---
+"saleor-app-data-importer": minor
+---
+
+App now is configured with Sentry Wizard and will log to sentry if proper env variables are set
--- apps/data-importer/next.config.js
+++ apps/data-importer/next.config.js
@@ -1,3 +1,9 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -8,3 +14,5 @@ module.exports = {
   },
   transpilePackages: ["nuvo-react", "@saleor/apps-shared"],
 };
+
+module.exports = withSentryConfig(module.exports, { silent: true }, { hideSourcemaps: true });
