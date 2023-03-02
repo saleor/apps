c6feb173df266f3d4ca594c5137fc8b01bff7d27
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-03-02 09:07:50 +0100

    refactor: :recycle: align saleor-app.ts with the rest

--- apps/taxes/saleor-app.ts
+++ apps/taxes/saleor-app.ts
@@ -1,18 +1,19 @@
-import { APL, FileAPL, SaleorCloudAPL, UpstashAPL } from "@saleor/app-sdk/APL";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
+import { APL, FileAPL, SaleorCloudAPL, UpstashAPL } from "@saleor/app-sdk/APL";
 
-const aplType = process.env.APL ?? "file";
+/**
+ * By default auth data are stored in the `.auth-data.json` (FileAPL).
+ * For multi-tenant applications and deployments please use UpstashAPL.
+ *
+ * To read more about storing auth data, read the
+ * [APL documentation](https://github.com/saleor/saleor-app-sdk/blob/main/docs/apl.md)
+ */
 
-let apl: APL;
-
-switch (aplType) {
+export let apl: APL;
+switch (process.env.APL) {
   case "upstash":
+    // Require `UPSTASH_URL` and `UPSTASH_TOKEN` environment variables
     apl = new UpstashAPL();
-
-    break;
-  case "file":
-    apl = new FileAPL();
-
     break;
   case "saleor-cloud": {
     if (!process.env.REST_APL_ENDPOINT || !process.env.REST_APL_TOKEN) {
@@ -26,19 +27,9 @@ switch (aplType) {
 
     break;
   }
-  default: {
-    throw new Error("Invalid APL config, ");
-  }
+  default:
+    apl = new FileAPL();
 }
-
-if (!process.env.SECRET_KEY && process.env.NODE_ENV === "production") {
-  throw new Error(
-    "For production deployment SECRET_KEY is mandatory to use EncryptedSettingsManager."
-  );
-}
-
-// Use placeholder value for the development
-export const settingsManagerSecretKey = process.env.SECRET_KEY || "CHANGE_ME";
 
 export const saleorApp = new SaleorApp({
   apl,
