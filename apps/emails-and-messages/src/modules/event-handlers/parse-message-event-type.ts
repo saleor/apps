5a98658fd9ce819f7bc80f37de97a6908d196669
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-03-07 20:30:29 +0100

    Rename helper fn

--- apps/emails-and-messages/src/modules/event-handlers/parse-message-event-type.ts
+++ apps/emails-and-messages/src/modules/event-handlers/parse-message-event-type.ts
@@ -3,7 +3,10 @@ import { MessageEventTypes, messageEventTypes } from "./message-event-types";
 /**
  *  Returns the event type if it is valid, otherwise undefined.
  */
-export const checkMessageEventType = (eventType?: string): MessageEventTypes | undefined => {
+export const parseMessageEventType = (eventType?: string): MessageEventTypes | undefined => {
+  if (!eventType) {
+    return;
+  }
   if (messageEventTypes.includes(eventType as MessageEventTypes)) {
     return eventType as MessageEventTypes;
   }
