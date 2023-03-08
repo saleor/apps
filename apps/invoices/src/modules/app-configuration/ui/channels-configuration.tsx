9c066ad7b0fdfb25e595611ba054412023ee2417
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-03-08 19:19:18 +0100

    Remove unused Loader component

--- apps/invoices/src/modules/app-configuration/ui/channels-configuration.tsx
+++ apps/invoices/src/modules/app-configuration/ui/channels-configuration.tsx
@@ -7,7 +7,6 @@ import { AddressForm } from "./address-form";
 import { ChannelsList } from "./channels-list";
 import { actions, useAppBridge } from "@saleor/app-sdk/app-bridge";
 import { AppColumnsLayout } from "../../ui/app-columns-layout";
-import { Loader } from "../../ui/loader";
 
 const useStyles = makeStyles((theme) => {
   return {
