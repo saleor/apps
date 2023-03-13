2eab3fde9507bd820be566e92c2217b07b8306a1
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-03-13 20:46:53 +0100

    wip

--- apps/invoices/src/modules/invoice-generator/microinvoice/microinvoice-invoice-generator.test.ts
+++ apps/invoices/src/modules/invoice-generator/microinvoice/microinvoice-invoice-generator.test.ts
@@ -1,24 +1,24 @@
-import { describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, it, expect } from "vitest";
 import { MicroinvoiceInvoiceGenerator } from "./microinvoice-invoice-generator";
 import { readFile } from "fs/promises";
 import { join } from "path";
-// import rimraf from "rimraf";
+import rimraf from "rimraf";
 import { mockOrder } from "../../../fixtures/mock-order";
 import { getMockAddress } from "../../../fixtures/mock-address";
 
 const dirToSet = process.env.TEMP_PDF_STORAGE_DIR as string;
-const filePath = join(__dirname, dirToSet, "test-invoice.pdf");
+const filePath = join(process.cwd(), dirToSet, "test-invoice.pdf");
 
-// const cleanup = () => rimraf.sync(filePath);
+const cleanup = () => rimraf.sync(filePath);
 
 describe("MicroinvoiceInvoiceGenerator", () => {
-  // beforeEach(() => {
-  //   cleanup();
-  // });
-  //
-  // afterEach(() => {
-  //   cleanup();
-  // });
+  beforeEach(() => {
+    cleanup();
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
 
   it("Generates invoice file from Order", async () => {
     const instance = new MicroinvoiceInvoiceGenerator();
