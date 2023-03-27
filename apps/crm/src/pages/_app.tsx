c8ae765f97145ecf32078529b422687de8c9f8e3
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-03-27 16:32:43 +0200

    Allow iframe to NOT have app layout

--- apps/crm/src/pages/_app.tsx
+++ apps/crm/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 
 import { AppBridge, AppBridgeProvider } from "@saleor/app-sdk/app-bridge";
 import { RoutePropagator } from "@saleor/app-sdk/app-bridge/next";
-import React from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { AppProps } from "next/app";
 
 import { NoSSRWrapper } from "@saleor/apps-shared";
@@ -10,14 +10,31 @@ import { trpcClient } from "../modules/trpc/trpc-client";
 import { ThemeProvider } from "@saleor/macaw-ui/next";
 
 import "@saleor/macaw-ui/next/style";
+import { NextPage } from "next";
 
 /**
  * Ensure instance is a singleton.
  * TODO: This is React 18 issue, consider hiding this workaround inside app-sdk
  */
-export let appBridgeInstance = typeof window !== "undefined" ? new AppBridge() : undefined;
+export let appBridgeInstance: AppBridge | undefined;
 
-function NextApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+if (typeof window !== "undefined" && !appBridgeInstance) {
+  appBridgeInstance = new AppBridge();
+}
+
+export type NextPageWithLayoutOverwrite<P = {}, IP = P> = NextPage<P, IP> & {
+  overwriteLayout?: (page: ReactElement) => ReactElement;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayoutOverwrite;
+};
+
+function NextApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
+  if (Component.overwriteLayout) {
+    return Component.overwriteLayout(<Component {...pageProps} />);
+  }
+
   return (
     <NoSSRWrapper>
       <AppBridgeProvider appBridgeInstance={appBridgeInstance}>
--- apps/crm/src/pages/configuration/mailchimp/auth.tsx
+++ apps/crm/src/pages/configuration/mailchimp/auth.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from "next";
 import React from "react";
 import { MailchimpAuth } from "../../../modules/mailchimp/mailchimp-auth/mailchimp-auth";
+import { NextPageWithLayoutOverwrite } from "../../_app";
 
-const MailchimpAuthPage: NextPage = () => {
+const MailchimpAuthPage: NextPageWithLayoutOverwrite = () => {
   return <MailchimpAuth />;
 };
+
+MailchimpAuthPage.overwriteLayout = (page) => page;
 
 export default MailchimpAuthPage;
