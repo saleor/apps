24c4efacd0d35b977dd0781d481f9f81c6a0b562
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-04-17 20:27:13 +0200

    Reformat CRM app with eslint fix

--- apps/crm/src/pages/api/auth/mailchimp/callback.ts
+++ apps/crm/src/pages/api/auth/mailchimp/callback.ts
@@ -4,6 +4,7 @@ import { createLogger } from "../../../../lib/logger";
 
 export const getBaseUrl = (headers: { [name: string]: string | string[] | undefined }): string => {
   const { host, "x-forwarded-proto": protocol = "http" } = headers;
+
   return `${protocol}://${host}`;
 };
 
