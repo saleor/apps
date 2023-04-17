2eec8b1843077956ff122d39e616446c587005cc
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-04-17 20:38:15 +0200

    Apply eslint fix on monitoring codebase

--- apps/monitoring/src/pages/_app.tsx
+++ apps/monitoring/src/pages/_app.tsx
@@ -69,6 +69,7 @@ function NextApp({ Component, pageProps }: AppProps) {
    */
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
+
     if (jssStyles) {
       jssStyles?.parentElement?.removeChild(jssStyles);
     }
