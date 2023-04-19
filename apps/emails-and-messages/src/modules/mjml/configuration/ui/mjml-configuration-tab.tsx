5b1066f8ac198c7a425818a76f7eb16a56c19fba
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-04-19 18:13:33 +0200

    Remove channels config

--- apps/emails-and-messages/src/modules/mjml/configuration/ui/mjml-configuration-tab.tsx
+++ apps/emails-and-messages/src/modules/mjml/configuration/ui/mjml-configuration-tab.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IconButton, makeStyles } from "@saleor/macaw-ui";
+import { makeStyles } from "@saleor/macaw-ui";
 import { AppColumnsLayout } from "../../../ui/app-columns-layout";
 import { trpcClient } from "../../../trpc/trpc-client";
 import { MjmlConfigurationForm } from "./mjml-configuration-form";
@@ -7,11 +7,8 @@ import { getDefaultEmptyConfiguration } from "../mjml-config-container";
 import { NextRouter, useRouter } from "next/router";
 import { mjmlUrls } from "../../urls";
 import { MjmlTemplatesCard } from "./mjml-templates-card";
-import { SideMenu } from "../../../app-configuration/ui/side-menu";
 import { MjmlConfiguration } from "../mjml-config";
 import { LoadingIndicator } from "../../../ui/loading-indicator";
-import { Add } from "@material-ui/icons";
-import { useQueryClient } from "@tanstack/react-query";
 import { MjmlInstructions } from "./mjml-instructions";
 import { useDashboardNotification } from "@saleor/apps-shared";
 
@@ -42,6 +39,7 @@ const navigateToFirstConfiguration = (router: NextRouter, configurations?: MjmlC
     return;
   }
   const firstConfigurationId = configurations[0]?.id;
+
   if (firstConfigurationId) {
     router.replace(mjmlUrls.configuration(firstConfigurationId));
     return;
@@ -50,16 +48,13 @@ const navigateToFirstConfiguration = (router: NextRouter, configurations?: MjmlC
 
 export const MjmlConfigurationTab = ({ configurationId }: MjmlConfigurationTabProps) => {
   const styles = useStyles();
-  const { notifyError, notifySuccess } = useDashboardNotification();
   const router = useRouter();
-  const queryClient = useQueryClient();
 
   const {
     data: configurations,
     refetch: refetchConfigurations,
     isLoading: configurationsIsLoading,
     isFetching: configurationsIsFetching,
-    isRefetching: configurationsIsRefetching,
   } = trpcClient.mjmlConfiguration.getConfigurations.useQuery(undefined, {
     onSuccess(data) {
       if (!configurationId) {
@@ -68,38 +63,6 @@ export const MjmlConfigurationTab = ({ configurationId }: MjmlConfigurationTabPr
       }
     },
   });
-
-  const { mutate: deleteConfiguration } =
-    trpcClient.mjmlConfiguration.deleteConfiguration.useMutation({
-      onError: (error) => {
-        notifyError("Could not remove the configuration", error.message);
-      },
-      onSuccess: async (_data, variables) => {
-        await queryClient.cancelQueries({ queryKey: ["mjmlConfiguration", "getConfigurations"] });
-        // remove value from the cache after the success
-        queryClient.setQueryData<Array<MjmlConfiguration>>(
-          ["mjmlConfiguration", "getConfigurations"],
-          (old) => {
-            if (old) {
-              const index = old.findIndex((c) => c.id === variables.id);
-              if (index !== -1) {
-                delete old[index];
-                return [...old];
-              }
-            }
-          }
-        );
-
-        // if we just deleted the configuration that was selected
-        // we have to update the URL
-        if (variables.id === configurationId) {
-          router.replace(mjmlUrls.configuration());
-        }
-
-        refetchConfigurations();
-        notifySuccess("Success", "Removed successfully");
-      },
-    });
 
   if (configurationsIsLoading || configurationsIsFetching) {
     return <LoadingIndicator />;
@@ -113,25 +76,6 @@ export const MjmlConfigurationTab = ({ configurationId }: MjmlConfigurationTabPr
 
   return (
     <AppColumnsLayout>
-      <SideMenu
-        title="Configurations"
-        selectedItemId={configurationId}
-        headerToolbar={
-          <IconButton
-            variant="secondary"
-            onClick={() => {
-              router.replace(mjmlUrls.configuration());
-            }}
-          >
-            <Add />
-          </IconButton>
-        }
-        onClick={(id) => router.replace(mjmlUrls.configuration(id))}
-        onDelete={(id) => {
-          deleteConfiguration({ id });
-        }}
-        items={configurations?.map((c) => ({ label: c.configurationName, id: c.id })) || []}
-      />
       <div className={styles.configurationColumn}>
         {configurationsIsLoading || configurationsIsFetching ? (
           <LoadingIndicator />
--- apps/emails-and-messages/src/modules/sendgrid/configuration/ui/sendgrid-configuration-tab.tsx
+++ apps/emails-and-messages/src/modules/sendgrid/configuration/ui/sendgrid-configuration-tab.tsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { IconButton, makeStyles } from "@saleor/macaw-ui";
+import { makeStyles } from "@saleor/macaw-ui";
 import { AppColumnsLayout } from "../../../ui/app-columns-layout";
 import { trpcClient } from "../../../trpc/trpc-client";
 import { SendgridConfigurationForm } from "./sendgrid-configuration-form";
 import { getDefaultEmptyConfiguration } from "../sendgrid-config-container";
 import { NextRouter, useRouter } from "next/router";
-import { SideMenu } from "../../../app-configuration/ui/side-menu";
 import { SendgridConfiguration } from "../sendgrid-config";
 import { LoadingIndicator } from "../../../ui/loading-indicator";
-import { Add } from "@material-ui/icons";
-import { useQueryClient } from "@tanstack/react-query";
 import { sendgridUrls } from "../../urls";
 import { SendgridTemplatesCard } from "./sendgrid-templates-card";
 import { SendgridInstructions } from "./sendgrid-instructions";
-import { useDashboardNotification } from "@saleor/apps-shared";
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -45,6 +41,7 @@ const navigateToFirstConfiguration = (
     return;
   }
   const firstConfigurationId = configurations[0]?.id;
+
   if (firstConfigurationId) {
     router.replace(sendgridUrls.configuration(firstConfigurationId));
     return;
@@ -54,15 +51,12 @@ const navigateToFirstConfiguration = (
 export const SendgridConfigurationTab = ({ configurationId }: SendgridConfigurationTabProps) => {
   const styles = useStyles();
   const router = useRouter();
-  const queryClient = useQueryClient();
-  const { notifySuccess, notifyError } = useDashboardNotification();
 
   const {
     data: configurations,
     refetch: refetchConfigurations,
     isLoading: configurationsIsLoading,
     isFetching: configurationsIsFetching,
-    isRefetching: configurationsIsRefetching,
   } = trpcClient.sendgridConfiguration.getConfigurations.useQuery(undefined, {
     onSuccess(data) {
       if (!configurationId) {
@@ -71,41 +65,6 @@ export const SendgridConfigurationTab = ({ configurationId }: SendgridConfigurat
       }
     },
   });
-
-  const { mutate: deleteConfiguration } =
-    trpcClient.sendgridConfiguration.deleteConfiguration.useMutation({
-      onError: (error) => {
-        notifyError("Could not remove the configuration", error.message);
-      },
-      onSuccess: async (_data, variables) => {
-        await queryClient.cancelQueries({
-          queryKey: ["sendgridConfiguration", "getConfigurations"],
-        });
-        // remove value from the cache after the success
-        queryClient.setQueryData<Array<SendgridConfiguration>>(
-          ["sendgridConfiguration", "getConfigurations"],
-          (old) => {
-            if (old) {
-              const index = old.findIndex((c) => c.id === variables.id);
-              if (index !== -1) {
-                delete old[index];
-                return [...old];
-              }
-            }
-          }
-        );
-
-        // if we just deleted the configuration that was selected
-        // we have to update the URL
-        if (variables.id === configurationId) {
-          router.replace(sendgridUrls.configuration());
-        }
-
-        refetchConfigurations();
-
-        notifySuccess("Success", "Removed successfully");
-      },
-    });
 
   if (configurationsIsLoading || configurationsIsFetching) {
     return <LoadingIndicator />;
@@ -119,25 +78,6 @@ export const SendgridConfigurationTab = ({ configurationId }: SendgridConfigurat
 
   return (
     <AppColumnsLayout>
-      <SideMenu
-        title="Configurations"
-        selectedItemId={configurationId}
-        headerToolbar={
-          <IconButton
-            variant="secondary"
-            onClick={() => {
-              router.replace(sendgridUrls.configuration());
-            }}
-          >
-            <Add />
-          </IconButton>
-        }
-        onClick={(id) => router.replace(sendgridUrls.configuration(id))}
-        onDelete={(id) => {
-          deleteConfiguration({ id });
-        }}
-        items={configurations?.map((c) => ({ label: c.configurationName, id: c.id })) || []}
-      />
       <div className={styles.configurationColumn}>
         {configurationsIsLoading || configurationsIsFetching ? (
           <LoadingIndicator />
