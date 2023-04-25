f40ffacb113496c6ae0f6edc3f6b5f4265408743
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-04-25 15:12:55 +0200

    remove log

--- apps/crm/src/lib/metadata-manager.ts
+++ apps/crm/src/lib/metadata-manager.ts
@@ -40,8 +40,6 @@ export async function fetchAllMetadata(client: Pick<Client, "query">): Promise<M
     .query<FetchAppDetailsQuery>(FetchAppDetailsDocument, {})
     .toPromise();
 
-  console.log(error);
-
   if (error) {
     return [];
   }
