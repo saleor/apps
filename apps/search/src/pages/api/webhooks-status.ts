e97d956af7f58976eb342e71cbe9f0b1d961404d
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-04-27 11:21:54 +0200

    display webhooks status

---
+++ apps/search/src/pages/api/webhooks-status.ts
@@ -0,0 +1,30 @@
+import { createProtectedHandler } from "@saleor/app-sdk/handlers/next";
+import { saleorApp } from "../../../saleor-app";
+import { createClient } from "../../lib/graphql";
+import { FetchOwnWebhooksDocument } from "../../../generated/graphql";
+
+export default createProtectedHandler(
+  async (req, res, ctx) => {
+    const { authData } = ctx;
+    const client = createClient(authData.saleorApiUrl, async () => ({
+      token: authData.token,
+    }));
+
+    try {
+      const webhooks = await client
+        .query(FetchOwnWebhooksDocument, { id: authData.appId })
+        .toPromise()
+        .then((r) => r.data?.app?.webhooks);
+
+      if (!webhooks) {
+        return res.status(500).end();
+      }
+
+      return res.status(200).json(webhooks);
+    } catch (e) {
+      return res.status(500).end();
+    }
+  },
+  saleorApp.apl,
+  []
+);
--- apps/search/src/views/configuration/configuration.view.tsx
+++ apps/search/src/views/configuration/configuration.view.tsx
@@ -2,12 +2,77 @@ import { Box, Text } from "@saleor/macaw-ui/next";
 import { AppSection } from "../../components/AppSection";
 import { AlgoliaConfigurationForm } from "../../components/AlgoliaConfigurationForm";
 import { ImportProductsToAlgolia } from "../../components/ImportProductsToAlgolia";
-import { actions, useAppBridge } from "@saleor/app-sdk/app-bridge";
+import { actions, useAppBridge, useAuthenticatedFetch } from "@saleor/app-sdk/app-bridge";
+import { useEffect, useState } from "react";
+import { OwnWebhookFragment } from "../../../generated/graphql";
+import { Accordion, Chip } from "@saleor/macaw-ui/next";
 
 const SALEOR_EVENTS_DOCS_URL =
   "https://docs.saleor.io/docs/3.x/developer/extending/apps/asynchronous-webhooks#available-webhook-events";
 
 const ALGOLIA_DASHBOARD_TOKENS_URL = "https://www.algolia.com/account/api-keys/all";
+
+const WebhooksStatus = () => {
+  // todo why fetch type is "any" ??
+  const fetch: typeof window.fetch = useAuthenticatedFetch();
+  const [webhooksData, setWebhooksData] = useState<OwnWebhookFragment[] | null>(null);
+
+  useEffect(() => {
+    fetch("/api/webhooks-status")
+      .then((resp) => resp.json())
+      .then(setWebhooksData);
+  }, []);
+
+  console.log(webhooksData);
+
+  if (!webhooksData) {
+    return null;
+  }
+
+  return (
+    <Box>
+      <Accordion display={"grid"} gap={4}>
+        {webhooksData.map((webhook) => (
+          <Accordion.Item value={webhook.id}>
+            <Accordion.Item.Trigger>
+              <Box
+                width={"100%"}
+                paddingRight={8}
+                display={"grid"}
+                gridTemplateColumns={3}
+                gap={4}
+                alignItems={"center"}
+              >
+                <Text size={"small"}>{webhook.asyncEvents[0].name}</Text>
+                <Text color={"textNeutralSubdued"} variant={"caption"} size={"small"}>
+                  {webhook.asyncEvents[0].eventType}
+                </Text>
+                <Chip
+                  padding={2}
+                  marginLeft={"auto"}
+                  size={"small"}
+                  backgroundColor={
+                    webhook.isActive ? "decorativeSurfaceSubdued2" : "surfaceCriticalSubdued"
+                  }
+                >
+                  <Text
+                    color={webhook.isActive ? "text2Decorative" : "textCriticalSubdued"}
+                    textTransform={"uppercase"}
+                    margin={3}
+                    variant={"caption"}
+                  >
+                    {webhook.isActive ? "Active" : "Disabled"}
+                  </Text>
+                </Chip>
+              </Box>
+            </Accordion.Item.Trigger>
+            <Accordion.Item.Content>TODO Attempts + hide trigger if active</Accordion.Item.Content>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </Box>
+  );
+};
 
 export const ConfigurationView = () => {
   const { appBridge } = useAppBridge();
@@ -70,6 +135,18 @@ export const ConfigurationView = () => {
       </Box>
 
       <AppSection
+        includePadding
+        heading="Webhooks status"
+        sideContent={
+          <Text>
+            Check status of registered webhooks. If they fail, please revisit configuration
+          </Text>
+        }
+        mainContent={<WebhooksStatus />}
+      />
+
+      <AppSection
+        marginTop={13}
         heading="Algolia settings"
         mainContent={<AlgoliaConfigurationForm />}
         sideContent={
