6938b1da88334b252899120b764f875c41d6c229
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-04-27 09:34:33 +0200

    refactor: :recycle: replace material-ui with macaw-ui; remove all views

--- apps/taxes/src/pages/_app.tsx
+++ apps/taxes/src/pages/_app.tsx
@@ -1,63 +1,22 @@
 import "../styles/globals.css";
-
-import { Theme } from "@material-ui/core/styles";
+import "@saleor/macaw-ui/next/style";
 import { AppBridge, AppBridgeProvider } from "@saleor/app-sdk/app-bridge";
 import { RoutePropagator } from "@saleor/app-sdk/app-bridge/next";
-import {
-  dark,
-  light,
-  SaleorThemeColors,
-  ThemeProvider as MacawUIThemeProvider,
-} from "@saleor/macaw-ui";
-import React, { PropsWithChildren, useEffect } from "react";
-import { AppProps } from "next/app";
+import { ThemeProvider } from "@saleor/macaw-ui/next";
 
-import { GraphQLProvider } from "../providers/GraphQLProvider";
+import { AppProps } from "next/app";
+import { useEffect } from "react";
+
 import { ThemeSynchronizer } from "../lib/theme-synchronizer";
 import { trpcClient } from "../modules/trpc/trpc-client";
-import { NoSSRWrapper } from "../lib/no-ssr-wrapper";
-
-const themeOverrides: Partial<Theme> = {
-  /**
-   * You can override MacawUI theme here
-   */
-};
+import { GraphQLProvider } from "../providers/GraphQLProvider";
+import { NoSSRWrapper } from "@saleor/apps-shared";
 
 /**
  * Ensure instance is a singleton.
  * TODO: This is React 18 issue, consider hiding this workaround inside app-sdk
  */
 export const appBridgeInstance = typeof window !== "undefined" ? new AppBridge() : undefined;
-
-type PalettesOverride = Record<"light" | "dark", SaleorThemeColors>;
-
-/**
- * Temporary override of colors, to match new dashboard palette.
- * Long term this will be replaced with Macaw UI 2.x with up to date design tokens
- */
-const palettes: PalettesOverride = {
-  light: {
-    ...light,
-    background: {
-      default: "#fff",
-      paper: "#fff",
-    },
-  },
-  dark: {
-    ...dark,
-    background: {
-      default: "hsla(211, 42%, 14%, 1)",
-      paper: "hsla(211, 42%, 14%, 1)",
-    },
-  },
-};
-
-/**
- * That's a hack required by Macaw-UI incompatibility with React@18
- */
-const ThemeProvider = MacawUIThemeProvider as React.FC<
-  PropsWithChildren<{ overrides?: Partial<Theme>; ssr: boolean; palettes: PalettesOverride }>
->;
 
 function NextApp({ Component, pageProps }: AppProps) {
   /**
@@ -75,7 +34,7 @@ function NextApp({ Component, pageProps }: AppProps) {
     <NoSSRWrapper>
       <AppBridgeProvider appBridgeInstance={appBridgeInstance}>
         <GraphQLProvider>
-          <ThemeProvider palettes={palettes} overrides={themeOverrides} ssr>
+          <ThemeProvider>
             <ThemeSynchronizer />
             <RoutePropagator />
             <Component {...pageProps} />
