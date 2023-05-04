35787b05a43b8b6492eb5b7c717c5f03d95d92e7
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-05-04 08:54:54 +0200

    Replace Search logger with shared one

--- apps/search/src/lib/logger.ts
+++ apps/search/src/lib/logger.ts
@@ -1,17 +1,10 @@
-import pino from "pino";
+import { createLogger as _createLogger } from "@saleor/apps-shared";
 
-export const logger = pino({
-  level: "debug",
+/**
+ * Extend factory to add more settings specific for the app
+ */
+export const logger = _createLogger({
   redact: ["token", "secretKey"],
-  transport:
-    process.env.NODE_ENV === "development"
-      ? {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-          },
-        }
-      : undefined,
 });
 
 export const createLogger = logger.child.bind(logger);
