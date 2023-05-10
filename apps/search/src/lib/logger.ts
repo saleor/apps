8b22b1c1f83d40e276d03285044830f753c16e1e
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-05-10 16:57:20 +0200

    Fix logs (#452)
    * Add pino and pino-pretty to every package, to fix logger failing
    
    * Fix taxes child logger
    
    * Fix Search child logger
    
    * Update examples .env
    
    * changeset

--- apps/search/src/lib/logger.ts
+++ apps/search/src/lib/logger.ts
@@ -3,8 +3,11 @@ import { createLogger as _createLogger } from "@saleor/apps-shared";
 /**
  * Extend factory to add more settings specific for the app
  */
-export const logger = _createLogger({
-  redact: ["token", "secretKey"],
-});
+export const logger = _createLogger(
+  {},
+  {
+    redact: ["token", "secretKey"],
+  }
+);
 
 export const createLogger = logger.child.bind(logger);
