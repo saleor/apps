f9b776f85d4d69d122b4c0d79d92647a728527b4
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-05-11 14:49:06 +0200

    feat: :alembic: add logging to channels-fetcher

--- apps/taxes/src/modules/channels/channels-fetcher.ts
+++ apps/taxes/src/modules/channels/channels-fetcher.ts
@@ -3,22 +3,31 @@ import {
   TaxConfigurationsListDocument,
   TaxConfigurationsListQueryVariables,
 } from "../../../generated/graphql";
+import { createLogger } from "../../lib/logger";
 
 export class ChannelsFetcher {
   constructor(private client: Client) {}
 
   fetchChannels() {
-    return this.client
+    const logger = createLogger({ service: "ChannelsFetcher" });
+
+    logger.fatal("fetchChannels called");
+
+    const response = this.client
       .query(TaxConfigurationsListDocument, {
         first: 10,
       } as TaxConfigurationsListQueryVariables)
       .toPromise()
       .then((r) => {
+        logger.fatal({ response: r.data }, "raw fetchChannels response");
         return (
           r.data?.taxConfigurations?.edges
             .filter(({ node }) => node.taxCalculationStrategy === "TAX_APP")
             .map(({ node }) => node.channel) ?? []
         );
       });
+
+    logger.fatal({ response }, "filtered fetchChannels response");
+    return response;
   }
 }
