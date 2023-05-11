93735fae8f12b25dc933881e41cf0d8614e51587
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-05-11 11:31:18 +0200

    wip

---
+++ packages/metadata-migration/src/graphql-client.ts
@@ -0,0 +1,43 @@
+import { AuthConfig, authExchange } from "@urql/exchange-auth";
+import { cacheExchange, createClient as urqlCreateClient, dedupExchange } from "urql";
+
+
+interface IAuthState {
+  token: string;
+}
+
+export const createClient = (url: string, getAuth: AuthConfig<IAuthState>["getAuth"]) =>
+  urqlCreateClient({
+    url,
+    exchanges: [
+      dedupExchange,
+      cacheExchange,
+      authExchange<IAuthState>({
+        addAuthToOperation: ({ authState, operation }) => {
+          if (!authState || !authState?.token) {
+            return operation;
+          }
+
+          const fetchOptions =
+            typeof operation.context.fetchOptions === "function"
+              ? operation.context.fetchOptions()
+              : operation.context.fetchOptions || {};
+
+          return {
+            ...operation,
+            context: {
+              ...operation.context,
+              fetchOptions: {
+                ...fetchOptions,
+                headers: {
+                  ...fetchOptions.headers,
+                  "Authorization-Bearer": authState.token,
+                },
+              },
+            },
+          };
+        },
+        getAuth,
+      }),
+    ],
+  });
--- packages/metadata-migration/src/index.ts
+++ packages/metadata-migration/src/index.ts
@@ -1,10 +1,54 @@
-import { EncryptedMetadataManager } from "@saleor/app-sdk/settings-manager";
+/* eslint-disable turbo/no-undeclared-env-vars */
+
+import { createSettingsManager } from "./metadata-manager";
+import { createClient } from "./graphql-client";
+import { SaleorCloudAPL } from "@saleor/app-sdk/APL";
+
+const getMetadataManagerForEnv = (apiUrl: string, appToken: string) => {
+  const client = createClient(apiUrl, async () => ({
+    token: appToken,
+  }));
+
+  return createSettingsManager(client);
+};
+
+const safeParse = (json?: string) => {
+  if (!json) return null;
+
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return null;
+  }
+};
 
 const run = async () => {
-  const manager = new EncryptedMetadataManager({});
-  const result = await manager.getMetadata();
+  console.log(process.env.SALEOR_CLOUD_TOKEN);
 
-  console.log(result);
+  const saleorAPL = new SaleorCloudAPL({
+    token: process.env.SALEOR_CLOUD_TOKEN!,
+    resourceUrl: process.env.SALEOR_CLOUD_RESOURCE_URL!,
+  });
+
+  const allEnvs = await saleorAPL.getAll();
+
+  const results = await Promise.all(
+    allEnvs.map((env) => {
+      const metadataManager = getMetadataManagerForEnv(env.saleorApiUrl, env.token);
+
+      return Promise.all([
+        metadataManager.get("app-config"),
+        metadataManager.get("app-config-v2"),
+      ]).then(([v1, v2]) => {
+        return {
+          schemaV1: safeParse(v1),
+          schemaV2: safeParse(v2),
+        };
+      });
+    })
+  );
+
+  console.log(results);
 };
 
 run();
