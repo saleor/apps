b0eb078eea9595a7f1b354f13f9e3e6d5f8484ba
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-05-11 12:21:12 +0200

    Fix broken build in invoices

--- apps/invoices/src/modules/invoices/invoice-generator/microinvoice/microinvoice-invoice-generator.ts
+++ apps/invoices/src/modules/invoices/invoice-generator/microinvoice/microinvoice-invoice-generator.ts
@@ -1,6 +1,7 @@
 import { InvoiceGenerator } from "../invoice-generator";
 import { Order, OrderPayloadFragment } from "../../../../../generated/graphql";
 import { SellerShopConfig } from "../../../app-configuration/schema-v1/app-config-v1";
+import { AddressV2Shape } from "../../../app-configuration/schema-v2/app-config-schema.v2";
 const Microinvoice = require("microinvoice");
 
 export class MicroinvoiceInvoiceGenerator implements InvoiceGenerator {
@@ -13,7 +14,7 @@ export class MicroinvoiceInvoiceGenerator implements InvoiceGenerator {
     order: OrderPayloadFragment;
     invoiceNumber: string;
     filename: string;
-    companyAddressData: SellerShopConfig["address"];
+    companyAddressData: AddressV2Shape;
   }): Promise<void> {
     const { invoiceNumber, order, companyAddressData, filename } = input;
 
