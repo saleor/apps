d7f9da33845c58e1993f8f1a6b2dc2621f65e00f
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-05-16 12:37:52 +0200

    Add TextLink

---
+++ packages/ui/src/text-link.tsx
@@ -0,0 +1,73 @@
+import { actions, useAppBridge } from "@saleor/app-sdk/app-bridge";
+import { Text, TextProps } from "@saleor/macaw-ui/next";
+import { useRouter } from "next/router";
+
+interface TextLinkProps extends TextProps {
+  href: string;
+  newTab?: boolean;
+}
+
+const BaseTextLink = (props: TextLinkProps) => {
+  return (
+    <Text
+      target="_blank"
+      as={"a"}
+      textDecoration={"underline"}
+      variant={"bodyStrong"}
+      rel="noopener noreferrer"
+      {...props}
+    >
+      {props.children}
+    </Text>
+  );
+};
+
+export const TextLink = ({ href, newTab = false, children, ...props }: TextLinkProps) => {
+  const { appBridge } = useAppBridge();
+  const { push } = useRouter();
+
+  const onExternalClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+
+    if (!appBridge) {
+      console.warn(
+        "App bridge is not initialized, TextLink cannot be used with external links without it."
+      );
+    }
+
+    appBridge?.dispatch(
+      actions.Redirect({
+        to: href,
+        newContext: true,
+      })
+    );
+
+    if (props.onClick) {
+      props.onClick(event);
+    }
+  };
+
+  const onInternalClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+
+    push(href);
+
+    if (props.onClick) {
+      props.onClick(event);
+    }
+  };
+
+  if (newTab) {
+    return (
+      <BaseTextLink href={href} onClick={onExternalClick} {...props}>
+        {children}
+      </BaseTextLink>
+    );
+  } else {
+    return (
+      <BaseTextLink href={href} onClick={onInternalClick} {...props}>
+        {children}
+      </BaseTextLink>
+    );
+  }
+};
