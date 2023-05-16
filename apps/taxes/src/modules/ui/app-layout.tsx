e56ba31ba7b13aae1da7afbbf7d953cc5341f50a
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-05-16 11:51:01 +0200

    feat: :alembic: move taxes app-grid to packages/ui

--- apps/taxes/src/modules/ui/app-layout.tsx
+++ apps/taxes/src/modules/ui/app-layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppContainer } from "./app-container";
-import { AppGrid } from "./app-grid";
+import { AppGrid } from "@saleor/apps-ui";
 
 export const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
