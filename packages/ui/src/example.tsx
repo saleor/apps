7aab665a24c44c21144078686aff2b31834f84d8
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-05-16 14:56:16 +0200

    refactor: :fire: remove usage in taxes

--- packages/ui/src/example.tsx
+++
@@ -1,8 +0,0 @@
-import { Box } from "@saleor/macaw-ui/next";
-import { PropsWithChildren } from "react";
-
-type ExampleProps = PropsWithChildren<{}>;
-
-export const Example = ({ children }: ExampleProps) => {
-  return <Box>{children}</Box>;
-};
