adaa3b7b34b534595bff3e3af1239388731181bd
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-05-18 14:00:41 +0200

    Clean up sentry boilderplate

--- apps/products-feed/next.config.js
+++ apps/products-feed/next.config.js
@@ -24,8 +24,8 @@ module.exports = withSentryConfig(
     // Suppresses source map uploading logs during build
     silent: true,
 
-    org: "saleor",
-    project: "product-feed",
+    org: process.env.SENTRY_ORG,
+    project: process.env.SENTRY_PROJECT,
   },
   {
     disableServerWebpackPlugin: !isSentryEnvAvailable,
