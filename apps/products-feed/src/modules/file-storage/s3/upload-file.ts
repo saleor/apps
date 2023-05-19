15cebd0c32d70e2ae22b05cf0ccdfbf2ab0189bc
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-05-19 12:13:48 +0200

    Explain sze limit on multipart upload

--- apps/products-feed/src/modules/file-storage/s3/upload-file.ts
+++ apps/products-feed/src/modules/file-storage/s3/upload-file.ts
@@ -9,6 +9,11 @@ export interface UploadFileArgs {
   bucketName: string;
 }
 
+/*
+ * AWS multipart uploads require a minimum file size of 5 MB.
+ * https://docs.aws.amazon.com/AmazonS3/latest/userguide/qfacts.html
+ */
+
 const MULTI_PART_SIZE_THRESHOLD = 5 * 1024 * 1024;
 
 export const uploadFile = async (args: UploadFileArgs) => {
