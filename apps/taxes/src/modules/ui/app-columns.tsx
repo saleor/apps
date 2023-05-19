b8a5adbefd3b7602add39749f6c137322e2da244
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-05-19 09:47:39 +0200

    refactor: :recycle: add gridRatio to support two app grids

--- apps/taxes/src/modules/ui/app-columns.tsx
+++ apps/taxes/src/modules/ui/app-columns.tsx
@@ -1,19 +1,30 @@
 import { Box } from "@saleor/macaw-ui/next";
 import React from "react";
 
+const ratioMap = {
+  "1/1": "1fr 1fr",
+  "1/2": "1fr 2fr",
+};
+
+type Ratio = keyof typeof ratioMap;
+
+type AppColumnsProps = {
+  top: React.ReactNode;
+  bottomLeft: React.ReactNode;
+  bottomRight: React.ReactNode;
+  gridRatio?: Ratio;
+};
+
 export const AppColumns = ({
   top,
   bottomLeft,
   bottomRight,
-}: {
-  top: React.ReactNode;
-  bottomLeft: React.ReactNode;
-  bottomRight: React.ReactNode;
-}) => {
+  gridRatio = "1/1",
+}: AppColumnsProps) => {
   return (
     <Box display={"grid"} __gap={"60px"}>
       <Box>{top}</Box>
-      <Box display={"grid"} gridTemplateColumns={2} gap={10}>
+      <Box display={"grid"} gap={10} __gridTemplateColumns={ratioMap[gridRatio]}>
         <Box>{bottomLeft}</Box>
         <Box>{bottomRight}</Box>
       </Box>
