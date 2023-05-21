a4f00915d19af87bff03019ce55870d83ff67056
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-05-21 18:54:41 +0200

    Remove duplicated code

--- apps/products-feed/src/modules/file-storage/s3/upload-file.ts
+++ apps/products-feed/src/modules/file-storage/s3/upload-file.ts
@@ -1,6 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import { UploadMultiPart } from "./upload-multi-part";
 import { UploadSinglePart } from "./upload-single-part";
+import { MULTI_PART_SIZE_THRESHOLD } from "./const";
 
 export interface UploadFileArgs {
   s3Client: S3Client;
@@ -8,13 +9,6 @@ export interface UploadFileArgs {
   buffer: Buffer;
   bucketName: string;
 }
-
-/*
- * AWS multipart uploads require a minimum file size of 5 MB.
- * https://docs.aws.amazon.com/AmazonS3/latest/userguide/qfacts.html
- */
-
-const MULTI_PART_SIZE_THRESHOLD = 5 * 1024 * 1024;
 
 export const uploadFile = async (args: UploadFileArgs) => {
   if (args.buffer.length > MULTI_PART_SIZE_THRESHOLD) {
