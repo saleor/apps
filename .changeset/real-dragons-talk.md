35a543830c9866cbaa189314466af6ab94b0fa35
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-05-22 18:07:21 +0200

    Add bindings for Select component

---
+++ .changeset/real-dragons-talk.md
@@ -0,0 +1,5 @@
+---
+"@saleor/react-hook-form-macaw": minor
+---
+
+Add Select input component
---
+++ packages/react-hook-form-macaw/src/components/Select.tsx
@@ -0,0 +1,36 @@
+import { Select as $Select, type SelectProps as $SelectProps } from "@saleor/macaw-ui/next";
+import { Control, Controller, FieldPath, FieldValues } from "react-hook-form";
+
+export type SelectProps<T extends FieldValues = FieldValues> = Omit<$SelectProps, "name"> & {
+  name: FieldPath<T>;
+  control: Control<T>;
+};
+
+export function Select<TFieldValues extends FieldValues = FieldValues>({
+  type,
+  required,
+  name,
+  control,
+  options,
+  ...rest
+}: SelectProps<TFieldValues>): JSX.Element {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { value, ...field }, fieldState: { error } }) => (
+        <$Select
+          {...rest}
+          {...field}
+          options={options}
+          value={value || ""}
+          name={name}
+          required={required}
+          type={type}
+          error={!!error}
+          helperText={rest.helperText}
+        />
+      )}
+    />
+  );
+}
