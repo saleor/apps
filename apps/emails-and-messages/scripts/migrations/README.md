94d7a096e3b3b728820cb6d67de63a1bca7084c2
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-06-05 14:49:58 +0200

    V2 migration scripts

---
+++ apps/emails-and-messages/scripts/migrations/README.md
@@ -0,0 +1,7 @@
+# Metadata migration scripts
+
+To run dry-run (check migration without mutating the data):
+`npx tsx scripts/migrations/run-report.ts`
+
+To update date and save it:
+`npx tsx scripts/migrations/run-migration.ts`
---
+++ apps/emails-and-messages/scripts/migrations/migration-utils.ts
@@ -0,0 +1,30 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
+
+import { createClient } from "../../src/lib/create-graphql-client";
+import { SaleorCloudAPL } from "@saleor/app-sdk/APL";
+import { createSettingsManager } from "../../src/lib/metadata-manager";
+
+export const getMetadataManagerForEnv = (apiUrl: string, appToken: string, appId: string) => {
+  const client = createClient(apiUrl, async () => ({
+    token: appToken,
+  }));
+
+  return createSettingsManager(client, appId);
+};
+
+export const verifyRequiredEnvs = () => {
+  const requiredEnvs = ["SALEOR_CLOUD_TOKEN", "SALEOR_CLOUD_RESOURCE_URL", "SECRET_KEY"];
+
+  if (!requiredEnvs.every((env) => process.env[env])) {
+    throw new Error(`Missing envs: ${requiredEnvs.join(" | ")}`);
+  }
+};
+
+export const fetchCloudAplEnvs = () => {
+  const saleorAPL = new SaleorCloudAPL({
+    token: process.env.SALEOR_CLOUD_TOKEN!,
+    resourceUrl: process.env.SALEOR_CLOUD_RESOURCE_URL!,
+  });
+
+  return saleorAPL.getAll();
+};
