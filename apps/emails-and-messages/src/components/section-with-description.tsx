71049870786a753535321fad6b48becdabf4e453
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-06-06 20:06:16 +0200

    More whitespace between description and box in the sections

--- apps/emails-and-messages/src/components/section-with-description.tsx
+++ apps/emails-and-messages/src/components/section-with-description.tsx
@@ -7,7 +7,7 @@ interface SectionWithDescriptionProps {
 }
 export const SectionWithDescription = (props: SectionWithDescriptionProps) => {
   return (
-    <Box display="grid" gridTemplateColumns={{ desktop: 3, mobile: 1 }} gap={3}>
+    <Box display="grid" gridTemplateColumns={{ desktop: 3, mobile: 1 }} gap={12}>
       <Box>
         <Text variant="heading" as="h2" paddingBottom={2}>
           {props.title}
