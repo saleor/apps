a8847000066322986e5c1777165cca4a70d33ef8
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-06-07 10:19:33 +0200

    test: :white_check_mark: add tests for channel-configuration-merge

---
+++ apps/taxes/src/modules/channel-configuration/channel-configuration-merger.ts
@@ -0,0 +1,18 @@
+import { ChannelFragment } from "../../../generated/graphql";
+import { ChannelsConfig } from "./channel-config";
+
+export class ChannelConfigurationMerger {
+  merge(channels: ChannelFragment[], channelsConfig: ChannelsConfig): ChannelsConfig {
+    return channels.map((channel) => {
+      const channelConfig = channelsConfig.find((c) => c.config.slug === channel.slug);
+
+      return {
+        id: channel.id,
+        config: {
+          providerInstanceId: channelConfig?.config.providerInstanceId ?? null,
+          slug: channel.slug,
+        },
+      };
+    });
+  }
+}
--- apps/taxes/src/modules/channel-configuration/channel-configuration.service.ts
+++ apps/taxes/src/modules/channel-configuration/channel-configuration.service.ts
@@ -1,8 +1,8 @@
 import { Client } from "urql";
-import { ChannelFragment } from "../../../generated/graphql";
-import { ChannelConfigProperties, ChannelsConfig } from "./channel-config";
+import { ChannelConfigProperties } from "./channel-config";
 import { ChannelConfigurationSettings } from "./channel-configuration-settings";
 import { ChannelsFetcher } from "./channel-fetcher";
+import { ChannelConfigurationMerger } from "./channel-configuration-merger";
 
 export class ChannelConfigurationService {
   private configurationService: ChannelConfigurationSettings;
@@ -10,29 +10,15 @@ export class ChannelConfigurationService {
     this.configurationService = new ChannelConfigurationSettings(this.client, this.saleorApiUrl);
   }
 
-  private mergeChannelsWithConfiguration(
-    channels: ChannelFragment[],
-    channelsConfig: ChannelsConfig
-  ): ChannelsConfig {
-    return channels.map((channel) => {
-      const channelConfig = channelsConfig.find((c) => c.config.slug === channel.slug);
-
-      return {
-        id: channel.id,
-        config: {
-          providerInstanceId: channelConfig?.config.providerInstanceId ?? null,
-          slug: channel.slug,
-        },
-      };
-    });
-  }
   async getAll() {
     const channelsFetcher = new ChannelsFetcher(this.client);
     const channels = await channelsFetcher.fetchChannels();
 
     const channelConfiguration = await this.configurationService.getAll();
 
-    return this.mergeChannelsWithConfiguration(channels, channelConfiguration);
+    const configurationMerger = new ChannelConfigurationMerger();
+
+    return configurationMerger.merge(channels, channelConfiguration);
   }
 
   async update(id: string, data: ChannelConfigProperties) {
