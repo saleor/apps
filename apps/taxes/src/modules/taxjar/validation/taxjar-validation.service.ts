6bb13b0a668b8f1a5a903d3508f6dae7a95d1336
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-06-07 09:56:50 +0200

    feat: :white_check_mark: extract taxjar-validation-error-resolver and add tests

--- apps/taxes/src/modules/taxjar/validation/taxjar-validation.service.ts
+++ apps/taxes/src/modules/taxjar/validation/taxjar-validation.service.ts
@@ -1,39 +1,21 @@
-import { TaxjarError } from "taxjar/dist/util/types";
 import { taxJarAddressFactory } from "../address-factory";
 import { TaxJarClient } from "../taxjar-client";
 import { TaxJarConfig } from "../taxjar-config";
+import { TaxJarValidationErrorResolver } from "./tax-jar-validation-error-resolver";
 
 export class TaxJarValidationService {
-  constructor() {}
-  private resolveTaxJarValidationError(error: unknown): Error {
-    if (!(error instanceof TaxjarError)) {
-      return new Error("Unknown error while validating TaxJar configuration.");
-    }
-
-    if (error.status === 401) {
-      return new Error(
-        "The provided API token is invalid. Please visit https://support.taxjar.com/article/160-how-do-i-get-a-sales-tax-api-token for more information."
-      );
-    }
-
-    if (error.status === 404) {
-      return new Error(
-        "The provided address is invalid. Please visit https://support.taxjar.com/article/659-address-validation to learn about address formatting."
-      );
-    }
-
-    return new Error(error.detail);
-  }
-
   async validate(config: TaxJarConfig): Promise<void> {
     const taxJarClient = new TaxJarClient(config);
 
     const address = taxJarAddressFactory.fromChannelToParams(config.address);
 
     try {
+      // if the address is invalid, TaxJar will throw an error (rather than 200 and error messages)
       await taxJarClient.validateAddress({ params: address });
     } catch (error) {
-      throw this.resolveTaxJarValidationError(error);
+      const errorResolver = new TaxJarValidationErrorResolver();
+
+      throw errorResolver.resolve(error);
     }
   }
 }
