5e39da87d3c461fd6ec05dc61d67bf466c0cdab0
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-06-14 16:14:27 +0200

    Install eslint rule that checks forbidden logger cal

--- packages/eslint-config-saleor/index.js
+++ packages/eslint-config-saleor/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo", "prettier", "plugin:@saleor/saleor-app/recommended"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
