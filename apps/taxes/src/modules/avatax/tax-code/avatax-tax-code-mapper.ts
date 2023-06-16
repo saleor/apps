c2e1a2a445e78978ab92f95b5ba488b68673b454
Adrian Pilarczyk <admin@peelar.dev>
2023-06-16 09:12:12 +0200

    Update avatax-tax-code-mapper.ts

--- apps/taxes/src/modules/avatax/tax-code/avatax-tax-code-mapper.ts
+++ apps/taxes/src/modules/avatax/tax-code/avatax-tax-code-mapper.ts
@@ -2,13 +2,6 @@ import { createId } from "../../../lib/utils";
 import { TaxCode } from "../../tax-codes/tax-code-match-schema";
 import { AvataxTaxCodeMatches } from "./avatax-tax-code-match-repository";
 
-/**
- * Maps the tax codes from Avatax to the tax code matches.
- * @constructor
- * @param {string} avataxTaxCodes - The tax codes from Avatax.
- * @param {string} savedMatches - The tax code matches that were already saved in the system.
- * @returns {AvataxTaxCodeMatches} The Avatax tax codes mapped to the tax code matches (with/without match).
- */
 export class AvataxTaxCodeMapper {
   map(avataxTaxCodes: TaxCode[], savedMatches: AvataxTaxCodeMatches): AvataxTaxCodeMatches {
     const mappedTaxCodes = avataxTaxCodes.map((taxCode) => {
