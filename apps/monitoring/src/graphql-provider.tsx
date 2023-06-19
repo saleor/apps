d77839ab0828ecc17bead6ba8da2f7ff0b632133
Krzysztof Wolski <krzysztof.wolski@saleor.io>
2023-06-19 14:42:20 +0200

    Update monitoring

--- apps/monitoring/src/graphql-provider.tsx
+++ apps/monitoring/src/graphql-provider.tsx
@@ -1,16 +1,17 @@
 import { useAppBridge } from "@saleor/app-sdk/app-bridge";
+import { createGraphQLClient } from "@saleor/apps-shared";
 import { PropsWithChildren } from "react";
 import { Provider } from "urql";
-import { createClient } from "./lib/create-graphq-client";
 
 export function GraphQLProvider(props: PropsWithChildren<{}>) {
   const { appBridgeState } = useAppBridge();
+  const saleorApiUrl = appBridgeState?.saleorApiUrl!;
+  const token = appBridgeState?.token!;
 
-  const client = createClient(
-    `/graphql`,
-    async () => Promise.resolve({ token: appBridgeState?.token! }),
-    () => appBridgeState?.saleorApiUrl!
-  );
+  const client = createGraphQLClient({
+    saleorApiUrl,
+    token,
+  });
 
   return <Provider value={client} {...props} />;
 }
