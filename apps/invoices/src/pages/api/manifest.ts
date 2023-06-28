04cf93ca2248d51578afdd0fada65b6205f8fba1
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-06-28 15:36:07 +0200

    Add development envs

--- apps/invoices/src/pages/api/manifest.ts
+++ apps/invoices/src/pages/api/manifest.ts
@@ -7,19 +7,27 @@ import { REQUIRED_SALEOR_VERSION } from "../../saleor-app";
 
 export default createManifestHandler({
   async manifestFactory(context) {
+    const iframeBaseUrl = process.env.APP_IFRAME_BASE_URL ?? context.appBaseUrl;
+    const apiBaseURL = process.env.APP_API_BASE_URL ?? context.appBaseUrl;
+
     const manifest: AppManifest = {
       name: "Invoices",
-      tokenTargetUrl: `${context.appBaseUrl}/api/register`,
-      appUrl: context.appBaseUrl,
+      tokenTargetUrl: `${apiBaseURL}/api/register`,
+      appUrl: iframeBaseUrl,
       permissions: ["MANAGE_ORDERS"],
       id: "saleor.app.invoices",
       version: packageJson.version,
-      webhooks: [invoiceRequestedWebhook.getWebhookManifest(context.appBaseUrl)],
+      webhooks: [invoiceRequestedWebhook.getWebhookManifest(apiBaseURL)],
       extensions: [],
       supportUrl: "https://github.com/saleor/apps/discussions",
       homepageUrl: "https://github.com/saleor/apps",
       dataPrivacyUrl: "https://saleor.io/legal/privacy/",
       author: "Saleor Commerce",
+      brand: {
+        logo: {
+          default: `${apiBaseURL}/logo.png`,
+        },
+      },
       /**
        * Requires 3.10 due to invoices event payload - in previous versions, order reference was missing
        */
