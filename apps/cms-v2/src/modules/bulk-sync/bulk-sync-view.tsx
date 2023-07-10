7e6641a360c2ff2b501dc7a82a417c4fa785cdca
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-07-10 13:48:44 +0200

    new frontend for uploading

--- apps/cms-v2/src/modules/bulk-sync/bulk-sync-view.tsx
+++ apps/cms-v2/src/modules/bulk-sync/bulk-sync-view.tsx
@@ -1,97 +1,12 @@
 import { Box, Button, Text } from "@saleor/macaw-ui/next";
 import { useEffect, useState } from "react";
-import {
-  AnyProviderConfigSchemaType,
-  ChannelProviderConnectionType,
-  RootConfigSchemaType,
-} from "../configuration";
+import { AnyProviderConfigSchemaType, ChannelProviderConnectionType } from "../configuration";
 import { ContentfulClient } from "../contentful/contentful-client";
 import { contentfulRateLimiter } from "../contentful/contentful-rate-limiter";
-import { trpcClient } from "../trpc/trpc-client";
 import { AppSection } from "../ui/app-section";
 import { useBulkSyncProductsState } from "./use-bulk-sync-products-state";
 import { useFetchAllProducts } from "./use-fetch-all-products";
 import { VariantsSyncStatusList } from "./variants-sync-status-list";
-
-const Results = (props: {
-  channelSlug: string;
-  providerConfig: RootConfigSchemaType["providers"][0];
-}) => {
-  const [started, setStarted] = useState(false);
-
-  const { products, finished } = useFetchAllProducts(started, props.channelSlug);
-
-  const { productsStatusList, setInitialProducts, setItemStatus } = useBulkSyncProductsState();
-
-  useEffect(() => {
-    if (!finished) {
-      return;
-    }
-
-    setInitialProducts(products);
-  }, [products, finished, setInitialProducts]);
-
-  useEffect(() => {
-    if (!finished) {
-      return;
-    }
-
-    // todo make abstraction
-
-    const contentful = new ContentfulClient({
-      accessToken: props.providerConfig.authToken,
-      space: props.providerConfig.spaceId,
-    });
-
-    const promises = products.flatMap((product) => {
-      return product.variants?.map((variant) => {
-        return contentfulRateLimiter(() => {
-          setItemStatus(variant.id, "uploading");
-
-          return contentful
-            .upsertProduct({
-              configuration: props.providerConfig,
-              variant: {
-                id: variant.id,
-                name: variant.name,
-                channelListings: variant.channelListings,
-                product: {
-                  id: product.id,
-                  name: product.name,
-                  slug: product.slug,
-                },
-              },
-            })
-            .then((r) => {
-              if (r?.metadata) {
-                setItemStatus(variant.id, "success");
-              }
-            })
-            .catch((e) => {
-              console.error(e);
-
-              setItemStatus(variant.id, "error");
-            });
-        });
-      });
-    });
-
-    Promise.all(promises).then(() => {
-      console.log("all");
-    });
-  }, [finished, products]);
-
-  return (
-    <Box>
-      {!started && (
-        <Box display="flex" justifyContent="flex-end">
-          <Button onClick={() => setStarted(true)}>Prefetch products</Button>
-        </Box>
-      )}
-      {productsStatusList && <VariantsSyncStatusList marginTop={8} variants={productsStatusList} />}
-    </Box>
-  );
-};
 
 const FetchProductsStep = (props: { onButtonClick(): void }) => {
   return (
@@ -158,6 +73,56 @@ export const BulkSyncView = ({
     setInitialProducts(products);
     setState("fetched");
   }, [products, saleorProductsFetchFinished, setInitialProducts]);
+
+  useEffect(() => {
+    if (state !== "uploading") {
+      return;
+    }
+
+    // todo make abstraction
+
+    const contentful = new ContentfulClient({
+      accessToken: configuration.authToken,
+      space: configuration.spaceId,
+    });
+
+    const promises = products.flatMap((product) => {
+      return product.variants?.map((variant) => {
+        return contentfulRateLimiter(() => {
+          setItemStatus(variant.id, "uploading");
+
+          return contentful
+            .upsertProduct({
+              configuration,
+              variant: {
+                id: variant.id,
+                name: variant.name,
+                channelListings: variant.channelListings,
+                product: {
+                  id: product.id,
+                  name: product.name,
+                  slug: product.slug,
+                },
+              },
+            })
+            .then((r) => {
+              if (r?.metadata) {
+                setItemStatus(variant.id, "success");
+              }
+            })
+            .catch((e) => {
+              console.error(e);
+
+              setItemStatus(variant.id, "error");
+            });
+        });
+      });
+    });
+
+    Promise.all(promises).then(() => {
+      console.log("all");
+    });
+  }, [state, products]);
 
   return (
     <Box>
