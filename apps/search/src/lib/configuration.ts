126588927032928741811232e19f0cf2139c444d
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2023-07-11 18:51:41 +0200

    Update react query

--- apps/search/src/lib/configuration.ts
+++ apps/search/src/lib/configuration.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useAuthenticatedFetch } from "@saleor/app-sdk/app-bridge";
 import { AppConfigurationFields } from "../domain/configuration";
 
