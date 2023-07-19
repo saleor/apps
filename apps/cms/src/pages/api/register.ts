d04adc9b49b12941b8e22762a91c41d303d16ce7
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-07-19 11:35:15 +0200

    remove CMS v1 app codebase

--- apps/cms/src/pages/api/register.ts
+++
@@ -1,9 +0,0 @@
-import { createAppRegisterHandler } from "@saleor/app-sdk/handlers/next";
-
-import { saleorApp } from "../../../saleor-app";
-
-/**
- * Required endpoint, called by Saleor to install app.
- * It will exchange tokens with app, so saleorApp.apl will contain token
- */
-export default createAppRegisterHandler(saleorApp);
