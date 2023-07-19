5b2976306e773ea8a03d2e52789262d2e673140b
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-07-19 14:01:17 +0200

    remove CMS v1 app codebase (#789)

--- apps/cms/src/lib/cms/client/index.ts
+++
@@ -1,3 +0,0 @@
-export { createCmsOperations } from "./clients-operations";
-export { executeCmsOperations } from "./clients-execution";
-export { updateMetadata, batchUpdateMetadata } from "./metadata-execution";
--- apps/cms/src/modules/channels/ui/types.ts
+++
@@ -1,7 +0,0 @@
-import { ProductsVariantsSyncLoading } from "../../cms/hooks/useProductsVariantsSync";
-import { ChannelsDataLoading } from "./hooks/useChannels";
-
-export interface ChannelsLoading {
-  channels: ChannelsDataLoading;
-  productsVariantsSync: ProductsVariantsSyncLoading;
-}
