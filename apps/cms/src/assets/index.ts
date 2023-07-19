5618417f2ad6125785577d509f01f9d633f8c1d4
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-07-19 14:01:17 +0200

    remove CMS v1 app codebase (#789)

--- apps/cms/src/assets/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ContentfulIcon } from "./contentful-icon.svg";
-export { default as StrapiIcon } from "./strapi-icon.svg";
-export { default as DatocmsIcon } from "./datocms-icon.svg";
