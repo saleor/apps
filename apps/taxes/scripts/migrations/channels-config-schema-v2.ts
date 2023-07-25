a7fbae4c6a71348c722c92cb19f18efbdcc55731
Adrian Pilarczyk <admin@peelar.dev>
2023-07-25 12:30:16 +0200

    refactor: :fire: migration code (#806)

--- apps/taxes/scripts/migrations/channels-config-schema-v2.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-import { channelsSchema } from "../../src/modules/channel-configuration/channel-config";
-
-export const channelsV2Schema = channelsSchema;
-
-export type ChannelsV2 = z.infer<typeof channelsV2Schema>;
--- apps/taxes/scripts/migrations/tax-providers-config-schema-v2.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-import { providerConnectionsSchema } from "../../src/modules/provider-connections/provider-connections";
-
-const taxProvidersV2Schema = providerConnectionsSchema;
-
-export type TaxProvidersV2 = z.infer<typeof taxProvidersV2Schema>;
