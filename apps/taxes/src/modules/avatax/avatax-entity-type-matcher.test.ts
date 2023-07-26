4cbb097d31eab705243b00f63d372c34e126e738
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-07-26 11:56:54 +0200

    test: :white_check_mark: add tests for entity-type-matcher

---
+++ apps/taxes/src/modules/avatax/avatax-entity-type-matcher.test.ts
@@ -0,0 +1,55 @@
+import { AvataxClient } from "./avatax-client";
+import { AvataxEntityTypeMatcher } from "./avatax-entity-type-matcher";
+import { describe, expect, it, vi } from "vitest";
+
+const mockGetEntityUseCode = vi.fn();
+
+const mockedMetadata = [
+  {
+    key: "avataxEntityCode",
+    value: "entityCode",
+  },
+  {
+    key: "somethingElse",
+    value: "otherTestValue",
+  },
+];
+
+describe("AvataxEntityTypeMatcher", () => {
+  it("returns empty string when entity code is not present in metadata", async () => {
+    const mockAvataxClient = {
+      getEntityUseCode: mockGetEntityUseCode.mockReturnValue(
+        Promise.resolve({ value: [{ code: "entityCode" }] })
+      ),
+    } as any as AvataxClient;
+
+    const matcher = new AvataxEntityTypeMatcher({ client: mockAvataxClient });
+    const result = await matcher.match([]);
+
+    expect(result).toBe("");
+  });
+  it("returns empty string when entity code is present in metadata but not in avatax", async () => {
+    const mockAvataxClient = {
+      getEntityUseCode: mockGetEntityUseCode.mockReturnValue(Promise.resolve({})),
+    } as any as AvataxClient;
+
+    const matcher = new AvataxEntityTypeMatcher({ client: mockAvataxClient });
+
+    const result = await matcher.match(mockedMetadata);
+
+    expect(result).toBe("");
+  });
+  it("returns entity code when entity code is present in metadata and in avatax", async () => {
+    const mockAvataxClient = {
+      getEntityUseCode: mockGetEntityUseCode.mockReturnValue(
+        Promise.resolve({ value: [{ code: "entityCode" }] })
+      ),
+    } as any as AvataxClient;
+
+    const matcher = new AvataxEntityTypeMatcher({ client: mockAvataxClient });
+
+    const result = await matcher.match(mockedMetadata);
+
+    expect(result).toBe("entityCode");
+  });
+});
