3335d1f4210745cd2a68af437e4278fc7274c40c
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-07-27 17:16:13 +0200

    feat: :construction: add basic boilerplate

--- apps/taxes/src/modules/taxes/tax-provider-webhook.ts
+++ apps/taxes/src/modules/taxes/tax-provider-webhook.ts
@@ -1,10 +1,10 @@
 import { SyncWebhookResponsesMap } from "@saleor/app-sdk/handlers/next";
 import {
+  OrderCancelledEventSubscriptionFragment,
   OrderCreatedSubscriptionFragment,
   OrderFulfilledSubscriptionFragment,
   TaxBaseFragment,
 } from "../../../generated/graphql";
-import { ChannelConfig } from "../channel-configuration/channel-config";
 
 export type CalculateTaxesResponse = SyncWebhookResponsesMap["ORDER_CALCULATE_TAXES"];
 
@@ -14,4 +14,5 @@ export interface ProviderWebhookService {
   calculateTaxes: (payload: TaxBaseFragment) => Promise<CalculateTaxesResponse>;
   createOrder: (payload: OrderCreatedSubscriptionFragment) => Promise<CreateOrderResponse>;
   fulfillOrder: (payload: OrderFulfilledSubscriptionFragment) => Promise<{ ok: boolean }>;
+  cancelOrder: (payload: OrderCancelledEventSubscriptionFragment) => Promise<any>;
 }
