1a41ca7684a00ba61fbc0cc441a1f300dc569ac8
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-07-27 17:10:38 +0200

    refactor: :recycle: extract order-metadata-manager

---
+++ apps/taxes/src/modules/app/order-metadata-manager.ts
@@ -0,0 +1,38 @@
+import {
+  UpdateMetadataDocument,
+  UpdateMetadataMutation,
+  UpdateMetadataMutationVariables,
+} from "../../../generated/graphql";
+import { Client } from "urql";
+import { PROVIDER_ORDER_ID_KEY } from "../avatax/order-fulfilled/avatax-order-fulfilled-payload-transformer";
+
+export class OrderMetadataManager {
+  private privateOrderIdKey = PROVIDER_ORDER_ID_KEY;
+
+  constructor(private client: Client) {}
+
+  /**
+   * We need to store the provider order id in the Saleor order metadata so that we can
+   * update the provider order when the Saleor order is fulfilled.
+   */
+  async updateOrderMetadataWithExternalId(orderId: string, externalId: string) {
+    const variables: UpdateMetadataMutationVariables = {
+      id: orderId,
+      input: [
+        {
+          key: this.privateOrderIdKey,
+          value: externalId,
+        },
+      ],
+    };
+    const { error } = await this.client
+      .mutation<UpdateMetadataMutation>(UpdateMetadataDocument, variables)
+      .toPromise();
+
+    if (error) {
+      throw error;
+    }
+
+    return { ok: true };
+  }
+}
