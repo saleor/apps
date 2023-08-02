293407e9f0b9769f7b151c63d87071c70fd177a6
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-08-02 11:47:24 +0200

    fix: :bug: use "DEFAULT" value of companyCode for commit to work

--- apps/taxes/src/pages/api/webhooks/order-fulfilled.ts
+++ apps/taxes/src/pages/api/webhooks/order-fulfilled.ts
@@ -38,18 +38,18 @@ export default orderFulfilledAsyncWebhook.createHandler(async (req, res, ctx) =>
     const channelSlug = payload.order?.channel.slug;
     const taxProvider = getActiveConnectionService(channelSlug, appMetadata, ctx.authData);
 
-    logger.info("Fetched taxProvider");
-
     // todo: figure out what fields are needed and add validation
     if (!payload.order) {
       return webhookResponse.error(new Error("Insufficient order data"));
     }
+    logger.info("Fulfilling order...");
+
     await taxProvider.fulfillOrder(payload.order);
 
     logger.info("Order fulfilled");
 
     return webhookResponse.success();
   } catch (error) {
-    return webhookResponse.error(new Error("Error while fulfilling tax provider order"));
+    return webhookResponse.error(error);
   }
 });
