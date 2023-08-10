016bb9f075c6dc3caf07edf5659779d9da240229
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-08-10 10:11:25 +0200

    schema configuratioj

--- apps/segment/src/modules/configuration/app-config.ts
+++ apps/segment/src/modules/configuration/app-config.ts
@@ -1,23 +1,8 @@
-import { ProvidersResolver } from "../providers/providers-resolver";
-import { generateId } from "../shared/generate-id";
-import { ChannelProviderConnectionConfig } from "./schemas/channel-provider-connection.schema";
-import { ProvidersConfig, RootConfig } from "./schemas/root-config.schema";
+import { z } from "zod";
+import { RootConfig } from "./schemas/root-config.schema";
 
-export const Errors = {
-  CONNECTION_ALREADY_EXISTS: "CONNECTION_ALREADY_EXISTS",
-  PROVIDER_DOESNT_EXIST: "PROVIDER_DOESNT_EXIST",
-};
-
-/**
- * TODO
- * - test
- * - extract and delegate smaller configs?
- */
 export class AppConfig {
-  private rootData: RootConfig.Shape = {
-    providers: [],
-    connections: [],
-  };
+  private rootData: RootConfig.Shape = null;
 
   constructor(initialData?: RootConfig.Shape) {
     if (initialData) {
@@ -33,116 +18,17 @@ export class AppConfig {
     return JSON.stringify(this.rootData);
   }
 
-  providers = {
-    checkProviderExists: (id: string) => {
-      return !!this.rootData.providers.find((p) => p.id === id);
-    },
+  setSegmentWriteKey(key: string) {
+    const parsedKey = z.string().min(1).parse(key);
 
-    addProvider: (providerConfigInput: ProvidersConfig.AnyInputShape) => {
-      const inputSchema = ProvidersResolver.getProviderInputSchema(providerConfigInput.type);
+    if (this.rootData) {
+      this.rootData.segmentWriteKey = parsedKey;
+    } else {
+      this.rootData = {
+        segmentWriteKey: parsedKey,
+      };
+    }
 
-      const parsedConfig = inputSchema.parse(providerConfigInput);
-
-      this.rootData.providers.push({
-        ...parsedConfig,
-        id: generateId(),
-      });
-
-      return this;
-    },
-
-    updateProvider: (providerConfig: ProvidersConfig.AnyFullShape) => {
-      const schema = ProvidersResolver.getProviderSchema(providerConfig.type);
-
-      const parsedConfig = schema.parse(providerConfig);
-
-      this.rootData.providers = this.rootData.providers.map((p) => {
-        if (p.id === parsedConfig.id) {
-          return parsedConfig;
-        } else {
-          return p;
-        }
-      });
-    },
-
-    deleteProvider: (id: string) => {
-      this.rootData.providers = this.rootData.providers.filter((p) => p.id !== id);
-      this.connections.deleteConnectionsWithProvider(id);
-
-      return this;
-    },
-
-    getProviders: () => {
-      return this.rootData.providers;
-    },
-
-    getProviderById: (id: string) => {
-      return this.providers.getProviders().find((p) => p.id === id);
-    },
-  };
-
-  connections = {
-    getConnections: () => {
-      return this.rootData.connections;
-    },
-
-    deleteConnection: (connectionID: string) => {
-      this.rootData.connections = this.rootData.connections.filter((c) => c.id !== connectionID);
-
-      return this;
-    },
-
-    addConnection: (input: ChannelProviderConnectionConfig.InputShape) => {
-      if (!this.providers.checkProviderExists(input.providerId)) {
-        const err = new Error("Provider doesnt exist");
-
-        err.cause = Errors.PROVIDER_DOESNT_EXIST;
-
-        throw err;
-      }
-
-      const parsed = ChannelProviderConnectionConfig.Schema.Input.parse(input);
-
-      if (this.checkConnectionExists(input)) {
-        const error = new Error("Connection already exists");
-
-        error.cause = Errors.CONNECTION_ALREADY_EXISTS;
-
-        throw error;
-      }
-
-      this.rootData.connections.push({
-        ...parsed,
-        id: generateId(),
-      });
-
-      return this;
-    },
-
-    deleteConnectionsWithProvider: (providerId: string) => {
-      this.rootData.connections = this.rootData.connections.filter((conn) => {
-        return conn.providerId !== providerId;
-      });
-    },
-
-    getConnectionById: (id: string) => {
-      return this.connections.getConnections().find((c) => c.id === id);
-    },
-  };
-
-  /**
-   * Returns true if exists
-   */
-  private checkConnectionExists(newConnection: ChannelProviderConnectionConfig.InputShape) {
-    /**
-     * Make array of strings so its easy to compare
-     */
-    const existingConnectionsAsStrings = this.connections
-      .getConnections()
-      .map((connection) => `${connection.channelSlug}-${connection.providerId}`);
-
-    const newConnectionAsString = `${newConnection.channelSlug}-${newConnection.providerId}`;
-
-    return existingConnectionsAsStrings.includes(newConnectionAsString);
+    return this;
   }
 }
--- apps/segment/src/modules/configuration/schemas/root-config.schema.ts
+++ apps/segment/src/modules/configuration/schemas/root-config.schema.ts
@@ -1,35 +1,4 @@
 import { z } from "zod";
-import { ChannelProviderConnectionConfig } from "./channel-provider-connection.schema";
-import { ContentfulProviderConfig } from "./contentful-provider.schema";
-import { BuilderIoProviderConfig } from "./builder-provider.schema";
-import { StrapiProviderConfig } from "./strapi-provider.schema";
-import { DatocmsProviderConfig } from "./datocms-provider.schema";
-
-export namespace ProvidersConfig {
-  const AnyFull = z.union([
-    /**
-     * Add more for each provider
-     */
-    ContentfulProviderConfig.Schema.Full,
-    DatocmsProviderConfig.Schema.Full,
-    StrapiProviderConfig.Schema.Full,
-    BuilderIoProviderConfig.Schema.Full,
-  ]);
-
-  export const Schema = {
-    AnyFull: AnyFull,
-    AnyInput: z.union([
-      ContentfulProviderConfig.Schema.Input,
-      DatocmsProviderConfig.Schema.Input,
-      StrapiProviderConfig.Schema.Input,
-      BuilderIoProviderConfig.Schema.Input,
-    ]),
-    AnyFullList: z.array(AnyFull),
-  };
-
-  export type AnyFullShape = z.infer<typeof Schema.AnyFull>;
-  export type AnyInputShape = z.infer<typeof Schema.AnyInput>;
-}
 
 export namespace RootConfig {
   /**
@@ -37,10 +6,11 @@ export namespace RootConfig {
    * - Only one request
    * - Always transactional
    */
-  export const Schema = z.object({
-    providers: ProvidersConfig.Schema.AnyFullList,
-    connections: z.array(ChannelProviderConnectionConfig.Schema.Full),
-  });
+  export const Schema = z
+    .object({
+      segmentWriteKey: z.string(),
+    })
+    .nullable();
 
   export type Shape = z.infer<typeof Schema>;
 }
