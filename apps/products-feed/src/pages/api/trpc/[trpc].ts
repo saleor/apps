5acaa8a20fffafac70937ee17175e314a5ae3dd7
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2023-08-10 18:09:32 +0200

    Improve logging on tRPC routes

--- apps/products-feed/src/pages/api/trpc/[trpc].ts
+++ apps/products-feed/src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,18 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { createTrpcContext } from "../../../modules/trpc/trpc-context";
 import { appRouter } from "../../../modules/trpc/trpc-app-router";
+import { createLogger } from "@saleor/apps-shared";
+
+const logger = createLogger({ name: "tRPC error" });
 
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext: createTrpcContext,
+  onError: ({ path, error }) => {
+    if (error.code === "INTERNAL_SERVER_ERROR") {
+      logger.error(error, `${path} returned error:`);
+      return;
+    }
+    logger.debug(error, `${path} returned error:`);
+  },
 });
