7d7f61d75b9e5dec1815221418fa259fbd2887ce
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-08-17 11:08:34 +0200

    Extract metadata manager creation to factory from shared

--- apps/search/src/test/handlers/webhooks-status-handler.test.ts
+++ apps/search/src/test/handlers/webhooks-status-handler.test.ts
@@ -30,6 +30,7 @@ const appWebhooksResponseData: Pick<OperationResult<FetchOwnWebhooksQuery, any>,
     app: {
       webhooks: [
         {
+          name: "W1",
           id: "w1",
           isActive: true,
           asyncEvents: [
@@ -53,6 +54,7 @@ describe("webhooksStatusHandler", () => {
   const webhooksTogglerServiceMock: IWebhookActivityTogglerService = {
     disableOwnWebhooks: vi.fn(),
     enableOwnWebhooks: vi.fn(),
+    recreateOwnWebhooks: vi.fn(),
   };
 
   const algoliaSearchProviderMock: Pick<SearchProvider, "ping"> = {
