e279f281751227748f3c8c85d13ab3078f9f2460
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2023-10-17 12:10:43 +0200

    refactor: :recycle: refactor AvataxCustomerCodeResolver so it falls back to dummy value

--- apps/taxes/src/modules/avatax/avatax-customer-code-resolver.ts
+++ apps/taxes/src/modules/avatax/avatax-customer-code-resolver.ts
@@ -1,32 +1,18 @@
 import { OrderConfirmedSubscriptionFragment } from "../../../generated/graphql";
-import { taxProviderUtils } from "../taxes/tax-provider-utils";
-import { TaxBadPayloadError } from "../taxes/tax-error";
 import { CalculateTaxesPayload } from "../../pages/api/webhooks/checkout-calculate-taxes";
 
 export class AvataxCustomerCodeResolver {
+  /*
+   * "If you're creating a sales order for a customer that does not yet exist in your system, you can send a dummyCustomerCode as sales orders are not recorded."
+   * https://developer.avalara.com/ecommerce-integration-guide/sales-tax-badge/transactions/simple-transactions/
+   */
+  private fallbackCustomerCode = "0";
+
   resolveOrderCustomerCode(order: OrderConfirmedSubscriptionFragment) {
-    return taxProviderUtils.resolveStringOrThrow(
-      order.user?.id,
-      new TaxBadPayloadError("Cannot resolve user id"),
-    );
+    return order.user?.id ?? this.fallbackCustomerCode;
   }
 
-  // During the checkout process, it appears the customer id is not always available. We can use the email address instead.
   resolveCalculateTaxesCustomerCode(payload: CalculateTaxesPayload): string {
-    if (payload.taxBase.sourceObject.__typename === "Checkout") {
-      return taxProviderUtils.resolveStringOrThrow(
-        payload.taxBase.sourceObject.email,
-        new TaxBadPayloadError("Cannot resolve email from sourceObject"),
-      );
-    }
-
-    if (payload.taxBase.sourceObject.__typename === "Order") {
-      return taxProviderUtils.resolveStringOrThrow(
-        payload.taxBase.sourceObject.userEmail,
-        new TaxBadPayloadError("Cannot resolve userEmail from sourceObject"),
-      );
-    }
-
-    throw new TaxBadPayloadError("Cannot resolve customer code");
+    return payload.taxBase.sourceObject.user?.id ?? this.fallbackCustomerCode;
   }
 }
