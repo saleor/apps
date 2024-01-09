0802b88b73b290a6fcf0cc4e333a07acb9911fb5
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-01-09 09:46:13 +0100

    refactors

--- packages/otel/src/otel-wrapper.ts
+++ packages/otel/src/otel-wrapper.ts
@@ -9,6 +9,7 @@ import { otelSdk } from "./instrumentation";
 import { loggerProvider, otelLogsProcessor } from "./otel-logs-setup";
 import { batchSpanProcessor } from "./otel-traces-setup";
 import { sharedOtelConfig } from "./shared-config";
+import { getAttributesFromRequest } from "./get-attributes-from-request";
 
 const tracer = getOtelTracer();
 
@@ -21,6 +22,20 @@ if (process.env.OTEL_ENABLED && process.env.OTEL_SERVICE_NAME) {
 }
 
 const OTEL_FLUSH_TIMEOUT = sharedOtelConfig.flushTimeout;
+
+const flushOtel = async () => {
+  await race({
+    promise: loggerProvider.forceFlush(),
+    error: new Error("Timeout flushing OTEL logs"),
+    timeout: OTEL_FLUSH_TIMEOUT,
+  });
+
+  await race({
+    promise: Promise.all([batchSpanProcessor.forceFlush(), otelLogsProcessor.forceFlush()]),
+    error: new Error("Timeout flushing OTEL spans"),
+    timeout: OTEL_FLUSH_TIMEOUT,
+  });
+};
 
 export const withOtel = (handler: NextApiHandler, staticRouteName: string): NextApiHandler => {
   if (!process.env.OTEL_ENABLED) {
@@ -35,92 +50,40 @@ export const withOtel = (handler: NextApiHandler, staticRouteName: string): Next
     ) => {
       const [req, res] = args;
 
-      if (!req) {
-        // winstonLogger.warn("No request object found, OTEL is not set-up");
+      if (!req || !res) {
+        // winstonLogger.warn("No request and/or response objects found, OTEL is not set-up");
 
         // @ts-expect-error runtime check
         return wrappingTarget.apply(thisArg, args);
       }
 
-      if (!res) {
-        // winstonLogger.warn("No response object found, OTEL is not set-up");
-
-        // @ts-expect-error runtime check
-        return wrappingTarget.apply(thisArg, args);
-      }
-
-      const vercelRequestId = getVercelRequestId(req);
-      const reqMethod = (req.method || "GET").toUpperCase();
-      const reqId = req.headers["x-vercel-id"] as string | undefined;
-      const saleorApiUrl = req.headers[SALEOR_API_URL_HEADER] as string | undefined;
+      const attributesFromRequest = getAttributesFromRequest(req);
 
       return tracer.startActiveSpan(
-        `${reqMethod} ${staticRouteName}`,
+        `${attributesFromRequest[SemanticAttributes.HTTP_METHOD]} ${staticRouteName}`,
         {
           kind: SpanKind.SERVER,
+          attributes: attributesFromRequest,
         },
         async (span) => {
-          if (vercelRequestId) {
-            span.setAttribute(SemanticAttributes.FAAS_EXECUTION, vercelRequestId);
-          }
-
-          if (req.headers["user-agent"]) {
-            span.setAttribute(SemanticAttributes.HTTP_USER_AGENT, req.headers["user-agent"]);
-          }
-
-          if (req.url) {
-            span.setAttribute("url.path", req.url);
-          }
-
-          if (req.headers.referer) {
-            span.setAttribute(SemanticAttributes.HTTP_TARGET, req.headers.referer);
-          }
-
-          if (req.headers.host) {
-            span.setAttribute(SemanticAttributes.NET_HOST_NAME, req.headers.host);
-          }
-
-          if (saleorApiUrl) {
-            span.setAttribute("saleorApiUrl", saleorApiUrl);
-          }
-
-          span.setAttribute("requestId", vercelRequestId);
-          span.setAttribute(SemanticAttributes.HTTP_METHOD, reqMethod);
-          span.setAttribute(SemanticAttributes.HTTP_ROUTE, staticRouteName);
-          span.setAttribute(SemanticAttributes.HTTP_TARGET, req.url ?? "");
-
-          if (reqId) {
-            span.setAttribute("vercelRequestId", reqId);
-          }
-
           const originalResEnd = res.end;
 
+          /**
+           * Override native res.end to flush OTEL traces before it ends
+           */
           // @ts-expect-error - this is a hack to get around Vercel freezing lambda's
           res.end = async function (this: unknown, ...args: unknown[]) {
             span.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, res.statusCode);
             span.end();
 
             try {
-              await race({
-                promise: loggerProvider.forceFlush(),
-                error: new Error("Timeout flushing OTEL logs"),
-                timeout: OTEL_FLUSH_TIMEOUT,
-              });
-
-              await race({
-                promise: Promise.all([
-                  batchSpanProcessor.forceFlush(),
-                  otelLogsProcessor.forceFlush(),
-                ]),
-                error: new Error("Timeout flushing OTEL spans"),
-                timeout: OTEL_FLUSH_TIMEOUT,
-              });
+              await flushOtel();
             } catch (e) {
               // winstonLogger.error("Failed to flush OTEL", { error: e });
               // noop - don't block return even if we loose traces
             }
 
-            // @ts-expect-error passthrough args
+            // @ts-expect-error passthrough args to the original function
             return originalResEnd.apply(this, args);
           };
 
@@ -136,25 +99,14 @@ export const withOtel = (handler: NextApiHandler, staticRouteName: string): Next
             span.end();
 
             try {
-              await race({
-                promise: loggerProvider.forceFlush(),
-                error: new Error("Timeout flushing OTEL logs"),
-                timeout: OTEL_FLUSH_TIMEOUT,
-              });
-              await race({
-                promise: Promise.all([
-                  batchSpanProcessor.forceFlush(),
-                  otelLogsProcessor.forceFlush(),
-                ]),
-                error: new Error("Timeout flushing OTEL spans"),
-                timeout: OTEL_FLUSH_TIMEOUT,
-              });
+              await flushOtel();
             } catch (e) {
               // winstonLogger.error("Failed to flush OTEL", { error: e });
-              // noop - don't block return even if we loose traces
             }
 
-            // Rethrow error so that Next.js (and other wrappers like Sentry) can handle it
+            /**
+             * Rethrow error so that Next.js (and other wrappers like Sentry) can handle it
+             */
             throw error;
           }
         },
@@ -165,14 +117,9 @@ export const withOtel = (handler: NextApiHandler, staticRouteName: string): Next
 
 export function setErrorOnSpan(error: unknown, span: Span) {
   span.setStatus({ code: SpanStatusCode.ERROR });
+
   if (error instanceof Error) {
     span.setAttribute("error.type", error.name);
     span.recordException(error);
-
-    // if (error instanceof BaseError) {
-    //   span.setAttribute("error.stack", error.stack ?? "");
-    //   span.setAttribute("error.severity", error.sentrySeverity ?? "");
-    //   span.setAttribute("error.errorCode", error.errorCode ?? "");
-    // }
   }
 }
