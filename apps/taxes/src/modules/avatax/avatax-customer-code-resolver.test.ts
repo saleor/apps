efd046c8dc23c3ced71a28b48cd2a5946159196d
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2024-02-01 11:15:51 +0100

    add resolve avataxCustomerCode

--- apps/taxes/src/modules/avatax/avatax-customer-code-resolver.test.ts
+++ apps/taxes/src/modules/avatax/avatax-customer-code-resolver.test.ts
@@ -1,104 +1,32 @@
 import { describe, expect, it } from "vitest";
-import { CalculateTaxesPayload } from "../../pages/api/webhooks/checkout-calculate-taxes";
-import { AvataxCustomerCodeResolver } from "./avatax-customer-code-resolver";
-import {
-  OrderConfirmedEventSubscriptionFragment,
-  OrderConfirmedSubscriptionFragment,
-} from "../../../generated/graphql";
+import { UserFragment } from "../../../generated/graphql";
+import { avataxCustomerCode } from "./avatax-customer-code-resolver";
 
-const idOrderPayload = {
-  user: {
-    id: "123",
-  },
-} as unknown as OrderConfirmedSubscriptionFragment;
-
-const noIdOrderPayload = {} as unknown as OrderConfirmedSubscriptionFragment;
-
-const noIdCalculateTaxesCheckoutPayload = {
-  taxBase: {
-    sourceObject: {
-      __typename: "Checkout",
-    },
-  },
-} as unknown as CalculateTaxesPayload;
-
-const idCalculateTaxesCheckoutPayload = {
-  taxBase: {
-    sourceObject: {
-      __typename: "Checkout",
-      user: {
-        id: "123",
-      },
-    },
-  },
-} as unknown as CalculateTaxesPayload;
-
-const noIdCalculateTaxesOrderPayload = {
-  taxBase: {
-    sourceObject: {
-      __typename: "Order",
-    },
-  },
-} as unknown as CalculateTaxesPayload;
-
-const idCalculateTaxesOrderPayload = {
-  taxBase: {
-    sourceObject: {
-      __typename: "Order",
-      user: {
-        id: "123",
-      },
-    },
-  },
-} as unknown as CalculateTaxesPayload;
-
-const resolver = new AvataxCustomerCodeResolver();
-
-describe("AvataxCustomerCodeResolver", () => {
-  describe("resolveOrderCustomerCode", () => {
+describe("avataxCustomerCode", () => {
+  describe("resolve", () => {
     it("returns user id when present in sourceObject", () => {
-      const customerCode = resolver.resolveOrderCustomerCode(idOrderPayload);
+      const customerCode = avataxCustomerCode.resolve({
+        email: "demo@saleor.io",
+        id: "123",
+      });
 
       expect(customerCode).toBe("123");
     });
+    it("returns avataxCustomerCode when present in sourceObject", () => {
+      const customerCode = avataxCustomerCode.resolve({
+        email: "demo@saleor.io",
+        id: "123",
+        avataxCustomerCode: "456",
+      });
+
+      expect(customerCode).toBe("456");
+    });
     it("returns 0 when no user id", () => {
-      const customerCode = resolver.resolveOrderCustomerCode(noIdOrderPayload);
+      const customerCode = avataxCustomerCode.resolve({
+        email: "demo@saleor.io",
+      } as UserFragment);
 
       expect(customerCode).toBe("0");
-    });
-  });
-  describe("resolveCalculateTaxesCustomerCode", () => {
-    describe("when checkout", () => {
-      it("returns user id when present in sourceObject", () => {
-        const customerCode = resolver.resolveCalculateTaxesCustomerCode(
-          idCalculateTaxesCheckoutPayload,
-        );
-
-        expect(customerCode).toBe("123");
-        it("returns 0 when no user id", () => {
-          const customerCode = resolver.resolveCalculateTaxesCustomerCode(
-            noIdCalculateTaxesCheckoutPayload,
-          );
-
-          expect(customerCode).toBe("0");
-        });
-      });
-    });
-    describe("when order", () => {
-      it("returns user id when present in sourceObject", () => {
-        const customerCode = resolver.resolveCalculateTaxesCustomerCode(
-          idCalculateTaxesOrderPayload,
-        );
-
-        expect(customerCode).toBe("123");
-      });
-      it("returns 0 when no user id", () => {
-        const customerCode = resolver.resolveCalculateTaxesCustomerCode(
-          noIdCalculateTaxesOrderPayload,
-        );
-
-        expect(customerCode).toBe("0");
-      });
     });
   });
 });
--- apps/taxes/src/modules/avatax/avatax-customer-code-resolver.ts
+++ apps/taxes/src/modules/avatax/avatax-customer-code-resolver.ts
@@ -1,18 +1,31 @@
-import { OrderConfirmedSubscriptionFragment } from "../../../generated/graphql";
-import { CalculateTaxesPayload } from "../../pages/api/webhooks/checkout-calculate-taxes";
+import { createLogger } from "@saleor/apps-logger";
+import { UserFragment } from "../../../generated/graphql";
 
-export class AvataxCustomerCodeResolver {
-  /*
-   * "If you're creating a sales order for a customer that does not yet exist in your system, you can send a dummyCustomerCode as sales orders are not recorded."
-   * https://developer.avalara.com/ecommerce-integration-guide/sales-tax-badge/transactions/simple-transactions/
-   */
-  private fallbackCustomerCode = "0";
+const logger = createLogger("avataxCustomerCode");
 
-  resolveOrderCustomerCode(order: OrderConfirmedSubscriptionFragment) {
-    return order.user?.id ?? this.fallbackCustomerCode;
-  }
+/*
+ * "If you're creating a sales order for a customer that does not yet exist in your system, you can send a dummyCustomerCode as sales orders are not recorded."
+ * https://developer.avalara.com/ecommerce-integration-guide/sales-tax-badge/transactions/simple-transactions/
+ */
+const FALLBACK_CUSTOMER_CODE = "0";
 
-  resolveCalculateTaxesCustomerCode(payload: CalculateTaxesPayload): string {
-    return payload.taxBase.sourceObject.user?.id ?? this.fallbackCustomerCode;
-  }
-}
+export const avataxCustomerCode = {
+  resolve(user: UserFragment | null | undefined): string {
+    const avataxCustomerCode = user?.avataxCustomerCode;
+
+    if (avataxCustomerCode) {
+      logger.trace(`Returning customer code found in the user metadata.`);
+      return avataxCustomerCode;
+    }
+
+    const userId = user?.id;
+
+    if (userId) {
+      logger.trace(`Returning user id as a customer code.`);
+      return userId;
+    }
+
+    logger.trace(`Returning fallback customer code.`);
+    return FALLBACK_CUSTOMER_CODE;
+  },
+};
