c43c2eee8bbb69ecec2e29c48cf0032415f61774
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2024-02-01 15:18:46 +0100

    AvataxCalculateTaxesResponseTransformer -> transformAvataxTransactionModelIntoShipping

--- apps/taxes/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-response-shipping-transformer.ts
+++ apps/taxes/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-response-shipping-transformer.ts
@@ -5,57 +5,55 @@ import { taxProviderUtils } from "../../taxes/tax-provider-utils";
 import { CalculateTaxesResponse } from "../../taxes/tax-provider-webhook";
 import { avataxShippingLine } from "./avatax-shipping-line";
 
-export class AvataxCalculateTaxesResponseShippingTransformer {
-  transform(
-    transaction: TransactionModel,
-  ): Pick<
-    CalculateTaxesResponse,
-    "shipping_price_gross_amount" | "shipping_price_net_amount" | "shipping_tax_rate"
-  > {
-    const shippingLine = avataxShippingLine.getFromTransactionModel(transaction);
+export function transformAvataxTransactionModelIntoShipping(
+  transaction: TransactionModel,
+): Pick<
+  CalculateTaxesResponse,
+  "shipping_price_gross_amount" | "shipping_price_net_amount" | "shipping_tax_rate"
+> {
+  const shippingLine = avataxShippingLine.getFromTransactionModel(transaction);
 
-    if (!shippingLine) {
-      return {
-        shipping_price_gross_amount: 0,
-        shipping_price_net_amount: 0,
-        shipping_tax_rate: 0,
-      };
-    }
-
-    if (!shippingLine.isItemTaxable) {
-      return {
-        shipping_price_gross_amount: taxProviderUtils.resolveOptionalOrThrowUnexpectedError(
-          shippingLine.lineAmount,
-          new TaxBadProviderResponseError("shippingLine.lineAmount is undefined"),
-        ),
-        shipping_price_net_amount: taxProviderUtils.resolveOptionalOrThrowUnexpectedError(
-          shippingLine.lineAmount,
-          new TaxBadProviderResponseError("shippingLine.lineAmount is undefined"),
-        ),
-        /*
-         * avatax doesn't return combined tax rate
-         * // todo: calculate percentage tax rate
-         */
-        shipping_tax_rate: 0,
-      };
-    }
-
-    const shippingTaxCalculated = taxProviderUtils.resolveOptionalOrThrowUnexpectedError(
-      shippingLine.taxCalculated,
-      new TaxBadProviderResponseError("shippingLine.taxCalculated is undefined"),
-    );
-    const shippingTaxableAmount = taxProviderUtils.resolveOptionalOrThrowUnexpectedError(
-      shippingLine.taxableAmount,
-      new TaxBadProviderResponseError("shippingLine.taxableAmount is undefined"),
-    );
-    const shippingGrossAmount = numbers.roundFloatToTwoDecimals(
-      shippingTaxableAmount + shippingTaxCalculated,
-    );
-
+  if (!shippingLine) {
     return {
-      shipping_price_gross_amount: shippingGrossAmount,
-      shipping_price_net_amount: shippingTaxableAmount,
+      shipping_price_gross_amount: 0,
+      shipping_price_net_amount: 0,
       shipping_tax_rate: 0,
     };
   }
+
+  if (!shippingLine.isItemTaxable) {
+    return {
+      shipping_price_gross_amount: taxProviderUtils.resolveOptionalOrThrowUnexpectedError(
+        shippingLine.lineAmount,
+        new TaxBadProviderResponseError("shippingLine.lineAmount is undefined"),
+      ),
+      shipping_price_net_amount: taxProviderUtils.resolveOptionalOrThrowUnexpectedError(
+        shippingLine.lineAmount,
+        new TaxBadProviderResponseError("shippingLine.lineAmount is undefined"),
+      ),
+      /*
+       * avatax doesn't return combined tax rate
+       * // todo: calculate percentage tax rate
+       */
+      shipping_tax_rate: 0,
+    };
+  }
+
+  const shippingTaxCalculated = taxProviderUtils.resolveOptionalOrThrowUnexpectedError(
+    shippingLine.taxCalculated,
+    new TaxBadProviderResponseError("shippingLine.taxCalculated is undefined"),
+  );
+  const shippingTaxableAmount = taxProviderUtils.resolveOptionalOrThrowUnexpectedError(
+    shippingLine.taxableAmount,
+    new TaxBadProviderResponseError("shippingLine.taxableAmount is undefined"),
+  );
+  const shippingGrossAmount = numbers.roundFloatToTwoDecimals(
+    shippingTaxableAmount + shippingTaxCalculated,
+  );
+
+  return {
+    shipping_price_gross_amount: shippingGrossAmount,
+    shipping_price_net_amount: shippingTaxableAmount,
+    shipping_tax_rate: 0,
+  };
 }
