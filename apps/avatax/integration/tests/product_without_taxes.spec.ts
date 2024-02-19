c830318650348e0453af024fcda0afc4fb6b752c
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2024-02-19 17:22:04 +0100

    Add more tests for making order and adding delivery method

--- apps/avatax/integration/tests/product_without_taxes.spec.ts
+++ apps/avatax/integration/tests/product_without_taxes.spec.ts
@@ -1,5 +1,6 @@
 import { it } from "vitest";
-import { spec } from "pactum";
+import { handler, spec } from "pactum";
+import { string } from "pactum-matchers";
 
 // Trick editor into highlighting gql, wihtout parsing
 const gql = String.raw;
@@ -67,5 +68,109 @@ it("should return taxes for product without taxes", async () => {
     .expectJson("data.checkoutCreate.checkout.totalPrice.tax", {
       amount: 26.63,
       currency: "USD",
+    })
+    .stores("CheckoutId", "data.checkoutCreate.checkout.id");
+
+  await spec()
+    .post("/graphql/")
+    .withGraphQLQuery(gql`
+      mutation checkoutDeliveryMethodUpdate($checkoutId: ID!, $deliveryMethodId: ID!) {
+        checkoutDeliveryMethodUpdate(id: $checkoutId, deliveryMethodId: $deliveryMethodId) {
+          errors {
+            ...CheckoutErrorFragment
+          }
+          checkout {
+            id
+            lines {
+              totalPrice {
+                gross {
+                  ...Money
+                }
+                tax {
+                  ...Money
+                }
+              }
+            }
+            shippingPrice {
+              gross {
+                ...Money
+              }
+              tax {
+                ...Money
+              }
+            }
+            totalPrice {
+              gross {
+                ...Money
+              }
+              tax {
+                ...Money
+              }
+            }
+          }
+        }
+      }
+
+      fragment CheckoutErrorFragment on CheckoutError {
+        message
+        field
+        code
+      }
+
+      fragment Money on Money {
+        currency
+        amount
+      }
+    `)
+    .withGraphQLVariables({
+      "@DATA:TEMPLATE@": "UpdateDeliveryMethod:USA",
+    })
+    .expectJson("data.checkoutDeliveryMethodUpdate.checkout.totalPrice.gross", {
+      currency: "USD",
+      amount: 402.09,
+    })
+    .expectJson("data.checkoutDeliveryMethodUpdate.checkout.totalPrice.tax", {
+      currency: "USD",
+      amount: 32.78,
+    })
+    .expectJson("data.checkoutDeliveryMethodUpdate.checkout.shippingPrice.gross", {
+      currency: "USD",
+      amount: 75.46,
+    })
+    .expectJson("data.checkoutDeliveryMethodUpdate.checkout.shippingPrice.tax", {
+      currency: "USD",
+      amount: 6.15,
+    });
+
+  await spec()
+    .post("/graphql/")
+    .withGraphQLQuery(gql`
+      mutation CompleteCheckout($checkoutId: ID!) {
+        checkoutComplete(id: $checkoutId) {
+          order {
+            id
+          }
+          confirmationNeeded
+          confirmationData
+          errors {
+            field
+            message
+            code
+            variants
+            lines
+            addressType
+          }
+        }
+      }
+    `)
+    .withGraphQLVariables({ checkoutId: "$S{CheckoutId}" })
+    .expectJsonMatch({
+      data: {
+        checkoutComplete: {
+          order: {
+            id: string(),
+          },
+        },
+      },
     });
 });
