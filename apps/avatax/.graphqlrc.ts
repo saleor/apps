20e1960149b950686d2351f4f0ce0568a3e79a20
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2024-02-23 12:11:53 +0100

    Add config for e2e GraphQL codegen

--- apps/avatax/.graphqlrc.ts
+++ apps/avatax/.graphqlrc.ts
@@ -1,6 +1,35 @@
 import { IGraphQLConfig } from "graphql-config";
 const config: IGraphQLConfig = {
   projects: {
+    e2e: {
+      schema: "graphql/schema.graphql",
+      documents: ["e2e/**/*.graphql"],
+      extensions: {
+        codegen: {
+          generates: {
+            // Export all Saleor scalar types into separate types.ts file
+            "e2e/generated/types.ts": {
+              plugins: ["typescript"],
+            },
+            // Export all queries and mutations into separate file, use Saleor types from types.ts
+            "e2e/generated/graphql.ts": {
+              preset: "import-types",
+              plugins: ["typescript-operations", "typescript-document-nodes"],
+              presetConfig: {
+                typesPath: "e2e/generated/types.ts",
+              },
+              config: {
+                // Export operations into raw string with `gql` used in template literals
+                rawRequest: true,
+                // We don't want to use `graphql-tag` for parsing of strings into AST
+                // because PactumJS expects string as input to requests
+                gqlImport: "../utils#gql",
+              },
+            },
+          },
+        },
+      },
+    },
     default: {
       schema: "graphql/schema.graphql",
       documents: [
