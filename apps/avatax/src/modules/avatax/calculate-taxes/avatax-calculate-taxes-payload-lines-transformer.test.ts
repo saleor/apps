905af50e69d2589a907261b030857fcc914ad9ed
Wojciech <wojciech.mista@hotmail.com>
2024-02-26 10:24:26 +0100

    determine which line is discounted

--- apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-payload-lines-transformer.test.ts
+++ apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-payload-lines-transformer.test.ts
@@ -101,14 +101,14 @@ describe("AvataxCalculateTaxesPayloadLinesTransformer", () => {
           quantity: 1,
           taxCode: "",
           taxIncluded: true,
-          discounted: true,
+          discounted: false,
         },
         {
           amount: 100,
           quantity: 2,
           taxCode: "",
           taxIncluded: true,
-          discounted: true,
+          discounted: false,
         },
         {
           amount: 48.33,
@@ -119,6 +119,54 @@ describe("AvataxCalculateTaxesPayloadLinesTransformer", () => {
           taxIncluded: true,
         },
       ]);
+    });
+    it("discounts propery", () => {
+      const mockGenerator = new AvataxCalculateTaxesMockGenerator();
+      const avataxConfigMock = mockGenerator.generateAvataxConfig();
+      const taxBaseMock = mockGenerator.generateTaxBase();
+      const matchesMock = mockGenerator.generateTaxCodeMatches();
+
+      const lines = transformer.transform(taxBaseMock, avataxConfigMock, matchesMock);
+
+      expect(lines).toEqual([
+        {
+          amount: 60,
+          quantity: 3,
+          taxCode: "P0000000",
+          taxIncluded: true,
+          discounted: false,
+        },
+        {
+          amount: 20,
+          quantity: 1,
+          taxCode: "",
+          taxIncluded: true,
+          discounted: false,
+        },
+        {
+          amount: 100,
+          quantity: 2,
+          taxCode: "",
+          taxIncluded: true,
+          discounted: false,
+        },
+        {
+          amount: 48.33,
+          itemCode: "Shipping",
+          quantity: 1,
+          taxCode: "FR000000",
+          taxIncluded: true,
+          discounted: false,
+        },
+      ]);
+      /*
+       * 1. get 3 products priced at 3.66, get 3 products priced at 5
+       * 2. get a 5% voucher for ONE item
+       * 3. Create a checkout
+       * 4. Add voucher
+       * 5. Set shipping method
+       * 6. check if the voucher is not distributed across 3 highest priced items
+       */
     });
   });
 });
