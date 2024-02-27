f82851fa5862a8d49f1ac59b224268570cc07927
Wojciech <wojciech.mista@hotmail.com>
2024-02-27 12:47:45 +0100

    small improvements

--- apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-payload-lines-transformer.test.ts
+++ apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-payload-lines-transformer.test.ts
@@ -8,8 +8,8 @@ describe("AvataxCalculateTaxesPayloadLinesTransformer", () => {
   describe("transform", () => {
     it("maps lines, adds shipping as line and maps the tax code of one product", () => {
       const mockGenerator = new AvataxCalculateTaxesMockGenerator();
-      const avataxConfigMock = mockGenerator.generateAvataxConfig();
       const taxBaseMock = mockGenerator.generateTaxBase();
+      const avataxConfigMock = mockGenerator.generateAvataxConfig();
       const matchesMock = mockGenerator.generateTaxCodeMatches();
 
       const lines = transformer.transform(taxBaseMock, avataxConfigMock, matchesMock);
@@ -78,7 +78,7 @@ describe("AvataxCalculateTaxesPayloadLinesTransformer", () => {
         },
       ]);
     });
-    it("when discounts, sets discounted to true", () => {
+    it("when discounts, sets discounted to true [entire checkout voucher]", () => {
       const mockGenerator = new AvataxCalculateTaxesMockGenerator();
       const avataxConfigMock = mockGenerator.generateAvataxConfig();
       const matchesMock = mockGenerator.generateTaxCodeMatches();
@@ -101,72 +101,24 @@ describe("AvataxCalculateTaxesPayloadLinesTransformer", () => {
           quantity: 1,
           taxCode: "",
           taxIncluded: true,
-          discounted: false,
+          discounted: true,
         },
         {
           amount: 100,
           quantity: 2,
           taxCode: "",
           taxIncluded: true,
-          discounted: false,
+          discounted: true,
         },
         {
           amount: 48.33,
-          discounted: false,
+          discounted: true,
           itemCode: "Shipping",
           quantity: 1,
           taxCode: "FR000000",
           taxIncluded: true,
         },
       ]);
-    });
-    it("discounts propery", () => {
-      const mockGenerator = new AvataxCalculateTaxesMockGenerator();
-      const avataxConfigMock = mockGenerator.generateAvataxConfig();
-      const taxBaseMock = mockGenerator.generateTaxBase();
-      const matchesMock = mockGenerator.generateTaxCodeMatches();
-
-      const lines = transformer.transform(taxBaseMock, avataxConfigMock, matchesMock);
-
-      expect(lines).toEqual([
-        {
-          amount: 60,
-          quantity: 3,
-          taxCode: "P0000000",
-          taxIncluded: true,
-          discounted: false,
-        },
-        {
-          amount: 20,
-          quantity: 1,
-          taxCode: "",
-          taxIncluded: true,
-          discounted: false,
-        },
-        {
-          amount: 100,
-          quantity: 2,
-          taxCode: "",
-          taxIncluded: true,
-          discounted: false,
-        },
-        {
-          amount: 48.33,
-          itemCode: "Shipping",
-          quantity: 1,
-          taxCode: "FR000000",
-          taxIncluded: true,
-          discounted: false,
-        },
-      ]);
-      /*
-       * 1. get 3 products priced at 3.66, get 3 products priced at 5
-       * 2. get a 5% voucher for ONE item
-       * 3. Create a checkout
-       * 4. Add voucher
-       * 5. Set shipping method
-       * 6. check if the voucher is not distributed across 3 highest priced items
-       */
     });
   });
 });
