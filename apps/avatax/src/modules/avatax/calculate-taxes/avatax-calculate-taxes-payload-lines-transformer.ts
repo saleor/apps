198b62cbddc059410251dbeb0481801f41f3a8d1
Wojciech <wojciech.mista@hotmail.com>
2024-02-28 15:26:36 +0100

    cr fixes

--- apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-payload-lines-transformer.ts
+++ apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-payload-lines-transformer.ts
@@ -30,37 +30,39 @@ const checkIfIsDiscountedLine = (isDiscounted: boolean, line: TaxBaseLine) => {
  * If entire checkout discount then lines in webhook subscription are not discounted
  * If one item is discounted then the line has discounted totalPrice
  */
-const checkDiscounts = (taxBase: TaxBaseFragment, isDiscounted: boolean) => {
+const checkDiscounts = (taxBase: TaxBaseFragment, isEntireSourceDiscounted: boolean) => {
   let hasEntireCheckoutDiscount = false;
   let hasOncePerOrderVoucher = false;
   let discountedLinesCount = 0;
 
-  for (const line of taxBase.lines) {
-    const isLineDiscounted = checkIfIsDiscountedLine(isDiscounted, line);
+  if (isEntireSourceDiscounted) {
+    for (const line of taxBase.lines) {
+      const isLineDiscounted = checkIfIsDiscountedLine(isEntireSourceDiscounted, line);
 
-    if (isLineDiscounted) {
-      discountedLinesCount++;
+      if (isLineDiscounted) {
+        discountedLinesCount++;
+      }
     }
   }
 
-  // If none of the lines are discounted, but isDiscounted is true then it's entire checkout discount
-  if (discountedLinesCount === 0 && isDiscounted) {
-    hasEntireCheckoutDiscount = true;
-  }
-
-  if (discountedLinesCount === 1) {
-    hasOncePerOrderVoucher = true;
-  }
+  // If none of the lines are discounted, but isEntireSourceDiscounted is true then it's entire checkout discount
+  hasEntireCheckoutDiscount = discountedLinesCount === 0;
+  hasOncePerOrderVoucher = discountedLinesCount === 1;
 
   return { hasEntireCheckoutDiscount, hasOncePerOrderVoucher };
 };
 
-const isProductLineDiscounted = (
-  line: TaxBaseLine,
-  isDiscounted: boolean,
-  hasOncePerOrderVoucher: boolean,
-  hasEntireCheckoutDiscount: boolean,
-) => {
+const isProductLineDiscounted = ({
+  line,
+  isDiscounted,
+  hasOncePerOrderVoucher,
+  hasEntireCheckoutDiscount,
+}: {
+  line: TaxBaseLine;
+  isDiscounted: boolean;
+  hasOncePerOrderVoucher: boolean;
+  hasEntireCheckoutDiscount: boolean;
+}) => {
   if (!isDiscounted) return false;
 
   if (hasOncePerOrderVoucher) {
@@ -95,12 +97,12 @@ export class AvataxCalculateTaxesPayloadLinesTransformer {
         taxIncluded: taxBase.pricesEnteredWithTax,
         taxCode,
         quantity: line.quantity,
-        discounted: isProductLineDiscounted(
+        discounted: isProductLineDiscounted({
           line,
           isDiscounted,
           hasOncePerOrderVoucher,
           hasEntireCheckoutDiscount,
-        ),
+        }),
       };
     });
 
