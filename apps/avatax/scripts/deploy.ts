975cb157f0233d57f957560629ee522b0912fb43
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-03-15 16:08:59 +0100

    Update avatax deploy script

--- apps/avatax/scripts/deploy.ts
+++ apps/avatax/scripts/deploy.ts
@@ -1,3 +1,13 @@
+import { default as envUtils } from "@next/env";
 import { execSync } from "child_process";
 
+envUtils.loadEnvConfig(".");
+
+// Must use dynamic import for env variables to load properly
+const { getReleaseTag } = await import("../src/release-utils");
+const release = getReleaseTag();
+
+console.log("Using release tag:", release);
+
+execSync(`SENTRY_RELEASE='${release}' pnpm run build`, { stdio: "inherit" });
 execSync("pnpm run migrate", { stdio: "inherit" });
