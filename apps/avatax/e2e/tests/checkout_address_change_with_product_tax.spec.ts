0b14abb759d4b1b24c6f7e660ec9fd7f1f093cd4
Wojciech <wojciech.mista@hotmail.com>
2024-03-27 11:23:38 +0100

    uncomment

--- apps/avatax/e2e/tests/checkout_address_change_with_product_tax.spec.ts
+++ apps/avatax/e2e/tests/checkout_address_change_with_product_tax.spec.ts
@@ -53,26 +53,18 @@ describe("App should calculate taxes for checkout with product tax [pricesEntere
         },
       })
       .expectStatus(200)
-      /*
-       * TODO:
-       * There's a difference in the expected values of the total price between v3.18 and v3.19
-       * They will be addressed in `SHOPX-428`
-       * Leaving it commented out for now
-       */
-      /*
-       * .expectJson("data.checkoutCreate.checkout.totalPrice.net", {
-       *   amount: TOTAL_GROSS_PRICE_BEFORE_SHIPPING, // no taxes are calculated yet
-       *   currency: "USD",
-       * })
-       * .expectJson("data.checkoutCreate.checkout.totalPrice.gross", {
-       *   amount: TOTAL_GROSS_PRICE_BEFORE_SHIPPING,
-       *   currency: "USD",
-       * })
-       * .expectJson("data.checkoutCreate.checkout.totalPrice.tax", {
-       *   amount: 0,
-       *   currency: "USD",
-       * })
-       */
+      .expectJson("data.checkoutCreate.checkout.totalPrice.net", {
+        amount: TOTAL_GROSS_PRICE_BEFORE_SHIPPING, // no taxes are calculated yet
+        currency: "USD",
+      })
+      .expectJson("data.checkoutCreate.checkout.totalPrice.gross", {
+        amount: TOTAL_GROSS_PRICE_BEFORE_SHIPPING,
+        currency: "USD",
+      })
+      .expectJson("data.checkoutCreate.checkout.totalPrice.tax", {
+        amount: 0,
+        currency: "USD",
+      })
       .stores("CheckoutId", "data.checkoutCreate.checkout.id");
   });
 
@@ -87,6 +79,7 @@ describe("App should calculate taxes for checkout with product tax [pricesEntere
         shippingAddress: "$M{Address.NewYork}",
       })
       .expectStatus(200)
+      .inspect()
       .expectJsonLike(
         "data.checkoutShippingAddressUpdate.checkout.shippingAddress",
         addressVerification,
