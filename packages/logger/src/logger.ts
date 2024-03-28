7f6aa3d27a7189e19d8db13dc5b58d1a3fd60fff
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-03-28 14:56:47 +0100

    Add changeset

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -10,10 +10,10 @@ function getMinLevel() {
       return 0;
     case "trace":
       return 1;
-    case "info":
-      return 3;
     case "debug":
       return 2;
+    case "info":
+      return 3;
     case "warn":
       return 4;
     case "error":
