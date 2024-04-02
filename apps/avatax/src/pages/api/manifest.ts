58d5b330e37e506a795ee40d763e028fd496219a
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-04-02 12:33:20 +0200

    remove test file

--- apps/avatax/src/pages/api/manifest.ts
+++ apps/avatax/src/pages/api/manifest.ts
@@ -7,16 +7,11 @@ import packageJson from "../../../package.json";
 import { REQUIRED_SALEOR_VERSION } from "../../../saleor-app";
 import { appWebhooks } from "../../../webhooks";
 import { loggerContext } from "../../logger-context";
-import { captureException } from "@sentry/nextjs";
 
 export default wrapWithLoggerContext(
   withOtel(
     createManifestHandler({
-      async manifestFactory({ appBaseUrl, request }) {
-        if (request.query.forceerror) {
-          captureException(new Error("Force error TEST"));
-        }
-
+      async manifestFactory({ appBaseUrl }) {
         const iframeBaseUrl = process.env.APP_IFRAME_BASE_URL ?? appBaseUrl;
         const apiBaseURL = process.env.APP_API_BASE_URL ?? appBaseUrl;
 
