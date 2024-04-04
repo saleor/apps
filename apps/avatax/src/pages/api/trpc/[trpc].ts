a5ef57cc004a30fb9b036c693b70adcb114c6b13
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-04-04 10:44:15 +0200

    Add Sentry to tRPC (#1298)

--- apps/avatax/src/pages/api/trpc/[trpc].ts
+++ apps/avatax/src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,11 @@
+import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
+import { withOtel } from "@saleor/apps-otel";
+import * as Sentry from "@sentry/nextjs";
 import * as trpcNext from "@trpc/server/adapters/next";
+import { createLogger } from "../../../logger";
+import { loggerContext } from "../../../logger-context";
 import { appRouter } from "../../../modules/trpc/trpc-app-router";
 import { createTrpcContext } from "../../../modules/trpc/trpc-context";
-import { withOtel } from "@saleor/apps-otel";
-import { createLogger } from "../../../logger";
-import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
-import { loggerContext } from "../../../logger-context";
 
 const logger = createLogger("tRPC error");
 
@@ -18,6 +19,7 @@ export default wrapWithLoggerContext(
       createContext: createTrpcContext,
       onError: ({ path, error }) => {
         if (error.code === "INTERNAL_SERVER_ERROR") {
+          Sentry.captureException(error);
           logger.error(`${path} returned error:`, error);
           return;
         }
