2772c2063309e8b84629de2ff0683dfb55794dc5
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-04-04 20:55:44 +0200

    add tests

--- apps/avatax/src/lib/app-config.test.ts
+++ apps/avatax/src/lib/app-config.test.ts
@@ -17,14 +17,143 @@ describe("AppConfig", () => {
       });
     });
 
-    it.todo(
-      "returns InvalidChannelSlugError if provided slug is not available in saved config (items in collection variant)",
-    );
+    it("returns InvalidChannelSlugError if provided slug is not available in saved config (items in collection variant)", () => {
+      const config = AppConfig.createFromParsedConfig({
+        channels: [
+          {
+            id: "1",
+            config: {
+              slug: "default-channel",
+              providerConnectionId: "pci-1",
+            },
+          },
+        ],
+        providerConnections: [
+          {
+            provider: "avatax",
+            id: "pci-1",
+            config: {
+              companyCode: "test",
+              credentials: {
+                password: "test",
+                username: "test",
+              },
+              address: {
+                city: "test",
+                country: "test",
+                zip: "10111",
+                state: "NY",
+                street: "test",
+              },
+              isSandbox: false,
+              name: "config",
+              isAutocommit: false,
+              isDocumentRecordingEnabled: false,
+              shippingTaxCode: "123",
+            },
+          },
+        ],
+      });
 
-    it.todo(
-      "returns MissingConfigurationError if channel was found, but there is no configuration assigned to it",
-    );
+      const result = config.getConfigForChannelSlug("another-channel");
 
-    it.todo("returns valid configuration for provided channel slug, if exists");
+      expect(result._unsafeUnwrapErr()).toBeInstanceOf(AppConfig.InvalidChannelSlugError);
+      expect(result._unsafeUnwrapErr()).toMatchObject({
+        channelSlug: "another-channel",
+      });
+    });
+
+    it("returns MissingConfigurationError if channel was found, but there is no configuration assigned to it", () => {
+      const config = AppConfig.createFromParsedConfig({
+        channels: [
+          {
+            id: "1",
+            config: {
+              slug: "default-channel",
+              providerConnectionId: "pci-1",
+            },
+          },
+        ],
+        providerConnections: [],
+      });
+
+      const result = config.getConfigForChannelSlug("default-channel");
+
+      expect(result._unsafeUnwrapErr()).toBeInstanceOf(AppConfig.MissingConfigurationError);
+      expect(result._unsafeUnwrapErr()).toMatchObject({
+        channelSlug: "default-channel",
+      });
+    });
+
+    it("returns valid configuration for provided channel slug, if exists", () => {
+      const config = AppConfig.createFromParsedConfig({
+        channels: [
+          {
+            id: "1",
+            config: {
+              slug: "default-channel",
+              providerConnectionId: "pci-1",
+            },
+          },
+        ],
+        providerConnections: [
+          {
+            provider: "avatax",
+            id: "pci-1",
+            config: {
+              companyCode: "test",
+              credentials: {
+                password: "test",
+                username: "test",
+              },
+              address: {
+                city: "test",
+                country: "test",
+                zip: "10111",
+                state: "NY",
+                street: "test",
+              },
+              isSandbox: false,
+              name: "config",
+              isAutocommit: false,
+              isDocumentRecordingEnabled: false,
+              shippingTaxCode: "123",
+            },
+          },
+        ],
+      });
+
+      const result = config.getConfigForChannelSlug("default-channel");
+
+      expect(result._unsafeUnwrap()).toMatchInlineSnapshot(`
+        {
+          "avataxConfig": {
+            "config": {
+              "address": {
+                "city": "test",
+                "country": "test",
+                "state": "NY",
+                "street": "test",
+                "zip": "10111",
+              },
+              "companyCode": "test",
+              "credentials": {
+                "password": "test",
+                "username": "test",
+              },
+              "isAutocommit": false,
+              "isDocumentRecordingEnabled": false,
+              "isSandbox": false,
+              "name": "config",
+              "shippingTaxCode": "123",
+            },
+            "id": "pci-1",
+            "provider": "avatax",
+          },
+          "channelConfigInternalId": "1",
+          "channelSlug": "default-channel",
+        }
+      `);
+    });
   });
 });
