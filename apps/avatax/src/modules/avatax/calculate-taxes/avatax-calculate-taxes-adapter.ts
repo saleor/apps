b12582a2ba7da97a266365016a1efb916ae316fb
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-04-25 11:30:22 +0200

    Add more data to Avatax logs

--- apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-adapter.ts
+++ apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-adapter.ts
@@ -1,18 +1,19 @@
 import { AuthData } from "@saleor/app-sdk/APL";
+import * as Sentry from "@sentry/nextjs";
+import { z } from "zod";
 import { createLogger } from "../../../logger";
+import { InvalidAppAddressError } from "../../taxes/tax-error";
 import { CalculateTaxesResponse } from "../../taxes/tax-provider-webhook";
 import { WebhookAdapter } from "../../taxes/tax-webhook-adapter";
 import { CalculateTaxesPayload } from "../../webhooks/payloads/calculate-taxes-payload";
 import { AvataxClient, CreateTransactionArgs } from "../avatax-client";
 import { AvataxConfig } from "../avatax-connection-schema";
 import { normalizeAvaTaxError } from "../avatax-error-normalizer";
-import { AvataxCalculateTaxesPayloadService } from "./avatax-calculate-taxes-payload.service";
-import { AvataxCalculateTaxesResponseTransformer } from "./avatax-calculate-taxes-response-transformer";
-import { z } from "zod";
-import * as Sentry from "@sentry/nextjs";
-import { InvalidAppAddressError } from "../../taxes/tax-error";
+import { extractTransactionArgsProperites } from "../create-transaction-args-properties";
 import { AvataxTaxCodeMatchesService } from "../tax-code/avatax-tax-code-matches.service";
 import { AvataxCalculateTaxesPayloadTransformer } from "./avatax-calculate-taxes-payload-transformer";
+import { AvataxCalculateTaxesPayloadService } from "./avatax-calculate-taxes-payload.service";
+import { AvataxCalculateTaxesResponseTransformer } from "./avatax-calculate-taxes-response-transformer";
 
 export type AvataxCalculateTaxesTarget = CreateTransactionArgs;
 export type AvataxCalculateTaxesResponse = CalculateTaxesResponse;
@@ -93,7 +94,12 @@ export class AvataxCalculateTaxesAdapter
 
     const target = await payloadService.getPayload(payload, config);
 
-    this.logger.debug("Calling AvaTax createTransaction with transformed payload...");
+    this.logger.info(
+      "Calling AvaTax createTransaction with transformed payload for calculate taxes event",
+      {
+        ...extractTransactionArgsProperites(target.model),
+      },
+    );
 
     try {
       const response = await this.avataxClient.createTransaction(target);
