6636ec0880277b92a0210c4f0211897192f9019a
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-04-25 15:08:00 +0200

    Add more data to Avatax logs (#1324)

---
+++ apps/avatax/src/modules/avatax/create-transaction-args-properties.ts
@@ -0,0 +1,23 @@
+import { CreateTransactionModel } from "avatax/lib/models/CreateTransactionModel";
+
+export const extractTransactionArgsProperites = (model: CreateTransactionModel) => ({
+  code: model.code,
+  type: model.type,
+  entityUseCode: model.entityUseCode,
+  customerCode: model.customerCode,
+  companyCode: model.companyCode,
+  isAutocommit: model.commit,
+  shipFromAddress: model.addresses?.shipFrom,
+  shipToAddress: model.addresses?.shipTo,
+  currencyCode: model.currencyCode,
+  lines: model.lines.map((line) => ({
+    amount: line.amount,
+    taxCode: line.taxCode,
+    quantity: line.quantity,
+    itemCode: line.itemCode,
+    description: line.description,
+  })),
+  date: model.date,
+  discount: model.discount,
+  isTaxIncluded: model.lines[0]?.taxIncluded ?? false,
+});
