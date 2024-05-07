7fcadca6932cf14c58277d1eb46efd2f3fea0fb5
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-05-07 15:04:01 +0200

    Log subscription payload errors (#1328)

--- apps/avatax/src/lib/error-utils.ts
+++ apps/avatax/src/lib/error-utils.ts
@@ -1,6 +1,12 @@
 import { TRPCClientError } from "@trpc/client";
+import { GraphQLError } from "graphql";
+import { BaseError } from "../error";
+import { logger } from "../logger";
+import { CalculateTaxesPayload } from "../modules/webhooks/payloads/calculate-taxes-payload";
+import { OrderCancelledPayload } from "../modules/webhooks/payloads/order-cancelled-payload";
+import { OrderConfirmedPayload } from "../modules/webhooks/payloads/order-confirmed-payload";
 
-function resolveTrpcClientError(error: unknown) {
+export function resolveTrpcClientError(error: unknown) {
   if (error instanceof TRPCClientError) {
     return error.message;
   }
@@ -8,6 +14,42 @@ function resolveTrpcClientError(error: unknown) {
   return "Unknown error";
 }
 
-export const errorUtils = {
-  resolveTrpcClientError,
-};
+export class SubscriptionPayloadErrorChecker {
+  static SubscriptionPayloadError = BaseError.subclass("SubscriptionPayloadError");
+
+  constructor(
+    private injectedLogger: Pick<typeof logger, "error">,
+    private injectedErrorCapture: (
+      exception: InstanceType<typeof SubscriptionPayloadErrorChecker.SubscriptionPayloadError>,
+    ) => void,
+  ) {}
+
+  checkPayload(payload: CalculateTaxesPayload | OrderCancelledPayload | OrderConfirmedPayload) {
+    // @ts-expect-error errors field is not yet typed on subscription payloads
+    const possibleErrors = payload?.errors as GraphQLError[];
+
+    const subscription = payload.__typename;
+
+    if (possibleErrors) {
+      possibleErrors.forEach((error) => {
+        const graphQLError = new SubscriptionPayloadErrorChecker.SubscriptionPayloadError(
+          error.message,
+          {
+            props: {
+              subscription,
+              source: error.source,
+              path: error.path,
+            },
+          },
+        );
+
+        this.injectedLogger.error(`Payload contains GraphQL error for ${subscription}`, {
+          error: graphQLError,
+          subscription,
+        });
+
+        this.injectedErrorCapture(graphQLError);
+      });
+    }
+  }
+}
