f95791b4595767cf0ae7431c05436286f5e5d3e7
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-05-13 20:38:00 +0200

    tests

--- apps/smtp/src/modules/smtp/services/html-to-text-compiler.ts
+++ apps/smtp/src/modules/smtp/services/html-to-text-compiler.ts
@@ -10,19 +10,19 @@ export interface IHtmlToTextCompiler {
 
 export class HtmlToTextCompiler implements IHtmlToTextCompiler {
   static HtmlToTextCompilerError = BaseError.subclass("HtmlToTextCompilerError");
-  static FailedToConvertError = this.HtmlToTextCompilerError.subclass("FailedToConvertError");
+  static FailedToCompileError = this.HtmlToTextCompilerError.subclass("FailedToConvertError");
 
   private logger = createLogger("HtmlToTextCompiler");
 
   compile(html: string): Result<string, InstanceType<typeof BaseError>> {
     this.logger.debug("Trying to convert HTML to plaintext");
 
-    return fromThrowable(
-      convert,
-      (err) =>
-        new HtmlToTextCompiler.FailedToConvertError("Failed to compile HTML to plain text", {
-          errors: [err],
-        }),
-    )(html);
+    return fromThrowable(convert, (err) => {
+      this.logger.debug("Failed to compile HTML to plain text", { error: err });
+
+      return new HtmlToTextCompiler.FailedToCompileError("Failed to compile HTML to plain text", {
+        errors: [err],
+      });
+    })(html);
   }
 }
