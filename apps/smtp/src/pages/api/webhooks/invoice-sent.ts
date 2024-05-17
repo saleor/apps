71e39579ea48ecadfda6afb107716e493266c1d0
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-05-17 14:08:22 +0200

    Apply use case error handling to webhook handlers

--- apps/smtp/src/pages/api/webhooks/invoice-sent.ts
+++ apps/smtp/src/pages/api/webhooks/invoice-sent.ts
@@ -8,6 +8,8 @@ import {
 import { withOtel } from "@saleor/apps-otel";
 import { createLogger } from "../../../logger";
 import { SendEventMessagesUseCaseFactory } from "../../../modules/event-handlers/use-case/send-event-messages.use-case.factory";
+import { SendEventMessagesUseCase } from "../../../modules/event-handlers/use-case/send-event-messages.use-case";
+import { captureException } from "@sentry/nextjs";
 
 const InvoiceSentWebhookPayload = gql`
   ${OrderDetailsFragmentDoc}
@@ -84,14 +86,47 @@ const handler: NextWebhookApiHandler<InvoiceSentWebhookPayloadFragment> = async 
 
   const useCase = useCaseFactory.createFromAuthData(authData);
 
-  await useCase.sendEventMessages({
-    channelSlug: channel,
-    event: "INVOICE_SENT",
-    payload: { order: payload.order },
-    recipientEmail,
-  });
+  return useCase
+    .sendEventMessages({
+      channelSlug: channel,
+      event: "INVOICE_SENT",
+      payload: { order: payload.order },
+      recipientEmail,
+    })
+    .then((result) =>
+      result.match(
+        (r) => {
+          logger.info("Successfully sent email(s)");
 
-  return res.status(200).json({ message: "The event has been handled" });
+          return res.status(200).json({ message: "The event has been handled" });
+        },
+        (err) => {
+          switch (err[0].constructor) {
+            case SendEventMessagesUseCase.ServerError: {
+              logger.error("Failed to send email(s) [server error]", { error: err });
+
+              return res.status(500).json({ message: "Failed to send email" });
+            }
+            case SendEventMessagesUseCase.ClientError: {
+              logger.info("Failed to send email(s) [client error]", { error: err });
+
+              return res.status(400).json({ message: "Failed to send email" });
+            }
+            case SendEventMessagesUseCase.NoOpError: {
+              logger.error("Sending emails aborted [no op]", { error: err });
+
+              return res.status(200).json({ message: "The event has been handled [no op]" });
+            }
+            default: {
+              logger.error("Failed to send email(s) [server error]", { error: err });
+              captureException(new Error("Unhandled useCase error", { cause: err }));
+
+              return res.status(500).json({ message: "Failed to send email [unhandled]" });
+            }
+          }
+        },
+      ),
+    );
 };
 
 export default withOtel(invoiceSentWebhook.createHandler(handler), "api/webhooks/invoice-sent");
