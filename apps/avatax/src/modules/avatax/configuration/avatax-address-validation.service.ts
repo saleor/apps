a6041cfac58d29f176adc3304bf04298713ee16a
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-05-22 10:25:20 +0200

    Improve Avatax parsing errors (#1358)

--- apps/avatax/src/modules/avatax/configuration/avatax-address-validation.service.ts
+++ apps/avatax/src/modules/avatax/configuration/avatax-address-validation.service.ts
@@ -1,23 +1,21 @@
+import * as Sentry from "@sentry/nextjs";
+import { fromPromise } from "neverthrow";
 import { avataxAddressFactory } from "../address-factory";
 import { AvataxClient } from "../avatax-client";
 import { AvataxConfig } from "../avatax-connection-schema";
-import { AvataxValidationErrorResolver } from "./avatax-validation-error-resolver";
-import { createLogger } from "../../../logger";
+import { AvataxErrorsParser } from "../avatax-errors-parser";
+
+const errorParser = new AvataxErrorsParser(Sentry.captureException);
 
 export class AvataxAddressValidationService {
-  private logger = createLogger("AvataxAddressValidationService");
-
   constructor(private avataxClient: AvataxClient) {}
 
   async validate(address: AvataxConfig["address"]) {
     const formattedAddress = avataxAddressFactory.fromChannelAddress(address);
 
-    try {
-      return this.avataxClient.validateAddress({ address: formattedAddress });
-    } catch (error) {
-      const errorResolver = new AvataxValidationErrorResolver();
-
-      throw errorResolver.resolve(error);
-    }
+    return fromPromise(
+      this.avataxClient.validateAddress({ address: formattedAddress }),
+      errorParser.parse,
+    );
   }
 }
