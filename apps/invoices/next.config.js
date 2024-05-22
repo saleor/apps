5ea6181672c8f15c7f10bb1d9ed425671afc8648
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-05-22 12:29:36 +0200

    Migrate packages & apps to ESM (#1360)

--- apps/invoices/next.config.js
+++ apps/invoices/next.config.js
@@ -1,4 +1,6 @@
-const { withSentryConfig } = require("@sentry/nextjs");
+// @ts-check
+
+import { withSentryConfig } from "@sentry/nextjs";
 
 const isSentryPropertiesInEnvironment = Boolean(
   process.env.SENTRY_AUTH_TOKEN && process.env.SENTRY_PROJECT && process.env.SENTRY_ORG,
@@ -25,20 +27,14 @@ const nextConfig = {
   },
 };
 
-const configWithSentry = withSentryConfig(
-  nextConfig,
-  {
-    silent: true,
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-  },
-  {
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    tunnelRoute: "/monitoring",
-    hideSourceMaps: true,
-    disableLogger: true,
-  },
-);
+const configWithSentry = withSentryConfig(nextConfig, {
+  silent: true,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+  disableLogger: true,
+});
 
-module.exports = isSentryPropertiesInEnvironment ? configWithSentry : nextConfig;
+export default isSentryPropertiesInEnvironment ? configWithSentry : nextConfig;
