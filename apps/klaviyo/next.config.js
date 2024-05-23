b8db6a7d9e68df28b2323796977b29a9eb863543
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-05-23 11:36:41 +0200

    Test klayvio deployment

--- apps/klaviyo/next.config.js
+++ apps/klaviyo/next.config.js
@@ -8,7 +8,13 @@ const isSentryPropertiesInEnvironment =
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@saleor/apps-shared", "@saleor/apps-ui", "@saleor/react-hook-form-macaw"],
+  transpilePackages: [
+    "@saleor/apps-shared",
+    "@saleor/apps-ui",
+    "@saleor/react-hook-form-macaw",
+    "@saleor/apps-logger",
+    "@saleor/apps-otel",
+  ],
   /*
    * Ignore opentelemetry warnings - https://github.com/open-telemetry/opentelemetry-js/issues/4173
    * Remove when https://github.com/open-telemetry/opentelemetry-js/pull/4660 is released
