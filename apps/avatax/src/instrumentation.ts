2475ecf5bddf6a3e5d9b987fef95413187f2e0c5
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-05-27 12:09:29 +0200

    add sentry check

--- apps/avatax/src/instrumentation.ts
+++ apps/avatax/src/instrumentation.ts
@@ -6,7 +6,7 @@ import { shouldExceptionLevelBeReported } from "./sentry-utils";
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
+  if (process.env.NEXT_RUNTIME === "nodejs" && SENTRY_DSN) {
     Sentry.init({
       dsn: SENTRY_DSN,
       enableTracing: false,
