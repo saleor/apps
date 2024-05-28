8cb3491b545c3e59351e11fa2f65dc4d80f86425
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-05-28 10:37:33 +0200

    wip

--- apps/avatax/src/modules/public-log-drain/public-log-drain.service.ts
+++ apps/avatax/src/modules/public-log-drain/public-log-drain.service.ts
@@ -1,3 +1,7 @@
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
+import { sharedOtelConfig } from "@saleor/apps-otel/src/shared-config";
+import { IResource } from "@opentelemetry/resources";
+
 export interface PublicLog<T extends Record<string, unknown> = {}> {
   message: string;
   eventType: string; // enum
@@ -28,6 +32,10 @@ export class PublicLogDrainService implements PublicLogDrain {
   emitLog(log: PublicLog): Promise<void> {
     return this.transporter.emit(log);
   }
+
+  getTransport() {
+    return this.transporter;
+  }
 }
 
 export class LogDrainJsonTransporter implements LogDrainTransporter {
@@ -41,14 +49,61 @@ export class LogDrainJsonTransporter implements LogDrainTransporter {
     return fetch(this.endpoint, {
       method: "POST",
       body: JSON.stringify(log),
-    });
+    })
+      .then((r) => r.json())
+      .then((res) => {
+        console.log(res); // todo
+
+        return;
+      });
   }
 
   setSettings() {}
 }
 
 export class LogDrainOtelTransporter implements LogDrainTransporter {
+  private otelExporter: OTLPLogExporter | null = null;
+
   async emit(log: PublicLog): Promise<void> {
-    throw new Error("Not implemented");
+    return new Promise((res, rej) => {
+      if (!this.otelExporter) {
+        throw new Error("Call setSettings first");
+      }
+
+      return this.otelExporter.export(
+        [
+          {
+            body: log.message,
+            attributes: log.attributes,
+            severityText: log.level,
+            hrTimeObserved: [0, 0],
+            hrTime: [0, 0],
+            resource: {
+              attributes: {},
+              merge(other: IResource | null): IResource {
+                return other as IResource; // todo
+              },
+            },
+            droppedAttributesCount: 0,
+            instrumentationScope: { name: "LogDrainOtelTransporter" },
+          },
+        ],
+        (cb) => {
+          if (cb.error) {
+            rej(cb.error);
+          } else {
+            res();
+          }
+        },
+      );
+    });
+  }
+
+  setSettings(settings: { url: string }) {
+    this.otelExporter = new OTLPLogExporter({
+      headers: sharedOtelConfig.exporterHeaders,
+      url: settings.url,
+      timeoutMillis: 2000,
+    });
   }
 }
