d6da7b7a30dd5fafd0bf40cf6772ff8edbd340bb
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-05-28 12:34:51 +0200

    full UI

--- apps/avatax/src/modules/ui/otel-section.tsx
+++ apps/avatax/src/modules/ui/otel-section.tsx
@@ -1,18 +1,21 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Button } from "@saleor/macaw-ui";
 import { Input } from "@saleor/react-hook-form-macaw";
-import { FormProvider, useForm } from "react-hook-form";
+import { FormProvider, useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
 import { AppCard } from "./app-card";
 import { Section } from "./app-section";
 
 const otelSectionSchema = z.object({
   url: z.string().url(),
+  headers: z.array(z.object({ key: z.string(), value: z.string() })),
 });
 
 type OtelConfig = z.infer<typeof otelSectionSchema>;
 
 const defaultOtelConfig: OtelConfig = {
   url: "",
+  headers: [],
 };
 
 export const OTELSection = () => {
@@ -23,6 +26,11 @@ export const OTELSection = () => {
 
   const { control } = formMethods;
 
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "headers",
+  });
+
   return (
     <>
       <Section.Description
@@ -32,9 +40,22 @@ export const OTELSection = () => {
           <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, minima quam?</>
         }
       />
-      <AppCard __minHeight={"320px"} height="100%" data-testid="otel-table">
+      <AppCard __minHeight={"320px"} height="100%" data-testid="otel-table" display="grid" gap={2}>
         <FormProvider {...formMethods}>
           <Input control={control} name="url" required label="Where to emit events" />
+          <Box title="Headers" width="100%" display="grid" gap={2}>
+            <Box>Add additional headers required by your OTEL collector.</Box>
+            <Box width="100%" display="grid" gap={3}>
+              {fields.map((field, index) => (
+                <Box display="flex" width="100%" justifyContent="space-between">
+                  <Input control={control} key={field.id} name={`headers.${index}.key`} />
+                  <Input control={control} key={field.id} name={`headers.${index}.value`} />
+                  <Button onClick={() => remove(index)}>Remove</Button>
+                </Box>
+              ))}
+            </Box>
+            <Button onClick={() => append({ key: "", value: "" })}>Add +</Button>
+          </Box>
         </FormProvider>
       </AppCard>
     </>
