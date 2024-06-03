531bdf429514594562b3d135e562da8404d7e784
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-06-03 13:04:47 +0200

    add import

---
+++ apps/avatax/src/instrumentation.sentry.ts
@@ -0,0 +1,17 @@
+import * as Sentry from "@sentry/nextjs";
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  enableTracing: false,
+  environment: process.env.ENV,
+  includeLocalVariables: true,
+  ignoreErrors: [],
+  integrations: [
+    Sentry.localVariablesIntegration({
+      captureAllExceptions: true,
+    }),
+    Sentry.extraErrorDataIntegration(),
+  ],
+});
--- apps/avatax/src/instrumentation.ts
+++ apps/avatax/src/instrumentation.ts
@@ -1,21 +1,5 @@
-import * as Sentry from "@sentry/nextjs";
-
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
-
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    Sentry.init({
-      dsn: SENTRY_DSN,
-      enableTracing: false,
-      environment: process.env.ENV,
-      includeLocalVariables: true,
-      ignoreErrors: [],
-      integrations: [
-        Sentry.localVariablesIntegration({
-          captureAllExceptions: true,
-        }),
-        Sentry.extraErrorDataIntegration(),
-      ],
-    });
+    await import("./instrumentation.sentry");
   }
 }
