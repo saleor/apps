0616b05469456ce765e0b6a52994016dcffbd993
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-06-03 12:07:36 +0200

    remove imports

--- apps/avatax/src/instrumentation.ts
+++ apps/avatax/src/instrumentation.ts
@@ -1,5 +1,4 @@
 import * as Sentry from "@sentry/nextjs";
-import { BaseError } from "./error";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
@@ -11,15 +10,6 @@ export async function register() {
       environment: process.env.ENV,
       includeLocalVariables: true,
       ignoreErrors: [],
-      beforeSend(errorEvent, hint) {
-        const error = hint.originalException;
-
-        if (error instanceof BaseError) {
-          errorEvent.fingerprint = ["{{ default }}", error.message];
-        }
-
-        return errorEvent;
-      },
       integrations: [
         Sentry.localVariablesIntegration({
           captureAllExceptions: true,
