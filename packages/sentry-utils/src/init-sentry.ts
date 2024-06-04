0cf9e04e632f44a82000c26662baf6b5b0413f88
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-06-04 15:40:37 +0200

    fix sentry import

--- packages/sentry-utils/src/init-sentry.ts
+++ packages/sentry-utils/src/init-sentry.ts
@@ -1,17 +1,17 @@
-import { extraErrorDataIntegration, init, localVariablesIntegration } from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs";
 
 export const initSentry = (dsn: string | undefined) => {
-  init({
+  Sentry.init({
     dsn,
     enableTracing: false,
     environment: process.env.ENV,
     includeLocalVariables: true,
     ignoreErrors: ["TRPCClientError"],
     integrations: [
-      localVariablesIntegration({
+      Sentry.localVariablesIntegration({
         captureAllExceptions: true,
       }),
-      extraErrorDataIntegration(),
+      Sentry.extraErrorDataIntegration(),
     ],
   });
 };
