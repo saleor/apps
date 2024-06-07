9a82701e55e1d51733fe04cfc8b3a0bdfd38e495
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-06-07 10:02:49 +0200

    test with next public & aliases

--- apps/avatax/src/instrumentation.ts
+++ apps/avatax/src/instrumentation.ts
@@ -1,5 +1,6 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs" && process.env.SENTRY_DSN) {
-    await import("./instrumentation.node");
+  if (process.env.NEXT_RUNTIME === "nodejs" && process.env.NEXT_PUBLIC_SENTRY_DSN) {
+    // @ts-expect-error
+    await import("@saleor/sentry-utils/node-instrumentation");
   }
 }
