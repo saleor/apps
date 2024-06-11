f7dccb0d6c110b13284f128c64dc0c14148eb763
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-06-11 14:32:26 +0200

    remove otel from calc tax

--- apps/avatax/src/pages/api/webhooks/checkout-calculate-taxes.ts
+++ apps/avatax/src/pages/api/webhooks/checkout-calculate-taxes.ts
@@ -1,4 +1,3 @@
-import { withOtel } from "@saleor/apps-otel";
 import * as Sentry from "@sentry/nextjs";
 import { captureException } from "@sentry/nextjs";
 import { createLogger } from "../../../logger";
@@ -33,107 +32,102 @@ const useCase = new CalculateTaxesUseCase({
  * TODO: Add tests to handler
  */
 export default wrapWithLoggerContext(
-  withOtel(
-    withMetadataCache(
-      checkoutCalculateTaxesSyncWebhook.createHandler(async (req, res, ctx) => {
-        try {
-          const { payload, authData } = ctx;
+  withMetadataCache(
+    checkoutCalculateTaxesSyncWebhook.createHandler(async (req, res, ctx) => {
+      try {
+        const { payload, authData } = ctx;
 
-          subscriptionErrorChecker.checkPayload(payload);
+        subscriptionErrorChecker.checkPayload(payload);
 
-          loggerContext.set("channelSlug", ctx.payload.taxBase.channel.slug);
-          loggerContext.set("checkoutId", ctx.payload.taxBase.sourceObject.id);
+        loggerContext.set("channelSlug", ctx.payload.taxBase.channel.slug);
+        loggerContext.set("checkoutId", ctx.payload.taxBase.sourceObject.id);
 
-          if (payload.version) {
-            Sentry.setTag(ObservabilityAttributes.SALEOR_VERSION, payload.version);
-            loggerContext.set(ObservabilityAttributes.SALEOR_VERSION, payload.version);
-          }
-
-          logger.info("Handler for CHECKOUT_CALCULATE_TAXES webhook called");
-
-          const appMetadata = payload.recipient?.privateMetadata ?? [];
-          const channelSlug = payload.taxBase.channel.slug;
-
-          const configExtractor = new AppConfigExtractor();
-
-          const config = configExtractor
-            .extractAppConfigFromPrivateMetadata(appMetadata)
-            .map((config) => {
-              try {
-                new AppConfigurationLogger(logger).logConfiguration(config, channelSlug);
-              } catch (e) {
-                captureException(
-                  new AppConfigExtractor.LogConfigurationMetricError(
-                    "Failed to log configuration metric",
-                    {
-                      cause: e,
-                    },
-                  ),
-                );
-              }
-
-              return config;
-            });
-
-          if (config.isErr()) {
-            logger.warn("Failed to extract app config from metadata", { error: config.error });
-
-            return res.status(400).send("App configuration is broken");
-          }
-
-          metadataCache.setMetadata(appMetadata);
-
-          return useCase.calculateTaxes(payload, authData).then((result) => {
-            return result.match(
-              (value) => {
-                return res.status(200).send(ctx.buildResponse(value));
-              },
-              (err) => {
-                logger.warn("Error calculating taxes", { error: err });
-
-                switch (err.constructor) {
-                  case CalculateTaxesUseCase.FailedCalculatingTaxesError: {
-                    return res.status(500).send("Failed to calculate taxes");
-                  }
-                  case CalculateTaxesUseCase.ConfigBrokenError: {
-                    return res
-                      .status(500)
-                      .send("Failed to calculate taxes due to invalid configuration");
-                  }
-                  case CalculateTaxesUseCase.ExpectedIncompletePayloadError: {
-                    return res
-                      .status(400)
-                      .send("Taxes cant be calculated due to incomplete payload");
-                  }
-                  case CalculateTaxesUseCase.UnhandledError: {
-                    captureException(err);
-
-                    return res.status(500).send("Failed to calculate taxes (Unhandled error)");
-                  }
-                }
-              },
-            );
-          });
-        } catch (error) {
-          // todo this should be now available in usecase. Catch it from FailedCalculatingTaxesError
-          if (error instanceof AvataxInvalidAddressError) {
-            logger.warn(
-              "InvalidAppAddressError: App returns status 400 due to broken address configuration",
-              { error },
-            );
-
-            return res.status(400).json({
-              message: "InvalidAppAddressError: Check address in app configuration",
-            });
-          }
-
-          Sentry.captureException(error);
-
-          return res.status(500).send("Unhandled error");
+        if (payload.version) {
+          Sentry.setTag(ObservabilityAttributes.SALEOR_VERSION, payload.version);
+          loggerContext.set(ObservabilityAttributes.SALEOR_VERSION, payload.version);
         }
-      }),
-    ),
-    "/api/webhooks/checkout-calculate-taxes",
+
+        logger.info("Handler for CHECKOUT_CALCULATE_TAXES webhook called");
+
+        const appMetadata = payload.recipient?.privateMetadata ?? [];
+        const channelSlug = payload.taxBase.channel.slug;
+
+        const configExtractor = new AppConfigExtractor();
+
+        const config = configExtractor
+          .extractAppConfigFromPrivateMetadata(appMetadata)
+          .map((config) => {
+            try {
+              new AppConfigurationLogger(logger).logConfiguration(config, channelSlug);
+            } catch (e) {
+              captureException(
+                new AppConfigExtractor.LogConfigurationMetricError(
+                  "Failed to log configuration metric",
+                  {
+                    cause: e,
+                  },
+                ),
+              );
+            }
+
+            return config;
+          });
+
+        if (config.isErr()) {
+          logger.warn("Failed to extract app config from metadata", { error: config.error });
+
+          return res.status(400).send("App configuration is broken");
+        }
+
+        metadataCache.setMetadata(appMetadata);
+
+        return useCase.calculateTaxes(payload, authData).then((result) => {
+          return result.match(
+            (value) => {
+              return res.status(200).send(ctx.buildResponse(value));
+            },
+            (err) => {
+              logger.warn("Error calculating taxes", { error: err });
+
+              switch (err.constructor) {
+                case CalculateTaxesUseCase.FailedCalculatingTaxesError: {
+                  return res.status(500).send("Failed to calculate taxes");
+                }
+                case CalculateTaxesUseCase.ConfigBrokenError: {
+                  return res
+                    .status(500)
+                    .send("Failed to calculate taxes due to invalid configuration");
+                }
+                case CalculateTaxesUseCase.ExpectedIncompletePayloadError: {
+                  return res.status(400).send("Taxes cant be calculated due to incomplete payload");
+                }
+                case CalculateTaxesUseCase.UnhandledError: {
+                  captureException(err);
+
+                  return res.status(500).send("Failed to calculate taxes (Unhandled error)");
+                }
+              }
+            },
+          );
+        });
+      } catch (error) {
+        // todo this should be now available in usecase. Catch it from FailedCalculatingTaxesError
+        if (error instanceof AvataxInvalidAddressError) {
+          logger.warn(
+            "InvalidAppAddressError: App returns status 400 due to broken address configuration",
+            { error },
+          );
+
+          return res.status(400).json({
+            message: "InvalidAppAddressError: Check address in app configuration",
+          });
+        }
+
+        Sentry.captureException(error);
+
+        return res.status(500).send("Unhandled error");
+      }
+    }),
   ),
   loggerContext,
 );
