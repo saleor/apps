455d1fb38ed1bd432754322e523e8906472645dc
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-06-11 10:29:25 +0200

    adjust vercel execution, fix trpc logger context

---
+++ .changeset/strange-lions-cheat.md
@@ -0,0 +1,5 @@
+---
+"smtp": patch
+---
+
+Fixed logging errors. Now error payload should be visible in logger.error() invocations
--- apps/smtp/src/pages/api/trpc/[trpc].ts
+++ apps/smtp/src/pages/api/trpc/[trpc].ts
@@ -3,21 +3,26 @@ import * as trpcNext from "@trpc/server/adapters/next";
 import { createLogger } from "../../../logger";
 import { appRouter } from "../../../modules/trpc/trpc-app-router";
 import { createTrpcContext } from "../../../modules/trpc/trpc-context";
+import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
+import { loggerContext } from "../../../logger-context";
 
 const logger = createLogger("tRPC wrapper");
 
-export default withOtel(
-  trpcNext.createNextApiHandler({
-    router: appRouter,
-    createContext: createTrpcContext,
-    onError: ({ path, error }) => {
-      if (error.code === "INTERNAL_SERVER_ERROR") {
-        logger.error(`${path} returned error:`, { error });
+export default wrapWithLoggerContext(
+  withOtel(
+    trpcNext.createNextApiHandler({
+      router: appRouter,
+      createContext: createTrpcContext,
+      onError: ({ path, error }) => {
+        if (error.code === "INTERNAL_SERVER_ERROR") {
+          logger.error(`${path} returned error:`, { error });
 
-        return;
-      }
-      logger.debug(`${path} returned error:`, { error });
-    },
-  }),
-  "/api/trpc",
+          return;
+        }
+        logger.debug(`${path} returned error:`, { error });
+      },
+    }),
+    "/api/trpc",
+  ),
+  loggerContext,
 );
