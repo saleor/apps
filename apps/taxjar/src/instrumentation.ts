ef3caea173d4ac42fd4a966eed3b34d5d36c3179
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-06-11 12:04:57 +0200

    restore sentry to v7

--- apps/taxjar/src/instrumentation.ts
+++
@@ -1,6 +0,0 @@
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs" && process.env.NEXT_PUBLIC_SENTRY_DSN) {
-    // @ts-expect-error - TypeScript is not aware of this dynamic import - it is working properly on dev and build
-    await import("@saleor/sentry-utils/node/instrumentation");
-  }
-}
