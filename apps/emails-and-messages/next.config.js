1d5a67900a2aca30e093cf6894ec8e11b77a0fd1
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-06-11 12:10:21 +0200

    disable instrumentation hook

--- apps/emails-and-messages/next.config.js
+++ apps/emails-and-messages/next.config.js
@@ -16,9 +16,7 @@ const nextConfig = {
     "@saleor/react-hook-form-macaw",
     "@saleor/sentry-utils",
   ],
-  experimental: {
-    instrumentationHook: true,
-  },
+  experimental: {},
   /*
    * Ignore opentelemetry warnings - https://github.com/open-telemetry/opentelemetry-js/issues/4173
    * Remove when https://github.com/open-telemetry/opentelemetry-js/pull/4660 is released
