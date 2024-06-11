61c6ab13087c1163dd13ca2b0919a5439d13bbc3
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-06-11 14:57:25 +0200

    Remove temporary sentry error

--- apps/avatax/src/pages/api/webhooks/order-calculate-taxes.ts
+++ apps/avatax/src/pages/api/webhooks/order-calculate-taxes.ts
@@ -111,10 +111,6 @@ export default wrapWithLoggerContext(
 
             logger.info("Taxes calculated", { calculatedTaxes });
 
-            if (payload.taxBase?.lines[0].quantity === 10) {
-              Sentry.captureException(new Error("TEST ERROR"));
-            }
-
             return res.status(200).json(ctx.buildResponse(calculatedTaxes));
           } else if (avataxWebhookServiceResult.isErr()) {
             const err = avataxWebhookServiceResult.error;
