ca72d9eeb708c747b149f92f4bf42be555ec4be9
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-06-13 17:43:42 +0200

    [vercel only app-avatax] change env

--- apps/avatax/sentry.server.config.ts
+++ apps/avatax/sentry.server.config.ts
@@ -1,1 +1,15 @@
-import "@saleor/sentry-utils/node/instrumentation";
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enableTracing: false,
+  environment: process.env.ENV,
+  includeLocalVariables: true,
+  release: process.env.SENTRY_RELEASE,
+  integrations: [
+    Sentry.localVariablesIntegration({
+      captureAllExceptions: true,
+    }),
+    Sentry.extraErrorDataIntegration(),
+  ],
+});
