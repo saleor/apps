efd7ff8aa929a37c0c36e460b02a412ced84e714
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-06-14 17:41:57 +0200

    [vercel only smtp]

---
+++ apps/smtp/sentry.edge.config.ts
@@ -0,0 +1,13 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  enableTracing: false,
+  environment: process.env.ENV,
+  includeLocalVariables: true,
+  integrations: [
+    new Sentry.Integrations.LocalVariables({
+      captureAllExceptions: true,
+    }),
+  ],
+});
