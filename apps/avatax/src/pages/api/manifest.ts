db448d33785ba619bbfefcdcc3c71cd4b7723c35
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-06-14 07:59:13 +0200

    Update manifest.ts

--- apps/avatax/src/pages/api/manifest.ts
+++ apps/avatax/src/pages/api/manifest.ts
@@ -7,7 +7,6 @@ import packageJson from "../../../package.json";
 import { REQUIRED_SALEOR_VERSION } from "../../../saleor-app";
 import { appWebhooks } from "../../../webhooks";
 import { loggerContext } from "../../logger-context";
-import { captureException } from "@sentry/nextjs";
 
 export default wrapWithLoggerContext(
   withOtel(
@@ -15,8 +14,6 @@ export default wrapWithLoggerContext(
       async manifestFactory({ appBaseUrl }) {
         const iframeBaseUrl = process.env.APP_IFRAME_BASE_URL ?? appBaseUrl;
         const apiBaseURL = process.env.APP_API_BASE_URL ?? appBaseUrl;
-
-        captureException(new Error("test"));
 
         const manifest: AppManifest = {
           about: "App connects with Avatax to dynamically calculate taxes",
