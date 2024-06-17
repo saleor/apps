188a12207da87c3a27acbc5c3d4f3c45b85f6167
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-06-17 14:03:26 +0200

    Remove OTEL from migration

--- apps/avatax/scripts/logger.ts
+++ apps/avatax/scripts/logger.ts
@@ -1,15 +1,8 @@
 // eslint-disable-next-line no-restricted-imports
 import { attachLoggerConsoleTransport, createLogger, logger } from "@saleor/apps-logger";
-import { attachLoggerOtelTransport } from "@saleor/apps-logger/node";
-import packageJson from "../package.json";
-import { loggerContext } from "../src/logger-context";
 
 logger.settings.maskValuesOfKeys = ["username", "password", "token"];
 
-if (process.env.DANGEROUS_ENABLE_MIGRATION_CONSOLE_LOGGER === "true") {
-  attachLoggerConsoleTransport(logger);
-}
-
-attachLoggerOtelTransport(logger, packageJson.version, loggerContext);
+attachLoggerConsoleTransport(logger);
 
 export { createLogger, logger };
