6516ce2a91403c4efd2b40cc50cdf0254418c077
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-06-18 14:58:35 +0200

    [vercel only smtp] test deploy without waituntil

--- apps/smtp/src/pages/api/test3.ts
+++ apps/smtp/src/pages/api/test3.ts
@@ -3,9 +3,12 @@ import { createLogger } from "../../logger";
 import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
 import { loggerContext } from "../../logger-context";
 import { withOtel } from "@saleor/apps-otel";
+import { captureException } from "@sentry/nextjs";
 
 const handler: NextApiHandler = async (req, res) => {
   createLogger("logger test3").info("log test3");
+
+  captureException(new Error("testing sentry"));
 
   res.status(200).send("ok");
 };
