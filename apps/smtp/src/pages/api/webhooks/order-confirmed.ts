dbd1c8a19121aec0702a2be391315b70e25282f1
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-06-20 09:32:47 +0200

    use only factory [vercel only smtp]

--- apps/smtp/src/pages/api/webhooks/order-confirmed.ts
+++ apps/smtp/src/pages/api/webhooks/order-confirmed.ts
@@ -1,7 +1,6 @@
 import { NextWebhookApiHandler, SaleorAsyncWebhook } from "@saleor/app-sdk/handlers/next";
 import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
 import { withOtel } from "@saleor/apps-otel";
-import { ObservabilityAttributes } from "@saleor/apps-otel/src/lib/observability-attributes";
 import { gql } from "urql";
 import {
   OrderConfirmedWebhookPayloadFragment,
@@ -48,30 +47,7 @@ const handler: NextWebhookApiHandler<OrderConfirmedWebhookPayloadFragment> = asy
 
   const useCaseFactory = new SendEventMessagesUseCaseFactory();
 
-  const { payload, authData } = context;
-  const { order } = payload;
-
-  if (!order) {
-    logger.error("No order data payload");
-    return res.status(200).end();
-  }
-
-  const recipientEmail = order.userEmail || order.user?.email;
-
-  if (!recipientEmail?.length) {
-    logger.error(`The order ${order.number} had no email recipient set. Aborting.`);
-    return res
-      .status(200)
-      .json({ error: "Email recipient has not been specified in the event payload." });
-  }
-
-  const channel = order.channel.slug;
-
-  loggerContext.set(ObservabilityAttributes.CHANNEL_SLUG, channel);
-
-  const useCase = useCaseFactory.createFromAuthData(authData);
-
-  logger.info(`Webhook received - for order confirmed event - ${useCase}`);
+  logger.info(`Webhook received - for order confirmed event - useCaseFactory: ${useCaseFactory}`);
 
   return res.status(200).json({ message: "The event has been handled" });
 };
