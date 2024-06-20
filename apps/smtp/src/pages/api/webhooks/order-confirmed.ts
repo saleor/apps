7f58ad00538040913f7ceace0e6b20fabe65b022
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-06-20 09:14:55 +0200

    debug SMTP - remove usecase [vercel only smtp]

--- apps/smtp/src/pages/api/webhooks/order-confirmed.ts
+++ apps/smtp/src/pages/api/webhooks/order-confirmed.ts
@@ -1,8 +1,6 @@
 import { NextWebhookApiHandler, SaleorAsyncWebhook } from "@saleor/app-sdk/handlers/next";
 import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
 import { withOtel } from "@saleor/apps-otel";
-import { ObservabilityAttributes } from "@saleor/apps-otel/src/lib/observability-attributes";
-import { captureException } from "@sentry/nextjs";
 import { gql } from "urql";
 import {
   OrderConfirmedWebhookPayloadFragment,
@@ -10,8 +8,6 @@ import {
 } from "../../../../generated/graphql";
 import { createLogger } from "../../../logger";
 import { loggerContext } from "../../../logger-context";
-import { SendEventMessagesUseCase } from "../../../modules/event-handlers/use-case/send-event-messages.use-case";
-import { SendEventMessagesUseCaseFactory } from "../../../modules/event-handlers/use-case/send-event-messages.use-case.factory";
 import { saleorApp } from "../../../saleor-app";
 
 const OrderConfirmedWebhookPayload = gql`
@@ -48,81 +44,9 @@ const handler: NextWebhookApiHandler<OrderConfirmedWebhookPayloadFragment> = asy
 ) => {
   const logger = createLogger(orderConfirmedWebhook.webhookPath);
 
-  const useCaseFactory = new SendEventMessagesUseCaseFactory();
+  logger.info("Webhook received - for order confirmed event");
 
-  logger.info("Webhook received");
-
-  const { payload, authData } = context;
-  const { order } = payload;
-
-  if (!order) {
-    logger.error("No order data payload");
-    return res.status(200).end();
-  }
-
-  const recipientEmail = order.userEmail || order.user?.email;
-
-  if (!recipientEmail?.length) {
-    logger.error(`The order ${order.number} had no email recipient set. Aborting.`);
-    return res
-      .status(200)
-      .json({ error: "Email recipient has not been specified in the event payload." });
-  }
-
-  const channel = order.channel.slug;
-
-  loggerContext.set(ObservabilityAttributes.CHANNEL_SLUG, channel);
-
-  const useCase = useCaseFactory.createFromAuthData(authData);
-
-  try {
-    return useCase
-      .sendEventMessages({
-        channelSlug: channel,
-        event: "ORDER_CONFIRMED",
-        payload: { order: payload.order },
-        recipientEmail,
-      })
-      .then((result) =>
-        result.match(
-          (r) => {
-            logger.info("Successfully sent email(s)");
-
-            return res.status(200).json({ message: "The event has been handled" });
-          },
-          (err) => {
-            const errorInstance = err[0];
-
-            if (errorInstance instanceof SendEventMessagesUseCase.ServerError) {
-              logger.error("Failed to send email(s) [server error]", { error: err });
-
-              return res.status(500).json({ message: "Failed to send email" });
-            } else if (errorInstance instanceof SendEventMessagesUseCase.ClientError) {
-              logger.info("Failed to send email(s) [client error]", { error: err });
-
-              return res.status(400).json({ message: "Failed to send email" });
-            } else if (errorInstance instanceof SendEventMessagesUseCase.NoOpError) {
-              logger.info("Sending emails aborted [no op]", { error: err });
-
-              return res.status(200).json({ message: "The event has been handled [no op]" });
-            }
-
-            logger.error("Failed to send email(s) [unhandled error]", { error: err });
-            captureException(new Error("Unhandled useCase error", { cause: err }));
-
-            return res.status(500).json({ message: "Failed to send email [unhandled]" });
-          },
-        ),
-      );
-  } catch (e) {
-    logger.error("Unhandled error from useCase", {
-      error: e,
-    });
-
-    captureException(e);
-
-    return res.status(500).json({ message: "Failed to execute webhook" });
-  }
+  return res.status(200).json({ message: "The event has been handled" });
 };
 
 export default wrapWithLoggerContext(
