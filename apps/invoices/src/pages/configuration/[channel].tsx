b5d8698d01eaaa39a6ec49637b73637db79cd38e
Paweł Chyła <pawel.chyla@saleor.io>
2024-08-02 12:23:39 +0200

    Remove Invoices app from monorepo (#1496)
    * Remove invoices app
    
    * Remove invoices from lint
    
    * Fix lint sherif script

--- apps/invoices/src/pages/configuration/[channel].tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import { ChannelConfigView } from "../../modules/app-configuration/views/channel-config.view";
-
-const ChannelConfigPage: NextPage = () => {
-  return <ChannelConfigView />;
-};
-
-export default ChannelConfigPage;
