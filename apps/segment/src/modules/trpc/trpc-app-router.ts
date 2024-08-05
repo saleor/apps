c51cd36ebde19db901f21e2412de161f5a2735f7
Paweł Chyła <pawel.chyla@saleor.io>
2024-08-05 10:05:23 +0200

    Remove Segment app from monorepo (#1502)
    * Remove segment
    
    * Update lock

--- apps/segment/src/modules/trpc/trpc-app-router.ts
+++
@@ -1,8 +0,0 @@
-import { configurationRouter } from "../configuration/configuration.router";
-import { router } from "./trpc-server";
-
-export const appRouter = router({
-  configuration: configurationRouter,
-});
-
-export type AppRouter = typeof appRouter;
