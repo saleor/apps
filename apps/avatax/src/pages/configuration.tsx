f9f4773e021787ceaac78dd3fc9c104f70854035
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-08-06 12:33:21 +0200

    Update Avatax to AvaTax (#1507)

--- apps/avatax/src/pages/configuration.tsx
+++ apps/avatax/src/pages/configuration.tsx
@@ -1,15 +1,15 @@
 import { useAppBridge } from "@saleor/app-sdk/app-bridge";
+import { Text } from "@saleor/macaw-ui";
 import { ChannelSection } from "../modules/channel-configuration/ui/channel-section";
 import { ProvidersSection } from "../modules/provider-connections/ui/providers-section";
 import { AppPageLayout } from "../modules/ui/app-page-layout";
 import { Section } from "../modules/ui/app-section";
 import { MatcherSection } from "../modules/ui/matcher-section";
-import { Text } from "@saleor/macaw-ui";
 
 const Header = () => {
   return (
     <Section.Header>
-      Configure the app by connecting to Avatax. You can connect to multiple accounts.
+      Configure the app by connecting to AvaTax. You can connect to multiple accounts.
     </Section.Header>
   );
 };
