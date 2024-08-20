7169f67613e6006ee15991b6eb44470a8e14d542
andrzejewsky <vox3r69@gmail.com>
2024-08-20 11:14:50 +0200

    Add logger

--- apps/smtp/src/modules/smtp/configuration/smtp-metadata-manager.ts
+++ apps/smtp/src/modules/smtp/configuration/smtp-metadata-manager.ts
@@ -13,7 +13,7 @@ import { SmtpConfig } from "./smtp-config-schema";
  *Maximum that call can take up to 1.5s, so we've set timeout to 3s as a safe margin,
  *anything longer should be considered as SLA violation
  */
-const PULL_CONFG_TIMEOUT = 100;
+const PULL_CONFG_TIMEOUT = 3000;
 
 // todo test
 export class SmtpMetadataManager {
