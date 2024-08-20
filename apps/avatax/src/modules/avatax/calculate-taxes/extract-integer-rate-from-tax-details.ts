6153c0c721c8dc2e75a7943e20c221dd3f25c1db
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-08-20 10:01:06 +0200

    Fix decimal places raporting in AvaTax

--- apps/avatax/src/modules/avatax/calculate-taxes/extract-integer-rate-from-tax-details.ts
+++ apps/avatax/src/modules/avatax/calculate-taxes/extract-integer-rate-from-tax-details.ts
@@ -1,11 +1,14 @@
-import { TransactionLineDetailModel } from "avatax/lib/models/TransactionLineDetailModel";
+const MAX_AVATAX_DECIMAL_PLACES = 6;
 
 /**
  * Saleor expects tax rate to be returned in integer value
  * https://docs.saleor.io/docs/3.x/developer/extending/webhooks/synchronous-events/tax#fields-1
  */
-export function extractIntegerRateFromTaxDetails(details: TransactionLineDetailModel[]) {
-  const rawSum = details.reduce((acc, detailModel) => acc + (detailModel.rate ?? 0), 0) * 100;
+export function extractIntegerRateFromTaxDetailsRates(
+  rates: Array<number | undefined> | undefined = [],
+) {
+  const rawSum =
+    rates.filter((num) => num !== undefined).reduce((acc, rate) => acc + rate, 0) * 100;
 
-  return parseFloat(rawSum.toPrecision(2));
+  return parseFloat(rawSum.toPrecision(MAX_AVATAX_DECIMAL_PLACES));
 }
