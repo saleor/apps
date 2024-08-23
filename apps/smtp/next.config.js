7a95395680aa7fb997f8b001ab2fecdc37bbef34
andrzejewsky <vox3r69@gmail.com>
2024-08-23 10:02:26 +0200

    Add bundle externals setting

--- apps/smtp/next.config.js
+++ apps/smtp/next.config.js
@@ -18,10 +18,13 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: [
       /*
-       *It solves the issue with 'require' and handlebars-helpers, when bundlePagesExternals is enabled.
-       *more info: https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
+       * The deps below are have node-related features. When the flag "bundlePagesExternals" is enabled, They raise errors,
+       * So we must explicitly declare them as externals. 
+       * more info: https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
        */
-      "handlebars-helpers"
+      "handlebars",
+      "handlebars-helpers",
+      "mjml",
     ],
     bundlePagesExternals: true,
   },
@@ -34,6 +37,13 @@ const nextConfig = {
     if (isServer) {
       config.ignoreWarnings = [{ module: /opentelemetry/ }];
     }
+
+    /*
+     *When the flag "bundlePagesExternals" is enabled, handlebars cannot be properly loaded,
+     *so we must explicitly declare what file we load.
+     */
+    config.resolve.alias.handlebars = "handlebars/dist/handlebars.js";
+
     return config;
   },
 };
