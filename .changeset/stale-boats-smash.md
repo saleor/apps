b487edb2b9db855fd60e8e78ac2aa966c0cf468c
Patryk Andrzejewski <vox3r69@gmail.com>
2024-08-23 10:17:30 +0200

    Add bundle externals flag (#1531)
    * Add bundle externals setting
    
    * Create stale-boats-smash.md
    
    * Update stale-boats-smash.md
    
    * Add bundle externals setting
    
    * Add bundle externals setting
    
    * Add bundle externals setting

---
+++ .changeset/stale-boats-smash.md
@@ -0,0 +1,5 @@
+---
+"smtp": patch
+---
+
+Now, the flag "bundlePagesExternals" was added, which means cold-starts should be decreased according to Vercel documentation.
--- apps/smtp/next.config.js
+++ apps/smtp/next.config.js
@@ -13,9 +13,21 @@ const nextConfig = {
     "@saleor/apps-logger",
     "@saleor/apps-shared",
     "@saleor/apps-ui",
-    "@saleor/react-hook-form-macaw",
+    "@saleor/react-hook-form-macaw"
   ],
-  experimental: {},
+  experimental: {
+    serverComponentsExternalPackages: [
+      /*
+       * The deps below are have node-related features. When the flag "bundlePagesExternals" is enabled, They raise errors,
+       * So we must explicitly declare them as externals. 
+       * more info: https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
+       */
+      "handlebars",
+      "handlebars-helpers",
+      "mjml",
+    ],
+    bundlePagesExternals: true,
+  },
   /*
    * Ignore opentelemetry warnings - https://github.com/open-telemetry/opentelemetry-js/issues/4173
    * Remove when https://github.com/open-telemetry/opentelemetry-js/pull/4660 is released
@@ -25,6 +37,13 @@ const nextConfig = {
     if (isServer) {
       config.ignoreWarnings = [{ module: /opentelemetry/ }];
     }
+
+    /*
+     *When the flag "bundlePagesExternals" is enabled, handlebars cannot be properly loaded,
+     *so we must explicitly declare what file we load.
+     */
+    config.resolve.alias.handlebars = "handlebars/dist/handlebars.js";
+
     return config;
   },
 };
