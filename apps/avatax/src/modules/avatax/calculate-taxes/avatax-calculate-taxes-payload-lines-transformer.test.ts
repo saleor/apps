635cd87f0b842dd3ac707742db4d82d87f24959c
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-09-02 09:42:31 +0200

    TDD for new extracted transformWithDiscountType method

--- apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-payload-lines-transformer.test.ts
+++ apps/avatax/src/modules/avatax/calculate-taxes/avatax-calculate-taxes-payload-lines-transformer.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, test } from "vitest";
 
 import { DEFAULT_TAX_CLASS_ID } from "../constants";
 import { AutomaticallyDistributedDiscountsStrategy } from "../discounts";
@@ -10,6 +10,10 @@ const transformer = new AvataxCalculateTaxesPayloadLinesTransformer();
 const discountsStrategy = new AutomaticallyDistributedDiscountsStrategy();
 
 describe("AvataxCalculateTaxesPayloadLinesTransformer", () => {
+  /**
+   * TODO: Remove this suite once transformWithDiscountType will be used,
+   * all tests have been moved there
+   */
   describe("transform", () => {
     it("maps lines, adds shipping as line and maps the tax code of one product", () => {
       const mockGenerator = new AvataxCalculateTaxesMockGenerator();
@@ -93,7 +97,6 @@ describe("AvataxCalculateTaxesPayloadLinesTransformer", () => {
         },
       ]);
     });
-
     it("should add discounted flag to lines when discounts are applied", () => {
       const mockGenerator = new AvataxCalculateTaxesMockGenerator("withDiscounts");
       const avataxConfigMock = mockGenerator.generateAvataxConfig();
@@ -137,6 +140,162 @@ describe("AvataxCalculateTaxesPayloadLinesTransformer", () => {
           discounted: true,
         },
       ]);
+    });
+  });
+
+  describe("transformWithDiscountType", () => {
+    it("maps lines, adds shipping as line and maps the tax code of one product", () => {
+      const mockGenerator = new AvataxCalculateTaxesMockGenerator();
+      const avataxConfigMock = mockGenerator.generateAvataxConfig();
+      const taxBaseMock = mockGenerator.generateTaxBase();
+      const matchesMock = mockGenerator.generateTaxCodeMatches();
+
+      const lines = transformer.transform(
+        taxBaseMock,
+        avataxConfigMock,
+        matchesMock,
+        discountsStrategy,
+      );
+
+      expect(lines).toEqual([
+        {
+          amount: 60,
+          quantity: 3,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: false,
+        },
+        {
+          amount: 20,
+          quantity: 1,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: false,
+        },
+        {
+          amount: 100,
+          quantity: 2,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: false,
+        },
+        {
+          amount: 48.33,
+          itemCode: "Shipping",
+          quantity: 1,
+          taxCode: "FR000000",
+          taxIncluded: true,
+          discounted: false,
+        },
+      ]);
+    });
+    it("when no shipping in tax base, does not add shipping as line", () => {
+      const mockGenerator = new AvataxCalculateTaxesMockGenerator();
+      const avataxConfigMock = mockGenerator.generateAvataxConfig();
+      const matchesMock = mockGenerator.generateTaxCodeMatches();
+      const taxBaseMock = mockGenerator.generateTaxBase({ shippingPrice: { amount: 0 } });
+
+      const lines = transformer.transform(
+        taxBaseMock,
+        avataxConfigMock,
+        matchesMock,
+        discountsStrategy,
+      );
+
+      expect(lines).toEqual([
+        {
+          amount: 60,
+          quantity: 3,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: false,
+        },
+        {
+          amount: 20,
+          quantity: 1,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: false,
+        },
+        {
+          amount: 100,
+          quantity: 2,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: false,
+        },
+      ]);
+    });
+    it("should add discounted flag to lines when discounts are applied", () => {
+      const mockGenerator = new AvataxCalculateTaxesMockGenerator("withDiscounts");
+      const avataxConfigMock = mockGenerator.generateAvataxConfig();
+      const taxBaseMock = mockGenerator.generateTaxBase();
+      const matchesMock = mockGenerator.generateTaxCodeMatches();
+      const lines = transformer.transform(
+        taxBaseMock,
+        avataxConfigMock,
+        matchesMock,
+        discountsStrategy,
+      );
+
+      expect(lines).toEqual([
+        {
+          amount: 60,
+          quantity: 3,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: true,
+        },
+        {
+          amount: 20,
+          quantity: 1,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: true,
+        },
+        {
+          amount: 100,
+          quantity: 2,
+          taxCode: DEFAULT_TAX_CLASS_ID,
+          taxIncluded: true,
+          discounted: true,
+        },
+        {
+          amount: 48.33,
+          itemCode: "Shipping",
+          quantity: 1,
+          taxCode: "FR000000",
+          taxIncluded: true,
+          discounted: true,
+        },
+      ]);
+    });
+
+    describe("Both SHIPPING and SUBTOTAL discounts types are provided", () => {
+      it.todo(
+        "Subtotal value will be mapped to DISCOUNTED. Shipping will NOT be discounted, but its value will be reduced. Case with single discounts.",
+      );
+      it.todo(
+        "Subtotal value will be mapped to DISCOUNTED. Shipping will NOT be discounted, but its value will be reduced. Case with multiple discounts.",
+      );
+    });
+
+    describe("Only SUBTOTAL type discount is provided", () => {
+      it.todo(
+        "Subtotal value will be mapped to DISCOUNTED. Shipping will not be reduced and not marked as discounted. Case with single SUBTOTAL discount",
+      );
+      it.todo(
+        "Subtotal value will be mapped to DISCOUNTED. Shipping will not be reduced and not marked as discounted. Case with multiple SUBTOTAL discounts",
+      );
+    });
+
+    describe("Only SHIPPING type discount is provided", () => {
+      it.todo(
+        "Subtotal value will not be reduced and not marked as discounted. Shipping value will reduced but not discounted. Case with single SHIPPING discount",
+      );
+      it.todo(
+        "Subtotal value will not be reduced and not marked as discounted. Shipping value will reduced but not discounted. Case with multiple SHIPPING discounts",
+      );
     });
   });
 });
