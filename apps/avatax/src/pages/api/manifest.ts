9a440fd179d1aa71470907f86cb0f88692cb5c2c
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-09-27 10:04:23 +0200

    Remove version checking on AvaTax register endpoint (#1592)

--- apps/avatax/src/pages/api/manifest.ts
+++ apps/avatax/src/pages/api/manifest.ts
@@ -4,7 +4,6 @@ import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
 import { withOtel } from "@saleor/apps-otel";
 
 import packageJson from "../../../package.json";
-import { REQUIRED_SALEOR_VERSION } from "../../../saleor-app";
 import { appWebhooks } from "../../../webhooks";
 import { loggerContext } from "../../logger-context";
 
@@ -30,7 +29,7 @@ export default wrapWithLoggerContext(
           id: "saleor.app.avatax",
           name: "AvaTax",
           permissions: ["HANDLE_TAXES", "MANAGE_ORDERS"],
-          requiredSaleorVersion: REQUIRED_SALEOR_VERSION,
+          requiredSaleorVersion: ">=3.19 <4",
           supportUrl: "https://github.com/saleor/apps/discussions",
           tokenTargetUrl: `${apiBaseURL}/api/register`,
           version: packageJson.version,
