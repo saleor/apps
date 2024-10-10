a90a73c7dccc2fb317ccfa0a027d5a0f8102f3ac
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-10-10 10:38:49 +0200

    rewrite events and mutations

---
+++ apps/avatax/graphql/subscriptions/OrderCancelled.ts
@@ -0,0 +1,46 @@
+import { graphql } from "@/graphql";
+
+import { WebhookMetadata } from "../fragments/WebhookMetadata";
+
+const OrderCancelledSubscriptionFragment = graphql(`
+  fragment OrderCancelledSubscription on Order {
+    id
+    avataxId: metafield(key: "avataxId")
+    channel {
+      id
+      slug
+    }
+  }
+`);
+
+const OrderCancelledEventSubscriptionFragment = graphql(
+  `
+    fragment OrderCancelledEventSubscription on Event {
+      __typename
+      ...WebhookMetadata
+      ... on OrderCancelled {
+        order {
+          ...OrderCancelledSubscription
+        }
+        recipient {
+          privateMetadata {
+            key
+            value
+          }
+        }
+      }
+    }
+  `,
+  [WebhookMetadata, OrderCancelledSubscriptionFragment],
+);
+
+export const OrderCancelledSubscription = graphql(
+  `
+    subscription OrderCancelledSubscription {
+      event {
+        ...OrderCancelledEventSubscription
+      }
+    }
+  `,
+  [OrderCancelledEventSubscriptionFragment],
+);
