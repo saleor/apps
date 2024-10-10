5d366df916f6e6627e908ed46c8132983ab640e6
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-10-10 13:52:33 +0200

    Check logger again

--- apps/avatax/src/logger.ts
+++ apps/avatax/src/logger.ts
@@ -40,12 +40,12 @@ logger.attachTransport((log) => {
 
   const bodyMessage = log._meta.name ? `[${log._meta.name}] ${message}` : message;
 
-  const formattedStuff = JSON.stringify({
+  const formattedStuff = {
     message: bodyMessage,
     ...attributes,
     ...loggerContext.getRawContext(),
     _meta,
-  });
+  };
 
   if (_meta.logLevelName === "ERROR") {
     console.error(formattedStuff);
