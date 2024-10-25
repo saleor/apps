151e86f1a0a818b9965a0823c6f1dc2e243e2ba6
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-10-25 15:05:42 +0200

    Bring back log attributes

--- packages/logger/src/logger-vercel-build-time-transport.ts
+++ packages/logger/src/logger-vercel-build-time-transport.ts
@@ -2,20 +2,21 @@ import { ILogObj, Logger } from "tslog";
 
 export const attachLoggerVercelBuildTimeTransport = (logger: Logger<ILogObj>) => {
   logger.attachTransport((log) => {
-    const { message, _meta } = log;
+    const { message, _meta, attributes } = log;
 
     const logMessage = log._meta.name ? `[${log._meta.name}] ${message}` : message;
+    const logAttributes = JSON.stringify(attributes, null, 2);
 
     if (_meta.logLevelName === "ERROR") {
-      console.error(logMessage);
+      console.error(logMessage, logAttributes);
       return;
     }
 
     if (_meta.logLevelName === "WARN") {
-      console.warn(logMessage);
+      console.warn(logMessage, logAttributes);
       return;
     }
 
-    console.log(logMessage);
+    console.log(logMessage, logAttributes);
   });
 };
