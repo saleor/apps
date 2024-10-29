a924651dcd129f04eda8a4be26fa95406ba17558
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-10-29 10:30:31 +0100

    remove isactive and use enabled

--- apps/avatax/scripts/run-webhooks-migration.ts
+++ apps/avatax/scripts/run-webhooks-migration.ts
@@ -77,7 +77,7 @@ const runMigrations = async () => {
           const baseUrl = new URL(targetUrl).origin;
 
           // All webhooks in this application are turned on or off. If any of them is enabled, we enable all of them.
-          return appWebhooks.map((w) => ({ ...w.getWebhookManifest(baseUrl), isActive: enabled }));
+          return appWebhooks.map((w) => ({ ...w.getWebhookManifest(baseUrl), enabled }));
         },
       });
 
