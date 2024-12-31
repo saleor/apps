d1cc0685f8d869080659f7bb29325806b39e27b2
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-12-31 10:18:10 +0100

    Add Sentry to Segment app

--- apps/segment/src/modules/create-segment-client-for-webhook-context.ts
+++ apps/segment/src/modules/create-segment-client-for-webhook-context.ts
@@ -1,10 +1,12 @@
 import { AuthData } from "@saleor/app-sdk/APL";
 
-import { SegmentNotConfiguredError } from "@/errors";
+import { BaseError } from "@/errors";
 
 import { AppConfigMetadataManager } from "./configuration/app-config-metadata-manager";
 import { SegmentClient } from "./segment/segment.client";
 import { SegmentEventsTracker } from "./tracking-events/segment-events-tracker";
+
+export const SegmentNotConfiguredError = BaseError.subclass("SegmentNotConfiguredError");
 
 export const createSegmentClientForWebhookContext = async (context: { authData: AuthData }) => {
   const config = await AppConfigMetadataManager.createFromAuthData(context.authData).get();
@@ -12,7 +14,7 @@ export const createSegmentClientForWebhookContext = async (context: { authData: 
   const segmentKey = config.getConfig()?.segmentWriteKey;
 
   if (!segmentKey) {
-    throw new SegmentNotConfiguredError();
+    throw new SegmentNotConfiguredError("Segment write key not found in app config");
   }
 
   return new SegmentEventsTracker(
