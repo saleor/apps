17df1fc68366cb0bb6d8f240db110e24f1d0e86c
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2024-12-31 09:11:20 +0100

    Update env

--- apps/segment/src/env.ts
+++ apps/segment/src/env.ts
@@ -29,7 +29,7 @@ export const env = createEnv({
     VERCEL_URL: z.string().optional(),
   },
   shared: {
-    NODE_ENV: z.enum(["development", "production", "test"]),
+    NODE_ENV: z.enum(["development", "production", "test"]).optional().default("development"),
     ENV: z.enum(["local", "development", "staging", "production"]).optional().default("local"),
   },
   // we use the manual destruction here to validate if env variable is set inside turbo.json
