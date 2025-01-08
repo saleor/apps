ad4068815c356308f8e3ccde17838aa95a4c81fc
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-01-08 10:52:35 +0100

    Use service in router

--- apps/segment/src/modules/configuration/configuration.router.ts
+++ apps/segment/src/modules/configuration/configuration.router.ts
@@ -1,9 +1,12 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createLogger } from "@/logger";
 
 import { protectedClientProcedure } from "../trpc/protected-client-procedure";
 import { router } from "../trpc/trpc-server";
+import { WebhooksActivityClient } from "../webhooks/webhook-activity/webhook-activity-client";
+import { WebhookActivityService } from "../webhooks/webhook-activity/webhook-activity-service";
 import { AppConfigMetadataManager } from "./app-config-metadata-manager";
 
 const logger = createLogger("configurationRouter");
@@ -36,5 +39,19 @@ export const configurationRouter = router({
     await manager.set(config);
 
     logger.debug("Successfully set config");
+
+    try {
+      const webhookActivityClient = new WebhooksActivityClient(ctx.apiClient);
+      const webhookActivityService = new WebhookActivityService(ctx.appId, webhookActivityClient);
+
+      await webhookActivityService.enableAppWebhooks();
+    } catch (error) {
+      logger.error("Error during enabling app webhooks", { error: error });
+
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "There with enabling app webhooks. Contact Saleor support.",
+      });
+    }
   }),
 });
