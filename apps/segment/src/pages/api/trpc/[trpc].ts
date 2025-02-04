fd3bb012deb95d521261448a5cf97e0750b1c20a
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-02-04 10:14:28 +0100

    Fix tRPC wrapping in Segment app (#1724)

--- apps/segment/src/pages/api/trpc/[trpc].ts
+++ apps/segment/src/pages/api/trpc/[trpc].ts
@@ -1,9 +1,25 @@
+import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
+import { withOtel } from "@saleor/apps-otel";
+import * as Sentry from "@sentry/nextjs";
 import * as trpcNext from "@trpc/server/adapters/next";
 
+import { loggerContext } from "@/logger-context";
 import { appRouter } from "@/modules/trpc/trpc-app-router";
 import { createTrpcContext } from "@/modules/trpc/trpc-context";
 
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: createTrpcContext,
-});
+export default wrapWithLoggerContext(
+  withOtel(
+    trpcNext.createNextApiHandler({
+      router: appRouter,
+      createContext: createTrpcContext,
+      onError: ({ error }) => {
+        if (error.code === "INTERNAL_SERVER_ERROR") {
+          Sentry.captureException(error);
+          return;
+        }
+      },
+    }),
+    "/api/trpc",
+  ),
+  loggerContext,
+);
