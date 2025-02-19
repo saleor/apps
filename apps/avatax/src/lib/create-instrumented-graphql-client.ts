91d4e00e04bd648486100be8f4d9e57dfe9748b5
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-02-19 13:55:35 +0100

    add otel exchange

--- apps/avatax/src/lib/create-instrumented-graphql-client.ts
+++ apps/avatax/src/lib/create-instrumented-graphql-client.ts
@@ -1,11 +1,13 @@
 import { createGraphQLClient, CreateGraphQLClientArgs } from "@saleor/apps-shared";
 
+import { otelExchange } from "./otel-exchange";
+
 type CreateGraphQLClientProps = Omit<CreateGraphQLClientArgs, "opts">;
 
 export const createInstrumentedGraphqlClient = (props: CreateGraphQLClientProps) =>
   createGraphQLClient({
     ...props,
     opts: {
-      prependingFetchExchanges: [],
+      prependingFetchExchanges: [otelExchange],
     },
   });
