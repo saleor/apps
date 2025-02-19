e7913854b252f709d27823d664376f3ca98ff82d
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-02-19 10:36:39 +0100

    Add custom span

--- apps/avatax/src/pages/api/manifest.ts
+++ apps/avatax/src/pages/api/manifest.ts
@@ -1,3 +1,4 @@
+import { trace } from "@opentelemetry/api";
 import { createManifestHandler } from "@saleor/app-sdk/handlers/next";
 import { AppManifest } from "@saleor/app-sdk/types";
 import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
@@ -15,29 +16,39 @@ export default wrapWithLoggerContext(
       const iframeBaseUrl = env.APP_IFRAME_BASE_URL ?? appBaseUrl;
       const apiBaseURL = env.APP_API_BASE_URL ?? appBaseUrl;
 
-      const manifest: AppManifest = {
-        about: "App connects with AvaTax to dynamically calculate taxes",
-        appUrl: iframeBaseUrl,
-        author: "Saleor Commerce",
-        brand: {
-          logo: {
-            default: `${apiBaseURL}/logo.png`,
-          },
-        },
-        dataPrivacyUrl: "https://saleor.io/legal/privacy/",
-        extensions: [],
-        homepageUrl: "https://github.com/saleor/apps",
-        id: env.MANIFEST_APP_ID,
-        name: "AvaTax",
-        permissions: ["HANDLE_TAXES", "MANAGE_ORDERS"],
-        requiredSaleorVersion: ">=3.19 <4",
-        supportUrl: "https://github.com/saleor/apps/discussions",
-        tokenTargetUrl: `${apiBaseURL}/api/register`,
-        version: packageJson.version,
-        webhooks: appWebhooks.map((w) => w.getWebhookManifest(apiBaseURL)),
-      };
+      return trace
+        .getTracer("saleor-app-avatax")
+        .startActiveSpan("createManifestHandler", (span) => {
+          span.setAttribute("appBaseUrl", appBaseUrl);
+          span.setAttribute("iframeBaseUrl", iframeBaseUrl);
+          span.setAttribute("apiBaseURL", apiBaseURL);
 
-      return manifest;
+          const manifest: AppManifest = {
+            about: "App connects with AvaTax to dynamically calculate taxes",
+            appUrl: iframeBaseUrl,
+            author: "Saleor Commerce",
+            brand: {
+              logo: {
+                default: `${apiBaseURL}/logo.png`,
+              },
+            },
+            dataPrivacyUrl: "https://saleor.io/legal/privacy/",
+            extensions: [],
+            homepageUrl: "https://github.com/saleor/apps",
+            id: env.MANIFEST_APP_ID,
+            name: "AvaTax",
+            permissions: ["HANDLE_TAXES", "MANAGE_ORDERS"],
+            requiredSaleorVersion: ">=3.19 <4",
+            supportUrl: "https://github.com/saleor/apps/discussions",
+            tokenTargetUrl: `${apiBaseURL}/api/register`,
+            version: packageJson.version,
+            webhooks: appWebhooks.map((w) => w.getWebhookManifest(apiBaseURL)),
+          };
+
+          span.end();
+
+          return manifest;
+        });
     },
   }),
   /*
