16d8ef2a59941b713f2299da3eef1d754b603775
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-02-20 08:40:14 +0100

    rename instrumentation

--- apps/avatax/src/instrumentation.ts
+++ apps/avatax/src/instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
   // eslint-disable-next-line node/no-process-env
   if (process.env.NEXT_RUNTIME === "nodejs" && process.env.OTEL_ENABLED === "true") {
-    await import("./instrumentation.node");
+    await import("./otel-instrumentation.node");
   }
 }
