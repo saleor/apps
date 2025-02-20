7d1486b1611d536dad459264fddf7032db191c84
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-02-20 09:10:17 +0100

    additional factories

--- apps/avatax/src/otel-instrumentation.node.ts
+++ apps/avatax/src/otel-instrumentation.node.ts
@@ -1,9 +1,8 @@
 import { W3CTraceContextPropagator } from "@opentelemetry/core";
-import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
-import { Resource } from "@opentelemetry/resources";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { createBatchSpanProcessor } from "@saleor/apps-otel/src/batch-span-processor-factory";
+import { createBatchSpanProcessor } from "@saleor/apps-otel/batch-span-processor-factory";
+import { createHttpInstrumentation } from "@saleor/apps-otel/http-instrumentation-factory";
+import { createResource } from "@saleor/apps-otel/resource-factory";
 
 import { env } from "@/env";
 
@@ -11,27 +10,18 @@ import pkg from "../package.json";
 import { OTELSampler } from "./lib/otel-sampler";
 
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: env.OTEL_SERVICE_NAME,
-    [SemanticResourceAttributes.SERVICE_VERSION]: pkg.version,
-    [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: env.ENV,
-    "commit-sha": env.VERCEL_GIT_COMMIT_SHA,
+  resource: createResource({
+    serviceName: env.OTEL_SERVICE_NAME,
+    serviceVersion: pkg.version,
+    serviceEnviroment: env.ENV,
+    serviceCommitSha: env.VERCEL_GIT_COMMIT_SHA,
   }),
   textMapPropagator: new W3CTraceContextPropagator(),
   sampler: new OTELSampler(), // custom sampler to test allow all spans
   spanProcessor: createBatchSpanProcessor({
     accessToken: env.OTEL_ACCESS_TOKEN,
   }),
-  instrumentations: [
-    new HttpInstrumentation({
-      requireParentforIncomingSpans: true,
-      requireParentforOutgoingSpans: true,
-      ignoreOutgoingUrls: [
-        (url) => url.includes("ingest.sentry.io"),
-        (url) => url.includes("/v1/logs"),
-      ],
-    }),
-  ],
+  instrumentations: [createHttpInstrumentation()],
 });
 
 sdk.start();
