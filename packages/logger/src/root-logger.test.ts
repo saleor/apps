5d728bf4b860931e18b2b5ce3d945944458f44b5
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-02-24 13:00:24 +0100

    Cleanup OTEL pkgs & remove not needed log transport (#1743)

--- packages/logger/src/root-logger.test.ts
+++ packages/logger/src/root-logger.test.ts
@@ -1,9 +1,6 @@
-import { logs } from "@opentelemetry/api-logs";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { describe, expect, it, vi } from "vitest";
 
 import { attachLoggerConsoleTransport } from "./logger-console-transport";
-import { attachLoggerOtelTransport } from "./logger-otel-transport";
 import { rootLogger } from "./root-logger";
 
 vi.spyOn(console, "log");
@@ -44,126 +41,6 @@ describe("Logger", () => {
           2,
         ),
       );
-    });
-  });
-
-  describe("Otel Transport", () => {
-    it("Calls Open Telemetry logger emit() function, passing there required attributes", () => {
-      const logger = rootLogger.getSubLogger(
-        { name: "Test Logger" },
-        {
-          rootScopePrimitiveArg: 1,
-          rootScopeObjectArg: {
-            objectKey: "objectValue",
-          },
-        },
-      );
-
-      const mockOtelEmit = vi.fn();
-
-      vi.spyOn(logs, "getLogger").mockImplementation(() => {
-        return {
-          emit: mockOtelEmit,
-        };
-      });
-
-      vi.stubEnv("OTEL_SERVICE_NAME", "otel service name");
-      vi.stubEnv("ENV", "development");
-      vi.stubEnv("VERCEL_GIT_COMMIT_SHA", "ASD#!@#");
-
-      attachLoggerOtelTransport(logger, "1.0.0");
-
-      logger.info("Test Message", {
-        childScopePrimitiveArg: 2,
-        childScopeObjectArg: {
-          objectKey: "objectValue",
-        },
-      });
-
-      expect(mockOtelEmit).toHaveBeenCalledWith({
-        severityText: "INFO",
-        context: expect.anything(), // Unique otel context
-        body: "[Test Logger] Test Message",
-        attributes: {
-          rootScopePrimitiveArg: 1,
-          rootScopeObjectArg: {
-            objectKey: "objectValue",
-          },
-          childScopeObjectArg: {
-            objectKey: "objectValue",
-          },
-          childScopePrimitiveArg: 2,
-          [SemanticResourceAttributes.SERVICE_NAME]: "otel service name",
-          [SemanticResourceAttributes.SERVICE_VERSION]: "1.0.0",
-          [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: "development",
-          "commit-sha": "ASD#!@#",
-        },
-      });
-    });
-
-    it("Calls Open Telemetry logger emit() function, passing there error attribute", () => {
-      const logger = rootLogger.getSubLogger(
-        { name: "Test Logger" },
-        {
-          rootScopePrimitiveArg: 1,
-          rootScopeObjectArg: {
-            objectKey: "objectValue",
-          },
-          error: new Error("Error Message"),
-        },
-      );
-
-      expect.assertions(3);
-
-      const mockOtelEmit = vi.fn().mockImplementation((log) => {
-        const error = log.attributes.error;
-
-        expect(error.message).toBe("Error Message");
-        expect(error.cause).toBe("Error cause");
-      });
-
-      vi.spyOn(logs, "getLogger").mockImplementation(() => {
-        return {
-          emit: mockOtelEmit,
-        };
-      });
-
-      vi.stubEnv("OTEL_SERVICE_NAME", "otel service name");
-      vi.stubEnv("ENV", "development");
-      vi.stubEnv("VERCEL_GIT_COMMIT_SHA", "ASD#!@#");
-
-      attachLoggerOtelTransport(logger, "1.0.0");
-
-      logger.error("Test Message", {
-        childScopePrimitiveArg: 2,
-        childScopeObjectArg: {
-          objectKey: "objectValue",
-        },
-        error: new Error("Error Message", {
-          cause: "Error cause",
-        }),
-      });
-
-      expect(mockOtelEmit).toHaveBeenCalledWith({
-        severityText: "ERROR",
-        context: expect.anything(), // Unique otel context
-        body: "[Test Logger] Test Message",
-        attributes: {
-          error: expect.any(Error),
-          rootScopePrimitiveArg: 1,
-          rootScopeObjectArg: {
-            objectKey: "objectValue",
-          },
-          childScopeObjectArg: {
-            objectKey: "objectValue",
-          },
-          childScopePrimitiveArg: 2,
-          [SemanticResourceAttributes.SERVICE_NAME]: "otel service name",
-          [SemanticResourceAttributes.SERVICE_VERSION]: "1.0.0",
-          [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: "development",
-          "commit-sha": "ASD#!@#",
-        },
-      });
     });
   });
 });
