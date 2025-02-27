cb1ad14af8418cc86650c148ec3f72a07935aa95
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-02-27 11:14:18 +0100

    use 19 sec

--- apps/avatax/src/otel-instrumentation.node.ts
+++ apps/avatax/src/otel-instrumentation.node.ts
@@ -22,7 +22,7 @@ const sdk = new NodeSDK({
   }),
   metricReader: createMetricReader({
     accessToken: env.OTEL_ACCESS_TOKEN,
-    exportIntervalMillis: 20_000, // max lambda execution time
+    exportIntervalMillis: 19_000, // max lambda execution time
   }),
   instrumentations: [createHttpInstrumentation()],
 });
