abebbadc993ecbc4c35498d96c152e370de431b7
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-02-28 12:04:29 +0100

    test with otel wrapper

---
+++ apps/avatax/src/lib/with-otel.ts
@@ -0,0 +1,78 @@
+import { trace } from "@opentelemetry/api";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { SALEOR_API_URL_HEADER, SALEOR_SCHEMA_VERSION } from "@saleor/app-sdk/const";
+import { ObservabilityAttributes } from "@saleor/apps-otel/src/observability-attributes";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+
+import { BaseError } from "@/error";
+
+import { race } from "./race";
+
+export const withOtel = (
+  handler: NextApiHandler,
+  isOtelEnabled: boolean,
+  metricReader: PeriodicExportingMetricReader | undefined,
+): NextApiHandler => {
+  if (!isOtelEnabled) {
+    return handler;
+  }
+
+  return new Proxy(handler, {
+    apply: async (
+      wrappingTarget,
+      thisArg,
+      args: [NextApiRequest | undefined, NextApiResponse | undefined],
+    ) => {
+      const [req, res] = args;
+
+      if (!req || !res) {
+        console.warn("No request and/or response objects found, OTEL is not set-up");
+
+        // @ts-expect-error runtime check
+        return wrappingTarget.apply(thisArg, args);
+      }
+
+      const span = trace.getActiveSpan();
+
+      if (span) {
+        const saleorApiUrl = req.headers[SALEOR_API_URL_HEADER] as string | undefined;
+        const saleorVersion = req.headers[SALEOR_SCHEMA_VERSION] as string | undefined;
+
+        if (saleorApiUrl) {
+          span.setAttribute(ObservabilityAttributes.SALEOR_API_URL, saleorApiUrl);
+        }
+
+        if (saleorVersion) {
+          span.setAttribute(ObservabilityAttributes.SALEOR_VERSION, saleorVersion);
+        }
+      }
+
+      const originalResEnd = res.end;
+
+      /**
+       * Override native res.end to flush OTEL traces before it ends
+       */
+      // @ts-expect-error - this is a hack to get around Vercel freezing lambda's
+      res.end = async function (this: unknown, ...args: unknown[]) {
+        try {
+          if (metricReader) {
+            console.log("Force flush of metrics");
+            await race({
+              promise: metricReader.forceFlush(),
+              error: new BaseError("Timeout error while flushing metrics"),
+              timeout: 1_000,
+            });
+          }
+        } catch (e) {
+          console.error("Failed to flush OTEL", { error: e });
+          // noop - don't block return even if we loose traces
+        }
+
+        // @ts-expect-error passthrough args to the original function
+        return originalResEnd.apply(this, args);
+      };
+
+      wrappingTarget.apply(thisArg, [req, res]);
+    },
+  });
+};
--- apps/avatax/src/pages/api/manifest.ts
+++ apps/avatax/src/pages/api/manifest.ts
@@ -2,12 +2,12 @@ import { SpanKind } from "@opentelemetry/api";
 import { createManifestHandler } from "@saleor/app-sdk/handlers/next";
 import { AppManifest } from "@saleor/app-sdk/types";
 import { wrapWithLoggerContext } from "@saleor/apps-logger/node";
-import { wrapWithSpanAttributes } from "@saleor/apps-otel/src/wrap-with-span-attributes";
 
 import { env } from "@/env";
 import { BaseError } from "@/error";
 import { externalMeter } from "@/lib/otel-metrics";
 import { externalTracer } from "@/lib/otel-tracers";
+import { withOtel } from "@/lib/with-otel";
 import { loggerContext } from "@/logger-context";
 
 import packageJson from "../../../package.json";
@@ -18,68 +18,68 @@ const requestCounter = externalMeter.createCounter("http.requests", {
   unit: "{requests}",
 });
 
-export default wrapWithLoggerContext(
-  wrapWithSpanAttributes(
-    createManifestHandler({
-      async manifestFactory({ appBaseUrl }) {
-        const iframeBaseUrl = env.APP_IFRAME_BASE_URL ?? appBaseUrl;
-        const apiBaseURL = env.APP_API_BASE_URL ?? appBaseUrl;
+const handler = createManifestHandler({
+  async manifestFactory({ appBaseUrl }) {
+    const iframeBaseUrl = env.APP_IFRAME_BASE_URL ?? appBaseUrl;
+    const apiBaseURL = env.APP_API_BASE_URL ?? appBaseUrl;
 
-        return externalTracer.startActiveSpan(
-          "createManifestHandler",
-          {
-            kind: SpanKind.CLIENT,
-          },
-          async (span) => {
-            const manifest: AppManifest = {
-              about: "App connects with AvaTax to dynamically calculate taxes",
-              appUrl: iframeBaseUrl,
-              author: "Saleor Commerce",
-              brand: {
-                logo: {
-                  default: `${apiBaseURL}/logo.png`,
-                },
-              },
-              dataPrivacyUrl: "https://saleor.io/legal/privacy/",
-              extensions: [],
-              homepageUrl: "https://github.com/saleor/apps",
-              id: env.MANIFEST_APP_ID,
-              name: "AvaTax",
-              permissions: ["HANDLE_TAXES", "MANAGE_ORDERS"],
-              requiredSaleorVersion: ">=3.19 <4",
-              supportUrl: "https://github.com/saleor/apps/discussions",
-              tokenTargetUrl: `${apiBaseURL}/api/register`,
-              version: packageJson.version,
-              webhooks: appWebhooks.map((w) => w.getWebhookManifest(apiBaseURL)),
-            };
-
-            /*
-             * span.setAttribute("http.method", "GET");
-             * span.setAttribute("http.status_code", 200);
-             */
-
-            const error = new BaseError("Test error for span!");
-
-            // serialize to avoid leaking stack trace
-            span.recordException(BaseError.serialize(error));
-
-            span.addEvent("Fetched manifest");
-
-            // span.setStatus({ code: SpanStatusCode.ERROR });
-
-            span.end();
-
-            requestCounter.add(1);
-
-            console.log("Current metric reader", global.currentMetricReader);
-
-            await global.currentMetricReader?.forceFlush();
-
-            return manifest;
-          },
-        );
+    return externalTracer.startActiveSpan(
+      "createManifestHandler",
+      {
+        kind: SpanKind.CLIENT,
       },
-    }),
-  ),
+      async (span) => {
+        const manifest: AppManifest = {
+          about: "App connects with AvaTax to dynamically calculate taxes",
+          appUrl: iframeBaseUrl,
+          author: "Saleor Commerce",
+          brand: {
+            logo: {
+              default: `${apiBaseURL}/logo.png`,
+            },
+          },
+          dataPrivacyUrl: "https://saleor.io/legal/privacy/",
+          extensions: [],
+          homepageUrl: "https://github.com/saleor/apps",
+          id: env.MANIFEST_APP_ID,
+          name: "AvaTax",
+          permissions: ["HANDLE_TAXES", "MANAGE_ORDERS"],
+          requiredSaleorVersion: ">=3.19 <4",
+          supportUrl: "https://github.com/saleor/apps/discussions",
+          tokenTargetUrl: `${apiBaseURL}/api/register`,
+          version: packageJson.version,
+          webhooks: appWebhooks.map((w) => w.getWebhookManifest(apiBaseURL)),
+        };
+
+        /*
+         * span.setAttribute("http.method", "GET");
+         * span.setAttribute("http.status_code", 200);
+         */
+
+        const error = new BaseError("Test error for span!");
+
+        // serialize to avoid leaking stack trace
+        span.recordException(BaseError.serialize(error));
+
+        span.addEvent("Fetched manifest");
+
+        // span.setStatus({ code: SpanStatusCode.ERROR });
+
+        span.end();
+
+        requestCounter.add(1);
+
+        // console.log("Current metric reader", global.currentMetricReader);
+
+        // await global.currentMetricReader?.forceFlush();
+
+        return manifest;
+      },
+    );
+  },
+});
+
+export default wrapWithLoggerContext(
+  withOtel(handler, env.OTEL_ENABLED, global.currentMetricReader),
   loggerContext,
 );
