465b0e4bd52a77c2322a765a209ec84d86e52f7d
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-03 15:50:02 +0100

    unset var

--- apps/avatax/src/otel-instrumentation.ts
+++ apps/avatax/src/otel-instrumentation.ts
@@ -2,7 +2,7 @@
 // Use `process.env` here to avoid broken Next.js build
 import { Context, Sampler, SamplingResult, SpanKind } from "@opentelemetry/api";
 import { ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
-import { ATTR_DEPLOYMENT_ENVIRONMENT } from "@opentelemetry/semantic-conventions/incubating";
+import { ATTR_DEPLOYMENT_ENVIRONMENT_NAME } from "@opentelemetry/semantic-conventions/incubating";
 import { createBatchSpanProcessor } from "@saleor/apps-otel/src/batch-span-processor-factory";
 import { createHttpInstrumentation } from "@saleor/apps-otel/src/http-instrumentation-factory";
 import { registerOTel } from "@vercel/otel";
@@ -28,8 +28,11 @@ registerOTel({
   serviceName: process.env.OTEL_SERVICE_NAME,
   attributes: {
     [ATTR_SERVICE_VERSION]: pkg.version,
-    [ATTR_DEPLOYMENT_ENVIRONMENT]: process.env.ENV,
+    [ATTR_DEPLOYMENT_ENVIRONMENT_NAME]: process.env.ENV,
     "commit-sha": process.env.VERCEL_GIT_COMMIT_SHA,
+    // unset env set by @vercel/otel as it is used by our collector
+    env: undefined,
+    "vercel.env": process.env.VERCEL_ENV,
   },
   spanProcessors: [
     createBatchSpanProcessor({
