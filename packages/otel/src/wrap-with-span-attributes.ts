ae98562a460a205b7b23890e9500229600da44d6
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-03-03 15:01:59 +0100

    wip trying app dir route in segment app

--- packages/otel/src/wrap-with-span-attributes.ts
+++ packages/otel/src/wrap-with-span-attributes.ts
@@ -1,6 +1,7 @@
 import { trace } from "@opentelemetry/api";
 import { SALEOR_API_URL_HEADER, SALEOR_SCHEMA_VERSION } from "@saleor/app-sdk/const";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
 import { ObservabilityAttributes } from "./observability-attributes";
 
@@ -23,3 +24,22 @@ export const wrapWithSpanAttributes = (handler: NextApiHandler) => {
     return handler(req, res);
   };
 };
+
+export const wrapWithSpanAttributesAppRouter =
+  (handler: (req: Request) => Promise<Response> | Response) => (req: Request) => {
+    const span = trace.getActiveSpan();
+
+    if (span) {
+      const saleorApiUrl = req.headers.get(SALEOR_API_URL_HEADER) as string | undefined;
+      const saleorVersion = req.headers.get(SALEOR_SCHEMA_VERSION) as string | undefined;
+
+      if (saleorApiUrl) {
+        span.setAttribute(ObservabilityAttributes.SALEOR_API_URL, saleorApiUrl);
+      }
+
+      if (saleorVersion) {
+        span.setAttribute(ObservabilityAttributes.SALEOR_VERSION, saleorVersion);
+      }
+    }
+    return handler(req);
+  };
