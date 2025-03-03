9041df0b234c465cb6d832de95597a13e327cf88
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-03 16:03:54 +0100

    cleanup

--- apps/avatax/src/otel-instrumentation.ts
+++ apps/avatax/src/otel-instrumentation.ts
@@ -1,6 +1,5 @@
 /* eslint-disable node/no-process-env */
 // Use `process.env` here to avoid broken Next.js build
-import { Context, Sampler, SamplingResult, SpanKind } from "@opentelemetry/api";
 import { ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
 import { ATTR_DEPLOYMENT_ENVIRONMENT_NAME } from "@opentelemetry/semantic-conventions/incubating";
 import { createBatchSpanProcessor } from "@saleor/apps-otel/src/batch-span-processor-factory";
@@ -8,21 +7,6 @@ import { createHttpInstrumentation } from "@saleor/apps-otel/src/http-instrument
 import { registerOTel } from "@vercel/otel";
 
 import pkg from "../package.json";
-
-class OTELSampler implements Sampler {
-  shouldSample(
-    context: Context,
-    traceId: string,
-    spanName: string,
-    spanKind: SpanKind,
-  ): SamplingResult {
-    return { decision: 2 };
-  }
-
-  toString(): string {
-    return "OTELSampler";
-  }
-}
 
 registerOTel({
   serviceName: process.env.OTEL_SERVICE_NAME,
@@ -40,5 +24,4 @@ registerOTel({
     }),
   ],
   instrumentations: [createHttpInstrumentation()],
-  traceSampler: new OTELSampler(),
 });
