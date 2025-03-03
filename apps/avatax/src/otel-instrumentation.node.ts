d9bd2ee2a196c231052e2e22ff7ff5b573430d14
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-03 14:21:22 +0100

    add example sampler

--- apps/avatax/src/otel-instrumentation.node.ts
+++ apps/avatax/src/otel-instrumentation.node.ts
@@ -1,4 +1,5 @@
 /* eslint-disable node/no-process-env */
+import { Context, Sampler, SamplingResult, SpanKind } from "@opentelemetry/api";
 import { ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
 import { ATTR_DEPLOYMENT_ENVIRONMENT_NAME } from "@opentelemetry/semantic-conventions/incubating";
 import { createBatchSpanProcessor } from "@saleor/apps-otel/src/batch-span-processor-factory";
@@ -6,10 +7,26 @@ import { registerOTel } from "@vercel/otel";
 
 import pkg from "../package.json";
 
+class OTELSampler implements Sampler {
+  shouldSample(
+    context: Context,
+    traceId: string,
+    spanName: string,
+    spanKind: SpanKind,
+  ): SamplingResult {
+    return { decision: 2 };
+  }
+
+  toString(): string {
+    return "OTELSampler";
+  }
+}
+
 registerOTel({
   serviceName: process.env.OTEL_SERVICE_NAME,
   attributes: {
     [ATTR_SERVICE_VERSION]: pkg.version,
+    // TODO: migrate DD to use `deployment.environment.name` instead `deployment.environment`
     [ATTR_DEPLOYMENT_ENVIRONMENT_NAME]: process.env.ENV,
     "commit-sha": process.env.VERCEL_GIT_COMMIT_SHA,
   },
@@ -18,4 +35,5 @@ registerOTel({
       accessToken: process.env.OTEL_ACCESS_TOKEN!,
     }),
   ],
+  traceSampler: new OTELSampler(),
 });
