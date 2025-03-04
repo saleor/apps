878e889730a1d46fdffd401d8531276b35e65656
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-04 10:11:07 +0100

    Update CMS dep & tsconfig

--- apps/cms/src/instrumentations/otel.ts
+++ apps/cms/src/instrumentations/otel.ts
@@ -1,6 +1,6 @@
 // Use `process.env` here to avoid broken Next.js build
 import { ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
-import { ATTR_DEPLOYMENT_ENVIRONMENT_NAME } from "@opentelemetry/semantic-conventions/build/src/experimental_attributes";
+import { ATTR_DEPLOYMENT_ENVIRONMENT_NAME } from "@opentelemetry/semantic-conventions/incubating";
 import { createBatchSpanProcessor } from "@saleor/apps-otel/src/batch-span-processor-factory";
 import { createHttpInstrumentation } from "@saleor/apps-otel/src/http-instrumentation-factory";
 import { registerOTel } from "@vercel/otel";
