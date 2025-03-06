3691b21c7e401adacc44f9af48ede1e77f96611a
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-06 13:55:53 +0100

    Fixes after CR

--- packages/otel/src/aws-instrumentation-factory.ts
+++ packages/otel/src/aws-instrumentation-factory.ts
@@ -2,6 +2,7 @@ import { AwsInstrumentation } from "@opentelemetry/instrumentation-aws-sdk";
 
 export const createAwsInstrumentation = () => {
   return new AwsInstrumentation({
+    // disable http instrumentation for aws-sdk operations so that we don't create duplicate spans
     suppressInternalInstrumentation: true,
   });
 };
