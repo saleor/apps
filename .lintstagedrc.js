be89ccf3ac1462323134845a16caea7c003074c8
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-17 14:13:30 +0100

    Fix lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 /**
  * @type {import('lint-staged').Configuration}
  */
-module.exports = {
+export default {
   "*.{jsx,tsx,js,ts,md,mdx}": "eslint --cache --fix",
   "*.{jsx,tsx,js,ts,md,mdx}": "prettier --write",
   "*.{jsx,tsx,js,ts,md,mdx}": "cspell --no-must-find-files",
--- packages/shared/.lintstagedrc.cjs
+++
@@ -1,5 +0,0 @@
-import baseConfig from "../../.lintstagedrc.cjs";
-
-module.exports = {
-  ...baseConfig,
-};
