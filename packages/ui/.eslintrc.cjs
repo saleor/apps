c7a7b7f235ed37833010848acbc379b65f89174b
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-18 16:07:36 +0100

    Migrate packages

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: ["@saleor/eslint-config-apps"],
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "no-console": "error",
+    "@saleor/saleor-app/logger-leak": "error",
+    "max-params": ["error", { max: 3 }],
+    "@vitest/prefer-strict-equal": "error",
+    "@vitest/prefer-vi-mocked": "error",
+  },
 };
--- packages/webhook-utils/.eslintrc.cjs
+++ packages/webhook-utils/.eslintrc.cjs
@@ -3,6 +3,10 @@ module.exports = {
   extends: ["@saleor/eslint-config-apps"],
   rules: {
     "@saleor/saleor-app/logger-leak": "off",
+    "no-console": "error",
+    "max-params": ["error", { max: 3 }],
+    "@vitest/prefer-strict-equal": "error",
+    "@vitest/prefer-vi-mocked": "error",
   },
   parserOptions: {
     project: "tsconfig.json",
