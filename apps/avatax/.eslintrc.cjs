1d08a179da509443e371de2107ca43ef3e0a34d5
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-18 08:12:40 +0100

    add eslint

--- apps/avatax/.eslintrc.cjs
+++ apps/avatax/.eslintrc.cjs
@@ -5,6 +5,9 @@ module.exports = {
   rules: {
     "turbo/no-undeclared-env-vars": ["error"],
     "node/no-process-env": ["error"],
+    "max-params": ["error", { max: 3 }],
+    "@vitest/prefer-strict-equal": "error",
+    "@vitest/prefer-vi-mocked": "error",
   },
   parserOptions: {
     project: "tsconfig.json",
