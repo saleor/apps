a1aeea09074e5de6f213a6b3e25bffce8d392b4e
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-21 13:52:14 +0100

    use Sentry with OTEL

--- apps/avatax/src/instrumentations/otel-node.ts
+++ apps/avatax/src/instrumentations/otel-node.ts
@@ -1,31 +1,65 @@
-import { Context, Sampler, SamplingResult, SpanKind } from "@opentelemetry/api";
+/* eslint-disable max-params */
+import { ParentBasedSampler, TraceIdRatioBasedSampler } from "@opentelemetry/sdk-trace-node";
 import { ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
 import { ATTR_DEPLOYMENT_ENVIRONMENT_NAME } from "@opentelemetry/semantic-conventions/incubating";
 import { createAwsInstrumentation } from "@saleor/apps-otel/src/aws-instrumentation-factory";
 import { createBatchSpanProcessor } from "@saleor/apps-otel/src/batch-span-processor-factory";
 import { createHttpInstrumentation } from "@saleor/apps-otel/src/http-instrumentation-factory";
 import { ObservabilityAttributes } from "@saleor/apps-otel/src/observability-attributes";
+import * as Sentry from "@sentry/nextjs";
+import { SentryPropagator, wrapSamplingDecision } from "@sentry/opentelemetry";
 import { registerOTel } from "@vercel/otel";
 
 import { env } from "@/env";
 
+import { Context, Link, SpanAttributes, SpanKind } from "@opentelemetry/api";
 import pkg from "../../package.json";
 
-class OTELSampler implements Sampler {
-  // eslint-disable-next-line max-params
+class AppSampler extends ParentBasedSampler {
   shouldSample(
     context: Context,
     traceId: string,
     spanName: string,
     spanKind: SpanKind,
-  ): SamplingResult {
-    return { decision: 2 };
-  }
+    attributes: SpanAttributes,
+    links: Link[],
+  ) {
+    const { decision } = super.shouldSample(
+      context,
+      traceId,
+      spanName,
+      spanKind,
+      attributes,
+      links,
+    );
 
-  toString(): string {
-    return "OTELSampler";
+    console.log("AppSampler.shouldSample.decision", { decision });
+
+    return wrapSamplingDecision({
+      decision,
+      context,
+      spanAttributes: attributes,
+    });
+  }
+  toString() {
+    return "AppSampler";
   }
 }
+
+Sentry.init({
+  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: env.ENV,
+  includeLocalVariables: true,
+  skipOpenTelemetrySetup: true,
+  registerEsmLoaderHooks: false,
+  integrations: [
+    Sentry.localVariablesIntegration({
+      captureAllExceptions: true,
+    }),
+    Sentry.extraErrorDataIntegration(),
+    Sentry.httpIntegration({ spans: false }),
+  ],
+});
 
 registerOTel({
   serviceName: env.OTEL_SERVICE_NAME,
@@ -44,5 +78,9 @@ registerOTel({
     }),
   ],
   instrumentations: [createAwsInstrumentation(), createHttpInstrumentation()],
-  // traceSampler: new OTELSampler(),
+  traceSampler: new AppSampler({
+    root: new TraceIdRatioBasedSampler(env.OTEL_TRACES_SAMPLER_ARG),
+  }),
+  propagators: [new SentryPropagator()],
+  contextManager: new Sentry.SentryContextManager(),
 });
