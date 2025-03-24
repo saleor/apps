84d421c13c5d63ad81b9cf7d9ea5d3e6bb4faa29
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-24 13:05:07 +0100

    Renames

---
+++ cspell.config.js
@@ -0,0 +1,69 @@
+/**
+ * @type { import("@cspell/cspell-types").CSpellUserSettings }
+ */
+export default {
+  patterns: [
+    {
+      name: "comment-single-line",
+      pattern: "/#.*/g",
+    },
+    {
+      name: "comment-multi-line",
+      pattern: "/(?:\\/\\*[\\s\\S]*?\\*\\/)/g",
+    },
+    {
+      name: "comments",
+      pattern: ["comment-single-line", "comment-multi-line"],
+    },
+    {
+      name: "logger",
+      pattern: "/logger\\.(?:warn|error|info|debug)\\(.*\\)/g",
+    },
+  ],
+  languageSettings: [
+    {
+      languageId: "markdown",
+      caseSensitive: true,
+    },
+    {
+      languageId: ["javascriptreact", "typescriptreact", "typescript", "javascript"],
+      includeRegExpList: ["comments", "logger"],
+    },
+  ],
+  language: "en-US",
+  useGitignore: true,
+  ignorePaths: ["**/graphql.ts", "**/CHANGELOG.md", "**/schema.graphql", "**/generated/types.ts"],
+  words: [
+    "avalara",
+    "avatax",
+    "bruno",
+    "codegen",
+    "contentful",
+    "corepack",
+    "datocms",
+    "devcontainer",
+    "klaviyo",
+    "mailhog",
+    "mjml",
+    "neverthrow",
+    "nygard",
+    "opentelemetry",
+    "pactum",
+    "parentbased",
+    "pryce",
+    "quickstart",
+    "saleor",
+    "sendgrid",
+    "shopx",
+    "strapi",
+    "taxjar",
+    "toolset",
+    "traceidratio",
+    "turborepo",
+    "unobfuscated",
+    "upserted",
+    "upserting",
+    "upstash",
+    "urql",
+  ],
+};
---
+++ knip.config.ts
@@ -0,0 +1,7 @@
+import type { KnipConfig } from "knip";
+
+const config: KnipConfig = {
+  ignore: ["apps/**/generated/**", "packages/**/generated/**"],
+};
+
+export default config;
