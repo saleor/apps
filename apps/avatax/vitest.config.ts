b5d1f4ec4ae668d09f6b432d63d85b190c9915b3
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-24 15:23:38 +0100

    Shuffle tests

--- apps/avatax/vitest.config.ts
+++ apps/avatax/vitest.config.ts
@@ -19,5 +19,12 @@ export default defineProject({
     alias: {
       "@/": new URL("./src/", import.meta.url).pathname,
     },
+    sequence: {
+      /**
+       * Shuffle tests to avoid side effects, where test_2 relies on something that test_1 did.
+       * Now tests will fail a little earlier
+       */
+      shuffle: true,
+    },
   },
 });
--- packages/logger/vitest.config.ts
+++ packages/logger/vitest.config.ts
@@ -1,10 +1,16 @@
-import { mergeConfig } from "vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globals: true,
+    passWithNoTests: true,
     environment: "jsdom",
     globalSetup: "./test-globals.ts",
+    sequence: {
+      /**
+       * Shuffle tests to avoid side effects, where test_2 relies on something that test_1 did.
+       * Now tests will fail a little earlier
+       */
+      shuffle: true,
+    },
   },
 });
