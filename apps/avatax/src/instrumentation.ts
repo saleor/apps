6e19b72addf30297800fafd4eb06c4de0b0d9575
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-24 08:08:46 +0100

    Fix Sentry dropping internal OTEL spans (#1803)

--- apps/avatax/src/instrumentation.ts
+++ apps/avatax/src/instrumentation.ts
@@ -1,12 +1,12 @@
 // Use `process.env` here to avoid broken Next.js build
 
 export async function register() {
-  if (
-    process.env.NEXT_RUNTIME === "nodejs" &&
-    process.env.OTEL_ENABLED === "true" &&
-    process.env.NEXT_PUBLIC_SENTRY_DSN
-  ) {
+  if (process.env.NEXT_RUNTIME === "nodejs" && process.env.OTEL_ENABLED === "true") {
     await import("./instrumentations/otel-node");
+  }
+
+  if (process.env.NEXT_RUNTIME === "nodejs" && process.env.NEXT_PUBLIC_SENTRY_DSN) {
+    await import("./instrumentations/sentry-node");
   }
 
   if (process.env.NEXT_RUNTIME === "edge" && process.env.NEXT_PUBLIC_SENTRY_DSN) {
