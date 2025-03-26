269c5f0dd89720e32c50e03244ac0eef727e3598
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-26 10:37:37 +0100

    Remove e2e package

--- packages/e2e/tests/apps/product-feed/operations/set-category-mapping.ts
+++
@@ -1,7 +0,0 @@
-import { FrameLocator } from "@playwright/test";
-
-export const setCategoryMapping = async (iframeLocator: FrameLocator) => {
-  await iframeLocator.locator("select").first().selectOption({ index: 0 });
-
-  await iframeLocator.getByText("Save").first().click();
-};
