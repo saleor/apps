f6302f74332233976ddf870257ce9fece83a6f46
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-03-27 09:46:18 +0100

    Update eslint to 9.x.x (#1818)

---
+++ apps/segment/eslint.config.js
@@ -0,0 +1,24 @@
+import { config } from "@saleor/eslint-config-apps/index.js";
+import nodePlugin from "eslint-plugin-n";
+
+/** @type {import("eslint").Linter.Config} */
+export default [
+  ...config,
+  {
+    name: "saleor-app-segment/custom-config",
+    files: ["**/*.ts"],
+    plugins: {
+      n: nodePlugin,
+    },
+    rules: {
+      "n/no-process-env": "error",
+    },
+  },
+  {
+    name: "saleor-app-segment/override-no-process-env",
+    files: ["next.config.ts", "src/env.ts", "src/instrumentation.ts"],
+    rules: {
+      "n/no-process-env": "off",
+    },
+  },
+];
