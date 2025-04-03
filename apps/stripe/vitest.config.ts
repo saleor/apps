3e5526cf9f87ed4e8a0c0cfcb8a77f87503d43b9
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-04-03 09:08:12 +0200

    Scripts, tracing, graphql-client

--- apps/stripe/vitest.config.ts
+++ apps/stripe/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [],
   test: {
-    include: ["src/**/*.test.{ts,tsx}"],
     passWithNoTests: true, // TODO: remove this after we add first test
     setupFiles: "./src/__tests__/setup.ts",
     css: false,
@@ -12,6 +11,23 @@ export default defineConfig({
       "@/generated": new URL("./generated/", import.meta.url).pathname,
       "@/package.json": new URL("./package.json", import.meta.url).pathname,
     },
+    workspace: [
+      {
+        extends: true,
+        test: {
+          include: ["src/**/*.test.ts"],
+          exclude: ["src/**/*.integration.test.ts"], // exclude integration tests so vitest doesn't run them twice
+          name: "unit",
+        },
+      },
+      {
+        extends: true,
+        test: {
+          include: ["src/**/*.integration.test.{ts,tsx}"],
+          name: "integration",
+        },
+      },
+    ],
     sequence: {
       /**
        * Shuffle tests to avoid side effects, where test_2 relies on something that test_1 did.
