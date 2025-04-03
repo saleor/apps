e1b2aae02033db4a3e0d10db64c467a873b501e8
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-04-03 09:29:10 +0200

    add modern errors

---
+++ apps/stripe/src/lib/errors.ts
@@ -0,0 +1,11 @@
+import ModernError from "modern-errors";
+import modernErrorsSerialize from "modern-errors-serialize";
+
+export const BaseError = ModernError.subclass("BaseError", {
+  plugins: [modernErrorsSerialize],
+  serialize: {
+    exclude: ["stack"],
+  },
+});
+
+export const UnknownError = BaseError.subclass("UnknownError");
