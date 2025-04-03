6c34b7be967c02ca09df0b53777a25ec66689dfa
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-04-03 13:31:34 +0200

    fix typo

--- apps/stripe/src/modules/stripe/stripe-restriced-key.ts
+++ apps/stripe/src/modules/stripe/stripe-restriced-key.ts
@@ -1,4 +1,4 @@
-export class StripeSecretKey {
+export class StripeRestrictedKey {
   // TODO: use this to validate the keys on client side
   static testPrefix = "rk_test_";
   static livePrefix = "rk_live_";
@@ -6,7 +6,7 @@ export class StripeSecretKey {
   private constructor(private key: string) {}
 
   static createFromUserInput(args: { restrictedKey: string }) {
-    return new StripeSecretKey(args.restrictedKey);
+    return new StripeRestrictedKey(args.restrictedKey);
   }
 
   isValid() {
