cf0f48276798e1b93a47d8c1a9d8ce13fe4a8ddf
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-04-03 12:38:33 +0200

    fix eslint rule

--- apps/stripe/src/lib/tracing.ts
+++ apps/stripe/src/lib/tracing.ts
@@ -1,5 +1,5 @@
 import { trace } from "@opentelemetry/api";
 
-import pkg from "../../package.json";
+import pkg from "@/package.json";
 
 export const appInternalTracer = trace.getTracer("saleor.app.payment.stripe.core", pkg.version);
