12084d1c2364e68fa9f626f9ffe776d147757cae
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-04-11 12:13:09 +0200

    Use config id instead channel id in stripe webhook (#1857)
    * use configuration id in webhook url
    
    * simplify file config repo

--- apps/stripe/src/__tests__/mocks/constants.ts
+++ apps/stripe/src/__tests__/mocks/constants.ts
@@ -2,11 +2,12 @@ import { mockedSaleorApiUrl } from "@/__tests__/mocks/saleor-api-url";
 import { WebhookParams } from "@/app/api/stripe/webhook/webhook-params";
 
 export const mockedSaleorChannelId = "Q2hhbm5lbDox";
+export const mockedConfigurationId = "81f323bd-91e2-4838-ab6e-5affd81ffc3b";
 export const mockedSaleorAppId = "saleor-app-id";
 export const mockStripeWebhookSecretValue = "whsec_XYZ";
 export const mockAppToken = "XXXYYYZZZ";
 export const mockAppUrlBase = "https://my-app.saleor.app";
 export const mockAdyenWebhookUrl = `${mockAppUrlBase}?${new URLSearchParams({
   [WebhookParams.saleorApiUrlSearchParam]: mockedSaleorApiUrl.url,
-  [WebhookParams.channelIdSearchParam]: mockedSaleorChannelId,
+  [WebhookParams.configurationIdIdSearchParam]: mockedConfigurationId,
 }).toString()}`;
