51175208c521c72852aa4fd81ee4954076c8eb1c
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-04-17 07:59:26 +0200

    Stripe PK VO conversion

--- apps/stripe/src/modules/stripe/stripe-publishable-key.ts
+++ apps/stripe/src/modules/stripe/stripe-publishable-key.ts
@@ -1,45 +1,28 @@
-import { err, ok, Result } from "neverthrow";
+import { err, fromThrowable, ok, Result } from "neverthrow";
+import { z } from "zod";
 
 import { BaseError } from "@/lib/errors";
 
-export class StripePublishableKey {
-  readonly keyValue: string;
-
-  static ValidationError = BaseError.subclass("ValidationError", {
+export const StripePublishableKeyValidationError = BaseError.subclass(
+  "StripePublishableKeyValidationError",
+  {
     props: {
-      _internalName: "StripePublishableKey.ValidationError" as const,
+      _internalName: "StripePublishableKeyValidationError" as const,
     },
-  });
+  },
+);
 
-  private static testPrefix = "pk_test_";
-  private static livePrefix = "pk_live_";
+export const StripePublishableKeySchema = z
+  .string()
+  .min(1)
+  .refine((value) => {
+    return value.startsWith("pk_test_") || value.startsWith("pk_live_");
+  })
+  .brand("StripePublishableKey");
 
-  private constructor(keyValue: string) {
-    this.keyValue = keyValue;
-  }
+export const createStripePublishableKey = (raw: string | null) =>
+  fromThrowable(StripePublishableKeySchema.parse, (error) =>
+    StripePublishableKeyValidationError.normalize(error),
+  )(raw);
 
-  private static isInProperFormat(keyValue: string): boolean {
-    return (
-      keyValue.startsWith(StripePublishableKey.testPrefix) ||
-      keyValue.startsWith(StripePublishableKey.livePrefix)
-    );
-  }
-
-  static create(args: {
-    publishableKey: string;
-  }): Result<StripePublishableKey, InstanceType<typeof StripePublishableKey.ValidationError>> {
-    if (args.publishableKey.length === 0) {
-      return err(new this.ValidationError("Publishable key cannot be empty"));
-    }
-
-    if (!this.isInProperFormat(args.publishableKey)) {
-      return err(
-        new this.ValidationError(
-          "Invalid publishable key format - it should start with `pk_test_` or `pk_live_`",
-        ),
-      );
-    }
-
-    return ok(new StripePublishableKey(args.publishableKey));
-  }
-}
+export type StripePublishableKey = z.infer<typeof StripePublishableKeySchema>;
