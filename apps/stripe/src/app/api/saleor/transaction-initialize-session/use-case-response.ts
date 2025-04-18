b26bca1b5a638b9f1f212f06828a594c9d1661d7
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-04-18 08:53:41 +0200

    refactor again

--- apps/stripe/src/app/api/saleor/transaction-initialize-session/use-case-response.ts
+++ apps/stripe/src/app/api/saleor/transaction-initialize-session/use-case-response.ts
@@ -1,4 +1,7 @@
-import { buildSyncWebhookResponsePayload } from "@saleor/app-sdk/handlers/shared";
+import {
+  buildSyncWebhookResponsePayload,
+  SyncWebhookResponsesMap,
+} from "@saleor/app-sdk/handlers/shared";
 import { z } from "zod";
 
 import {
@@ -23,8 +26,10 @@ import {
 } from "@/modules/stripe/stripe-payment-intent-api-error";
 import { StripePaymentIntentId } from "@/modules/stripe/stripe-payment-intent-id";
 
+type ReponseResult = SyncWebhookResponsesMap["TRANSACTION_INITIALIZE_SESSION"]["result"];
+
 class ChargeActionRequired extends SuccessWebhookResponse {
-  readonly result = "CHARGE_ACTION_REQUIRED" as const;
+  readonly result: ReponseResult = "CHARGE_ACTION_REQUIRED";
   readonly stripeClientSecret: StripeClientSecret;
   readonly saleorMoney: SaleorMoney;
   readonly stripePaymentIntentId: StripePaymentIntentId;
@@ -53,6 +58,7 @@ class ChargeActionRequired extends SuccessWebhookResponse {
           stripeClientSecret: this.stripeClientSecret,
         },
       }),
+      // this.result now correctly matches the expected union type
       result: this.result,
       amount: this.saleorMoney.amount,
       pspReference: this.stripePaymentIntentId,
@@ -64,50 +70,12 @@ class ChargeActionRequired extends SuccessWebhookResponse {
   }
 }
 
-class AuthorizationActionRequired extends SuccessWebhookResponse {
-  readonly result = "AUTHORIZATION_ACTION_REQUIRED" as const;
-
-  readonly stripeClientSecret: StripeClientSecret;
-  readonly saleorMoney: SaleorMoney;
-  readonly stripePaymentIntentId: StripePaymentIntentId;
-
-  private static ResponseDataSchema = createSuccessWebhookResponseDataSchema(
-    z.object({
-      stripeClientSecret: StripeClientSecretSchema,
-    }),
-  );
-
-  constructor(args: {
-    stripeClientSecret: StripeClientSecret;
-    saleorMoney: SaleorMoney;
-    stripePaymentIntentId: StripePaymentIntentId;
-  }) {
-    super();
-    this.stripeClientSecret = args.stripeClientSecret;
-    this.saleorMoney = args.saleorMoney;
-    this.stripePaymentIntentId = args.stripePaymentIntentId;
-  }
-
-  getResponse() {
-    const typeSafeResponse = buildSyncWebhookResponsePayload<"TRANSACTION_INITIALIZE_SESSION">({
-      data: AuthorizationActionRequired.ResponseDataSchema.parse({
-        paymentIntent: {
-          stripeClientSecret: this.stripeClientSecret,
-        },
-      }),
-      result: this.result,
-      amount: this.saleorMoney.amount,
-      pspReference: this.stripePaymentIntentId,
-      // https://docs.stripe.com/payments/paymentintents/lifecycle
-      message: "Payment intent requires payment method",
-    });
-
-    return Response.json(typeSafeResponse, { status: this.statusCode });
-  }
+class AuthorizationActionRequired extends ChargeActionRequired {
+  readonly result: ReponseResult = "AUTHORIZATION_ACTION_REQUIRED";
 }
 
 class ChargeFailure extends SuccessWebhookResponse {
-  readonly result = "CHARGE_FAILURE" as const;
+  readonly result: ReponseResult = "CHARGE_FAILURE";
   readonly error: StripePaymentIntentAPIError | TransactionInitializeSessionEventDataError;
   readonly saleorEventAmount: number;
 
@@ -156,54 +124,8 @@ class ChargeFailure extends SuccessWebhookResponse {
   }
 }
 
-class AuthorizationFailure extends SuccessWebhookResponse {
-  readonly result = "AUTHORIZATION_FAILURE" as const;
-  readonly error: StripePaymentIntentAPIError | TransactionInitializeSessionEventDataError;
-  readonly saleorEventAmount: number;
-
-  private static ResponseDataSchema = createFailureWebhookResponseDataSchema(
-    z.array(
-      z.object({
-        code: z.union([
-          z.literal(ParseErrorPublicCode),
-          z.literal(UnsupportedPaymentMethodErrorPublicCode),
-          z.literal(StripeCardErrorPublicCode),
-          z.literal(StripeApiErrorPublicCode),
-        ]),
-        message: z.string(),
-      }),
-    ),
-  );
-
-  constructor(args: {
-    error: StripePaymentIntentAPIError | TransactionInitializeSessionEventDataError;
-    saleorEventAmount: number;
-  }) {
-    super();
-    this.error = args.error;
-    // TODO: remove this after Saleor allows to amount to be optional
-    this.saleorEventAmount = args.saleorEventAmount;
-  }
-
-  getResponse() {
-    const typeSafeResponse = buildSyncWebhookResponsePayload<"TRANSACTION_INITIALIZE_SESSION">({
-      result: this.result,
-      message: this.error.merchantMessage,
-      amount: this.saleorEventAmount,
-      data: AuthorizationFailure.ResponseDataSchema.parse({
-        paymentIntent: {
-          errors: [
-            {
-              code: this.error.publicCode,
-              message: this.error.publicMessage,
-            },
-          ],
-        },
-      }),
-    });
-
-    return Response.json(typeSafeResponse, { status: this.statusCode });
-  }
+class AuthorizationFailure extends ChargeFailure {
+  readonly result: ReponseResult = "AUTHORIZATION_FAILURE";
 }
 
 export const TransactionInitalizeSessionUseCaseResponses = {
