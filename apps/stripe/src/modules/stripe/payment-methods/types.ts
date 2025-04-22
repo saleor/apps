c60652af1153d7492a6b9e3986be1ca4ddaa8c16
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-04-22 13:08:25 +0200

    Add better support for authorization flow (#1878)

--- apps/stripe/src/modules/stripe/payment-methods/types.ts
+++ apps/stripe/src/modules/stripe/payment-methods/types.ts
@@ -1,7 +1,12 @@
+import Stripe from "stripe";
+
 import { TransactionFlowStrategyEnum } from "@/generated/graphql";
 
 export interface PaymentMethod {
   getSupportedTransactionFlow(
     saleorTransactionFlow: TransactionFlowStrategyEnum,
   ): TransactionFlowStrategyEnum;
+  getCreatePaymentIntentMethodOptions(
+    saleorTransactionFlow: TransactionFlowStrategyEnum,
+  ): Stripe.PaymentIntentCreateParams.PaymentMethodOptions;
 }
