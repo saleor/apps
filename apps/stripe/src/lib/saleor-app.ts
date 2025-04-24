fa5ba152de5025e1cbd5d2be008d0afd11fe41c1
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-04-24 09:47:45 +0200

    wip

--- apps/stripe/src/lib/saleor-app.ts
+++ apps/stripe/src/lib/saleor-app.ts
@@ -2,10 +2,19 @@ import { APL } from "@saleor/app-sdk/APL";
 import { FileAPL } from "@saleor/app-sdk/APL/file";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
 
+import { DynamoAPL } from "@/modules/apl/dynamodb-apl";
+
 import { env } from "./env";
 
 export let apl: APL;
 switch (env.APL) {
+  case "dynamodb": {
+    const repository = DynamoAPLRepositoryFactory.create();
+
+    apl = new DynamoAPL({ repository });
+    break;
+  }
+
   default: {
     apl = new FileAPL();
     break;
