e33168bc648a0e412b6d1b76f9dcf0f856c00926
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-04-25 12:06:35 +0200

    implement fetching specific config

--- apps/stripe/src/modules/app-config/repositories/dynamodb/dynamodb-app-config-repo.ts
+++ apps/stripe/src/modules/app-config/repositories/dynamodb/dynamodb-app-config-repo.ts
@@ -1,8 +1,9 @@
-import { BatchGetCommand, Parser, PutItemCommand } from "dynamodb-toolbox";
+import { BatchGetCommand, GetItemCommand, Parser, PutItemCommand } from "dynamodb-toolbox";
 import { execute } from "dynamodb-toolbox/table/actions/batchGet";
 import { QueryCommand } from "dynamodb-toolbox/table/actions/query";
 import { err, ok, Result } from "neverthrow";
 
+import { assertUnreachable } from "@/lib/assert-unreachable";
 import { BaseError } from "@/lib/errors";
 import { createLogger } from "@/lib/logger";
 import { AppRootConfig } from "@/modules/app-config/domain/app-root-config";
@@ -12,6 +13,8 @@ import {
   AppConfigRepoError,
   BaseAccessPattern,
   GetStripeConfigAccessPattern,
+  StripeConfigByChannelIdAccessPattern,
+  StripeConfigByConfigIdAccessPattern,
 } from "@/modules/app-config/repositories/app-config-repo";
 import {
   ChannelConfigMappingAccessPattern,
@@ -31,8 +34,8 @@ import { createStripeWebhookSecret } from "@/modules/stripe/stripe-webhook-secre
 export class DynamodbAppConfigRepo implements AppConfigRepo {
   private logger = createLogger("DynamodbAppConfigRepo");
 
-  stripeConfig: DynamoDbStripeConfigEntity;
-  channelConfigMapping: DynamoDbChannelConfigMappingEntity;
+  stripeConfigEntity: DynamoDbStripeConfigEntity;
+  channelConfigMappingEntity: DynamoDbChannelConfigMappingEntity;
 
   // todo: why do we inject entities? for testing only?
   constructor(config: {
@@ -41,8 +44,8 @@ export class DynamodbAppConfigRepo implements AppConfigRepo {
       channelConfigMapping: DynamoDbChannelConfigMappingEntity;
     };
   }) {
-    this.channelConfigMapping = config.entities.channelConfigMapping;
-    this.stripeConfig = config.entities.stripeConfig;
+    this.channelConfigMappingEntity = config.entities.channelConfigMapping;
+    this.stripeConfigEntity = config.entities.stripeConfig;
   }
 
   /**
@@ -52,9 +55,9 @@ export class DynamodbAppConfigRepo implements AppConfigRepo {
   async getRootConfig(
     access: BaseAccessPattern,
   ): Promise<Result<AppRootConfig, InstanceType<typeof BaseError>>> {
-    const allConfigsQuery = this.stripeConfig.table
+    const allConfigsQuery = this.stripeConfigEntity.table
       .build(QueryCommand)
-      .entities(this.stripeConfig)
+      .entities(this.stripeConfigEntity)
       .query({
         range: {
           beginsWith: StripeConfigAccessPattern.getSKforAllItems(),
@@ -66,9 +69,9 @@ export class DynamodbAppConfigRepo implements AppConfigRepo {
       })
       .options({ maxPages: Infinity });
 
-    const allMappingsQuery = this.channelConfigMapping.table
+    const allMappingsQuery = this.channelConfigMappingEntity.table
       .build(QueryCommand)
-      .entities(this.channelConfigMapping)
+      .entities(this.channelConfigMappingEntity)
       .query({
         range: {
           beginsWith: ChannelConfigMappingAccessPattern.getSKforAllChannels(),
@@ -143,10 +146,112 @@ export class DynamodbAppConfigRepo implements AppConfigRepo {
     }
   }
 
-  getStripeConfig(
+  private fetchStripeConfigByItsId(access: StripeConfigByConfigIdAccessPattern) {
+    const query = this.stripeConfigEntity.build(GetItemCommand).key({
+      PK: StripeConfigAccessPattern.getPK(access),
+      SK: StripeConfigAccessPattern.getSKforSpecificItem({
+        configId: access.configId,
+      }),
+    });
+
+    return query.send();
+  }
+
+  private fetchConfigIdFromChannelId(access: StripeConfigByChannelIdAccessPattern) {
+    const query = this.channelConfigMappingEntity.build(GetItemCommand).key({
+      PK: ChannelConfigMappingAccessPattern.getPK(access),
+      SK: ChannelConfigMappingAccessPattern.getSKforSpecificChannel({
+        channelId: access.channelId,
+      }),
+    });
+
+    return query.send();
+  }
+
+  async getStripeConfig(
     access: GetStripeConfigAccessPattern,
   ): Promise<Result<StripeConfig | null, InstanceType<typeof BaseError>>> {
-    return Promise.resolve(undefined);
+    const channelId = "channelId" in access ? access.channelId : undefined;
+    /**
+     * We eventually need config id, so its mutable. It's either provided or will be resolved later and written here
+     */
+    let configId = "configId" in access ? access.configId : undefined;
+
+    if (!configId && channelId) {
+      try {
+        const configIdResult = await this.fetchConfigIdFromChannelId({
+          appId: access.appId,
+          saleorApiUrl: access.saleorApiUrl,
+          channelId: channelId,
+        });
+
+        if (!configIdResult.Item) {
+          return ok(null);
+        }
+
+        const parsed = DynamoDbChannelConfigMappingEntrySchema.build(Parser).parse(
+          configIdResult.Item,
+        );
+
+        configId = parsed.configId;
+      } catch (e) {
+        return err(
+          new AppConfigRepoError.FailureFetchingConfig(
+            "Error fetching specific config from DynamoDB",
+            {
+              cause: e,
+            },
+          ),
+        );
+      }
+    }
+
+    if (!configId) {
+      throw new BaseError("Unreachable code reached");
+    }
+
+    try {
+      const result = await this.fetchStripeConfigByItsId({
+        configId,
+        appId: access.appId,
+        saleorApiUrl: access.saleorApiUrl,
+      });
+
+      if (!result.Item) {
+        return ok(null);
+      }
+
+      const dynamoItem = result.Item;
+
+      const configResult = StripeConfig.create({
+        name: dynamoItem.configName,
+        restrictedKey: createStripeRestrictedKey(dynamoItem.stripeRk)._unsafeUnwrap(), // make it throwable
+        webhookId: dynamoItem.stripeWhId,
+        id: dynamoItem.configId,
+        publishableKey: createStripePublishableKey(dynamoItem.stripePk)._unsafeUnwrap(), // make it throwable
+        webhookSecret: createStripeWebhookSecret(dynamoItem.stripeWhSecret)._unsafeUnwrap(), // make it throwable
+      });
+
+      if (configResult.isErr()) {
+        // Throw and catch it below, so neverthrow can continue
+        throw new BaseError("Failed to parse config from DynamoDB", {
+          cause: configResult.error,
+        });
+      }
+
+      return ok(configResult.value);
+    } catch (e) {
+      return err(
+        new AppConfigRepoError.FailureFetchingConfig(
+          "Error fetching specific config from DynamoDB",
+          {
+            cause: e,
+          },
+        ),
+      );
+    }
+
+    throw new BaseError("Unreachable code reached");
   }
 
   async saveStripeConfig({
@@ -158,7 +263,7 @@ export class DynamodbAppConfigRepo implements AppConfigRepo {
     saleorApiUrl: SaleorApiUrl;
     appId: string;
   }): Promise<Result<void | null, InstanceType<typeof AppConfigRepoError.FailureSavingConfig>>> {
-    const command = this.stripeConfig.build(PutItemCommand).item({
+    const command = this.stripeConfigEntity.build(PutItemCommand).item({
       configId: config.id,
       stripePk: config.publishableKey,
       stripeRk: config.restrictedKey,
