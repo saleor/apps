75f7741241a22dfe9c8171757865c48d193f41f0
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-04-28 09:59:45 +0200

    Tests for config repo (#1888)
    * partial impl of repo
    
    * read root config
    
    * implement fetching specific config
    
    * implement rest methods
    
    * refactor
    
    * replace file config with dynamo
    
    * Refactor names
    
    * tests
    
    * tests
    
    * silence cspell
    
    * Tests for repo

---
+++ apps/stripe/src/modules/app-config/repositories/dynamodb/__tests__/app-config-dynamodb-mocks.ts
@@ -0,0 +1,37 @@
+import {
+  mockedConfigurationId,
+  mockedSaleorAppId,
+  mockedSaleorChannelId,
+} from "@/__tests__/mocks/constants";
+import { mockedSaleorApiUrl } from "@/__tests__/mocks/saleor-api-url";
+
+const mockedStripeConfig = {
+  configName: "tasdafsdf",
+  createdAt: "2025-04-25T09:19:13.402Z",
+  stripeWhId: "we_1RHiPdKFxIUko8m01KAnXiRQ",
+  configId: mockedConfigurationId,
+  stripePk:
+    "pk_test_51Ng2yKKFxIUko8m0IUBO8GvarTlXcNIpAATM9cE7S2GaoFLsTAsn5avHHjfLFVKewqTFwMb2wqOP87CEbgwljzf200aXXm38oM",
+  modifiedAt: "2025-04-25T09:19:13.402Z",
+  stripeWhSecret: "whsec_ZOsiN376Ahfo0N8lWg7PYXNGpnDXShS5",
+  SK: `CONFIG_ID#${mockedConfigurationId}`,
+  stripeRk:
+    "rk_test_51Ng2yKKFxIUko8m0eaadRYweTPBGnnBA58rpt5JQ7Y7VqSBnQu39JHWoqMfd5lSxH9OH44Bm5NMOQkbzpaMdjD3v00VsW1DGyx",
+  PK: `${mockedSaleorApiUrl}#${mockedSaleorAppId}`,
+  _et: "StripeConfig",
+};
+
+const mockedMapping = {
+  createdAt: "2025-04-25T10:26:30.219Z",
+  configId: mockedConfigurationId,
+  modifiedAt: "2025-04-25T10:26:30.219Z",
+  SK: `CHANNEL_ID#${mockedSaleorChannelId}`,
+  PK: `${mockedSaleorApiUrl}#${mockedSaleorAppId}`,
+  channelId: mockedSaleorChannelId,
+  _et: "ChannelConfigMapping",
+};
+
+export const mockedDynamoConfigItems = {
+  mockedStripeConfig,
+  mockedMapping,
+};
