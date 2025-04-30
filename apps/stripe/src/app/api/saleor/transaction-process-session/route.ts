090b6d373873118048d7177b9b3266349d07dead
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-04-30 14:25:07 +0200

    saleor webhooks recipient verification (#1894)
    * saleor webhooks recipient verification
    
    * better test case
    
    * cr fixes
    
    * fix snapshot

--- apps/stripe/src/app/api/saleor/transaction-process-session/route.ts
+++ apps/stripe/src/app/api/saleor/transaction-process-session/route.ts
@@ -2,6 +2,7 @@ import { withSpanAttributesAppRouter } from "@saleor/apps-otel/src/with-span-att
 import { compose } from "@saleor/apps-shared/compose";
 import { captureException } from "@sentry/nextjs";
 
+import { withRecipientVerification } from "@/app/api/saleor/with-recipient-verification";
 import { withLoggerContext } from "@/lib/logger-context";
 import { appConfigRepoImpl } from "@/modules/app-config/repositories/app-config-repo-impl";
 import { createSaleorApiUrl } from "@/modules/saleor/saleor-api-url";
@@ -18,42 +19,43 @@ import { transactionProcessSessionWebhookDefinition } from "./webhook-definition
 const useCase = new TransactionProcessSessionUseCase({
   appConfigRepo: appConfigRepoImpl,
   stripePaymentIntentsApiFactory: new StripePaymentIntentsApiFactory(),
-  // TODO: change it to use DynamoDB
   transactionRecorder: transactionRecorder,
 });
 
-const handler = transactionProcessSessionWebhookDefinition.createHandler(async (_req, ctx) => {
-  try {
-    const saleorApiUrlResult = createSaleorApiUrl(ctx.authData.saleorApiUrl);
+const handler = transactionProcessSessionWebhookDefinition.createHandler(
+  withRecipientVerification(async (_req, ctx) => {
+    try {
+      const saleorApiUrlResult = createSaleorApiUrl(ctx.authData.saleorApiUrl);
 
-    if (saleorApiUrlResult.isErr()) {
-      captureException(saleorApiUrlResult.error);
-      const response = new MalformedRequestResponse();
+      if (saleorApiUrlResult.isErr()) {
+        captureException(saleorApiUrlResult.error);
+        const response = new MalformedRequestResponse();
+
+        return response.getResponse();
+      }
+
+      const result = await useCase.execute({
+        appId: ctx.authData.appId,
+        saleorApiUrl: saleorApiUrlResult.value,
+        event: ctx.payload,
+      });
+
+      return result.match(
+        (result) => {
+          return result.getResponse();
+        },
+        (err) => {
+          return err.getResponse();
+        },
+      );
+    } catch (error) {
+      captureException(error);
+      const response = new UnhandledErrorResponse();
 
       return response.getResponse();
     }
-
-    const result = await useCase.execute({
-      appId: ctx.authData.appId,
-      saleorApiUrl: saleorApiUrlResult.value,
-      event: ctx.payload,
-    });
-
-    return result.match(
-      (result) => {
-        return result.getResponse();
-      },
-      (err) => {
-        return err.getResponse();
-      },
-    );
-  } catch (error) {
-    captureException(error);
-    const response = new UnhandledErrorResponse();
-
-    return response.getResponse();
-  }
-});
+  }),
+);
 
 // TODO: write integration test for this route
 export const POST = compose(withLoggerContext, withSpanAttributesAppRouter)(handler);
