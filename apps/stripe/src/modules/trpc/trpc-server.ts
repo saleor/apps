9ff004e5a109eca8a54c282a38761211b73d548f
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-05-12 12:06:48 +0200

    Add handle payments to trpc requiredClientPermissions

--- apps/stripe/src/modules/trpc/trpc-server.ts
+++ apps/stripe/src/modules/trpc/trpc-server.ts
@@ -1,13 +1,21 @@
 import { Permission } from "@saleor/app-sdk/types";
+import { REQUIRED_SALEOR_PERMISSIONS } from "@saleor/apps-shared/permissions";
 import { initTRPC } from "@trpc/server";
 
 import { TrpcContextAppRouter } from "@/modules/trpc/context-app-router";
 
 interface Meta {
-  requiredClientPermissions?: Permission[];
+  requiredClientPermissions: Permission[];
 }
 
-const t = initTRPC.context<TrpcContextAppRouter>().meta<Meta>().create();
+const t = initTRPC
+  .context<TrpcContextAppRouter>()
+  .meta<Meta>()
+  .create({
+    defaultMeta: {
+      requiredClientPermissions: [...REQUIRED_SALEOR_PERMISSIONS, "HANDLE_PAYMENTS"],
+    },
+  });
 
 export const router = t.router;
 export const procedure = t.procedure;
