5fd1d58ff24f3fa59c9f4682c50c11516ede8b2c
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-05-13 08:08:38 +0200

    Add handle payments to trpc requiredClientPermissions (#1917)

--- apps/stripe/src/pages/config/index.tsx
+++ apps/stripe/src/pages/config/index.tsx
@@ -1,4 +1,3 @@
-import { useAppBridge } from "@saleor/app-sdk/app-bridge";
 import { Layout } from "@saleor/apps-ui";
 import { Box, Text } from "@saleor/macaw-ui";
 import { NextPage } from "next";
@@ -6,15 +5,12 @@ import { NextPage } from "next";
 import { AppHeader } from "@/modules/ui/app-header";
 import { ChannelConfigMappingSection } from "@/modules/ui/channel-configs/channel-config-mapping-section";
 import { ChannelConfigSection } from "@/modules/ui/stripe-configs/channel-config-section";
+import { useHasAppAccess } from "@/modules/ui/use-has-app-access";
 
 const ConfigPage: NextPage = () => {
-  const { appBridgeState } = useAppBridge();
+  const { haveAccessToApp } = useHasAppAccess();
 
-  if (!appBridgeState) {
-    return null;
-  }
-
-  if (appBridgeState.user?.permissions.includes("MANAGE_APPS") === false) {
+  if (!haveAccessToApp) {
     return <Text>You do not have permission to access this page.</Text>;
   }
 
