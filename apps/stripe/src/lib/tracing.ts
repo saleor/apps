4efcf200c9efa3edb81652e7da09a496e93aea1c
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-05-14 11:58:54 +0200

    Rework logs (#1929)
    * Gateway and initialize session logs
    
    * Rework logs
    
    * cr fixes
    
    * debug to log

--- apps/stripe/src/lib/tracing.ts
+++ apps/stripe/src/lib/tracing.ts
@@ -2,4 +2,4 @@ import { trace } from "@opentelemetry/api";
 
 import pkg from "@/package.json";
 
-export const appInternalTracer = trace.getTracer("saleor.app.payment.stripe.core", pkg.version);
+export const appInternalTracer = trace.getTracer("saleor-app-payment-stripe-v2.core", pkg.version);
