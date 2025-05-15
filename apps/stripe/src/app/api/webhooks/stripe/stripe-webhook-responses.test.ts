8e62c853817fd9ef840503ddd6141002e3224686
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-05-15 08:30:20 +0200

    Set webhook responses (#1927)

---
+++ apps/stripe/src/app/api/webhooks/stripe/stripe-webhook-responses.test.ts
@@ -0,0 +1,44 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  StripeWebhookAppIsNotConfiguredResponse,
+  StripeWebhookMalformedRequestResponse,
+  StripeWebhookSeverErrorResponse,
+  StripeWebhookSuccessResponse,
+} from "./stripe-webhook-responses";
+
+describe("StripeWebhookSuccessResponse", () => {
+  it("Returns response with status 200 and fixed message to Stripe", async () => {
+    const response = new StripeWebhookSuccessResponse().getResponse();
+
+    expect(response.status).toBe(200);
+    await expect(response.text()).resolves.toMatchInlineSnapshot(`"Ok"`);
+  });
+});
+
+describe("StripeWebhookMalformedRequestResponse", () => {
+  it("Returns response with status 400 and fixed message to Stripe", async () => {
+    const response = new StripeWebhookMalformedRequestResponse().getResponse();
+
+    expect(response.status).toBe(400);
+    await expect(response.text()).resolves.toMatchInlineSnapshot(`"Malformed request"`);
+  });
+});
+
+describe("StripeWebhookAppIsNotConfiguredResponse", () => {
+  it("Returns response with status 400 and fixed message to Stripe", async () => {
+    const response = new StripeWebhookAppIsNotConfiguredResponse().getResponse();
+
+    expect(response.status).toBe(400);
+    await expect(response.text()).resolves.toMatchInlineSnapshot(`"App is not configured"`);
+  });
+});
+
+describe("StripeWebhookSeverErrorResponse", () => {
+  it("Returns response with status 500 and fixed message to Stripe", async () => {
+    const response = new StripeWebhookSeverErrorResponse().getResponse();
+
+    expect(response.status).toBe(500);
+    await expect(response.text()).resolves.toMatchInlineSnapshot(`"Server error"`);
+  });
+});
