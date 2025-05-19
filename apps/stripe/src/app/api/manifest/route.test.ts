835f535af4373e07f2c148c81648918253cd2983
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-05-19 10:41:20 +0200

    fix test

--- apps/stripe/src/app/api/manifest/route.test.ts
+++ apps/stripe/src/app/api/manifest/route.test.ts
@@ -45,6 +45,7 @@ describe("Manifest handler", async () => {
             "dataPrivacyUrl": "https://saleor.io/legal/privacy/",
             "extensions": [],
             "homepageUrl": "https://github.com/saleor/apps",
+            "id": "saleor.app.payment.stripe-v2",
             "name": "Stripe",
             "permissions": [
               "HANDLE_PAYMENTS",
