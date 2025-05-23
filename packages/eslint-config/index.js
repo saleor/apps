863e988284c6133bc0bc5a8253da00a1f4ae6a8d
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-05-23 12:53:20 +0200

    Update index.js

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -157,8 +157,7 @@ export const config = [
           ],
         },
       ],
-      "turbo/no-undeclared-env-vars": "off",
-      "n/no-process-env": "off",
+      "turbo/no-undeclared-env-vars": "off"
     },
   },
   {
