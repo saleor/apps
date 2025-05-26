a076ab66c97b6055a2de8464e773f5154b7fe3b4
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-05-26 14:21:40 +0200

    remove not needed log

--- apps/stripe/src/modules/transactions-recording/repositories/dynamodb/dynamodb-transaction-recorder-repo.ts
+++ apps/stripe/src/modules/transactions-recording/repositories/dynamodb/dynamodb-transaction-recorder-repo.ts
@@ -79,10 +79,6 @@ export class DynamoDBTransactionRecorderRepo implements TransactionRecorderRepo 
         cause: result,
       });
     } catch (e) {
-      this.logger.error("Failed to write transaction to DynamoDB", {
-        error: e,
-      });
-
       return err(
         new TransactionRecorderError.FailedWritingTransactionError(
           "Failed to write transaction to DynamoDB",
