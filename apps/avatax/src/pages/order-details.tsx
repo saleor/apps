83ea9fc17e078755cfdf4fc5652304687d8f4be4
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-06-18 14:05:29 +0200

    Add comment about order details

--- apps/avatax/src/pages/order-details.tsx
+++ apps/avatax/src/pages/order-details.tsx
@@ -1,3 +1,4 @@
+// This is rendered as SSR hence we import styles again as `_app.tsx` has no ssr wrapper
 import "@saleor/macaw-ui/style";
 import "../styles/globals.css";
 
@@ -13,12 +14,21 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   };
 };
 
-const OrderDetailsPage = ({ queryParams }: { queryParams: Record<string, string> }) => {
+const OrderDetailsPage = ({
+  queryParams,
+}: {
+  queryParams: Record<string, string>;
+}) => {
   return (
     <ThemeProvider>
       <Box paddingTop={4} __height={200}>
         {Object.entries(queryParams).map(([key, value]) => (
-          <Box key={key} marginBottom={2} display="flex" justifyContent="space-between">
+          <Box
+            key={key}
+            marginBottom={2}
+            display="flex"
+            justifyContent="space-between"
+          >
             <Text key={key} as="span" fontWeight="bold">
               {key}:
             </Text>
