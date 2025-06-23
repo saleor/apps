0caf3e0950d2dc0310347a55e4947753ef54711a
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-06-23 13:50:35 +0200

    Add Stripe webhook integration test (#1984)

--- apps/stripe/src/__tests__/integration/env.ts
+++ apps/stripe/src/__tests__/integration/env.ts
@@ -7,11 +7,13 @@ export const env = createEnv({
     INTEGRATION_SALEOR_API_URL: z.string().url().endsWith("/graphql/"),
     INTEGRATION_STRIPE_RK: z.string().min(1),
     INTEGRATION_STRIPE_PK: z.string().min(1),
+    AWS_ENDPOINT_URL: z.string().url(),
   },
   runtimeEnv: {
     DYNAMODB_MAIN_TABLE_NAME: process.env.DYNAMODB_MAIN_TABLE_NAME,
     INTEGRATION_SALEOR_API_URL: process.env.INTEGRATION_SALEOR_API_URL,
     INTEGRATION_STRIPE_RK: process.env.INTEGRATION_STRIPE_RK,
     INTEGRATION_STRIPE_PK: process.env.INTEGRATION_STRIPE_PK,
+    AWS_ENDPOINT_URL: process.env.AWS_ENDPOINT_URL,
   },
 });
