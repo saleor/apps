7fa740c501e45bb82e68d9a01411ec03b262057f
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-06-26 14:43:46 +0200

    wip

--- apps/products-feed/scripts/init-local-s3.sh
+++ apps/products-feed/scripts/init-local-s3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 awslocal s3api \
 create-bucket --bucket product-feed \
---create-bucket-configuration LocationConstraint=eu-west-2 \
---region eu-west-2
+--create-bucket-configuration LocationConstraint=us-east-2 \
+--region us-east-2 \
+--acl public-read
