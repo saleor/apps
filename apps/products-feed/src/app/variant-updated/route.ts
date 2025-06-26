31b2dd62c678518a81589d763c1f95f38f0245c2
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-06-26 12:03:50 +0200

    mock webhook logic

--- apps/products-feed/src/app/variant-updated/route.ts
+++ apps/products-feed/src/app/variant-updated/route.ts
@@ -1,6 +1,30 @@
 import { variantUpdatedWebhookManifest } from "./webhook-manifest";
 
-export const POST = variantUpdatedWebhookManifest.createHandler((req, ctx) => {
+interface IDynamoDbRepo {
+  setDirtyVariant(variantId: string): Promise<void>;
+}
+
+//todo move somewhere
+class DynamoDbRepo implements IDynamoDbRepo {
+  setDirtyVariant(variantId: string): Promise<void> {
+    // todo implement
+    throw new Error("Method not implemented.");
+  }
+}
+
+const repo = new DynamoDbRepo();
+
+export const POST = variantUpdatedWebhookManifest.createHandler(async (req, ctx) => {
+  const productVariantId = ctx.payload.productVariant?.id;
+
+  if (!productVariantId) {
+    return new Response("Variant ID not found", {
+      status: 400,
+    });
+  }
+
+  await repo.setDirtyVariant(productVariantId);
+
   /**
    *  1. Get product from subscription
    *  2. Save its id in dynamoDB and mark as dirty
