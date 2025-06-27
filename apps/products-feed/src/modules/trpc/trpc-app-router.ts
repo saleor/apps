a11016aee86b5af9e9c55880a50292b9f1a1021f
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-06-27 13:39:59 +0200

    create initial xml

--- apps/products-feed/src/modules/trpc/trpc-app-router.ts
+++ apps/products-feed/src/modules/trpc/trpc-app-router.ts
@@ -1,12 +1,14 @@
 import { appConfigurationRouter } from "../app-configuration/app-configuration.router";
 import { channelsRouter } from "../app-configuration/channels/channels.router";
 import { categoryMappingRouter } from "../category-mapping/category-mapping.router";
+import { feedRouter } from "../google-feed/feed-router";
 import { router } from "./trpc-server";
 
 export const appRouter = router({
   channels: channelsRouter,
   appConfiguration: appConfigurationRouter,
   categoryMapping: categoryMappingRouter,
+  feed: feedRouter,
 });
 
 export type AppRouter = typeof appRouter;
