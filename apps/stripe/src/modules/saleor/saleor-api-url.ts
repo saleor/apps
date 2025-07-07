aa15b89c85ade12dc18c75be004bfee5c06c80c8
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-07-07 11:31:10 +0200

    wip

--- apps/stripe/src/modules/saleor/saleor-api-url.ts
+++ apps/stripe/src/modules/saleor/saleor-api-url.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 
 import { BaseError } from "@/lib/errors";
 
+/**
+ * @deprecated use @saleor/domain
+ */
 export const SaleorApiUrlValidationError = BaseError.subclass("SaleorApiUrlValidationError", {
   props: {
     _internalName: "SaleorApiUrl.ValidationError" as const,
@@ -18,9 +21,15 @@ const saleorApiUrlSchema = z
   })
   .brand("SaleorApiUrl");
 
+/**
+ * @deprecated use @saleor/domain
+ */
 export const createSaleorApiUrl = (raw: string) =>
   fromThrowable(saleorApiUrlSchema.parse, (error) => SaleorApiUrlValidationError.normalize(error))(
     raw,
   );
 
+/**
+ * @deprecated use @saleor/domain
+ */
 export type SaleorApiUrl = z.infer<typeof saleorApiUrlSchema>;
