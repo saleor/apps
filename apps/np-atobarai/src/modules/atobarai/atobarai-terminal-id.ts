ff0d31edc282296f409a75a7b7c7e13ad0d1f7a4
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-07-09 09:07:43 +0200

    Add docs & use catalog for neverthrow (#1998)
    * Add docs & use catalog for neverthrow
    
    * Add missing env vars to turbo configuration

--- apps/np-atobarai/src/modules/atobarai/atobarai-terminal-id.ts
+++ apps/np-atobarai/src/modules/atobarai/atobarai-terminal-id.ts
@@ -4,4 +4,7 @@ const schema = z.string().min(1).brand("AtobaraiTerminalId");
 
 export const createAtobaraiTerminalId = (raw: string) => schema.parse(raw);
 
+/**
+ * Id used in the NP Atobarai to uniquely identify the merchant system.
+ */
 export type AtobaraiTerminalId = z.infer<typeof schema>;
