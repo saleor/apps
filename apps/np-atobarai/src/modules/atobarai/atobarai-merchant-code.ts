3245a8b0218f8bca05814d15aeef597308555656
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-07-09 09:07:43 +0200

    Add docs & use catalog for neverthrow (#1998)
    * Add docs & use catalog for neverthrow
    
    * Add missing env vars to turbo configuration

--- apps/np-atobarai/src/modules/atobarai/atobarai-merchant-code.ts
+++ apps/np-atobarai/src/modules/atobarai/atobarai-merchant-code.ts
@@ -4,4 +4,7 @@ const schema = z.string().min(1).brand("AtobaraiMerchantCode");
 
 export const createAtobaraiMerchantCode = (raw: string) => schema.parse(raw);
 
+/**
+ * Code used in the NP Atobarai to uniquely identify the merchant. Used in conjunction with the merchant code as basic authentication (as login). See `AtobaraiApiClient` for more details.
+ */
 export type AtobaraiMerchantCode = z.infer<typeof schema>;
