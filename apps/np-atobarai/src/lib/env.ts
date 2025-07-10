2a57cbd63d38f93c35c03a20fd09d6d88c7da4ea
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-07-10 14:21:52 +0200

    dynamodb setup

--- apps/np-atobarai/src/lib/env.ts
+++ apps/np-atobarai/src/lib/env.ts
@@ -30,6 +30,7 @@ export const env = createEnv({
     VERCEL_ENV: z.string().optional(),
     VERCEL_GIT_COMMIT_SHA: z.string().optional(),
     APP_NAME: z.string().optional().default("NP Atobarai"),
+    DYNAMODB_MAIN_TABLE_NAME: z.string().optional().default("atobarai-main-table"),
   },
   shared: {
     NODE_ENV: z.enum(["development", "production", "test"]).optional().default("development"),
@@ -55,6 +56,7 @@ export const env = createEnv({
     VERCEL_ENV: process.env.VERCEL_ENV,
     VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA,
     APP_NAME: process.env.APP_NAME,
+    DYNAMODB_MAIN_TABLE_NAME: process.env.DYNAMODB_MAIN_TABLE_NAME,
   },
   isServer: typeof window === "undefined" || process.env.NODE_ENV === "test",
   onValidationError(issues) {
