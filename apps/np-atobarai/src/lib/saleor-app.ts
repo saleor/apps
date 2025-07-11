6d51ae70c7ad14a26ae4641867a9d17d372ee29e
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-07-11 08:01:18 +0200

    dynamodb setup and apl (#2004)
    * dynamodb setup
    
    * implement dynamo apl
    
    * test
    
    * Update apps/np-atobarai/src/modules/dynamodb/dynamodb-main-table.ts
    
    Co-authored-by: Jonatan Witoszek <jonatanwitoszek@gmail.com>
    
    ---------
    
    Co-authored-by: Jonatan Witoszek <jonatanwitoszek@gmail.com>

--- apps/np-atobarai/src/lib/saleor-app.ts
+++ apps/np-atobarai/src/lib/saleor-app.ts
@@ -1,12 +1,32 @@
+import { DynamoAPL } from "@saleor/apl-dynamo";
 import { APL } from "@saleor/app-sdk/APL";
 import { FileAPL } from "@saleor/app-sdk/APL/file";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
+
+import { createLogger } from "@/lib/logger";
+import { appInternalTracer } from "@/lib/tracing";
+import { dynamoMainTable } from "@/modules/dynamodb/dynamodb-main-table";
 
 import { env } from "./env";
 
 export let apl: APL;
 switch (env.APL) {
-  // TODO: Implement DynamoDB APL
+  case "dynamodb": {
+    apl = DynamoAPL.create({
+      table: dynamoMainTable,
+      tracer: appInternalTracer,
+      logger: createLogger("DynamoAPL"),
+      env: {
+        APL_TABLE_NAME: env.DYNAMODB_MAIN_TABLE_NAME,
+        AWS_ACCESS_KEY_ID: env.AWS_ACCESS_KEY_ID,
+        AWS_REGION: env.AWS_REGION,
+        AWS_SECRET_ACCESS_KEY: env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+
+    break;
+  }
+
   default: {
     apl = new FileAPL();
     break;
