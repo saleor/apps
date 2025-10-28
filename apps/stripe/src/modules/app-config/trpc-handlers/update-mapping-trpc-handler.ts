a8371d6d0c10f51435d1e7f7eca0511f2ee7c2a0
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-10-28 16:17:18 +0100

    Add logging to update-mapping-trpc-handler

--- apps/stripe/src/modules/app-config/trpc-handlers/update-mapping-trpc-handler.ts
+++ apps/stripe/src/modules/app-config/trpc-handlers/update-mapping-trpc-handler.ts
@@ -3,11 +3,14 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { BaseError } from "@/lib/errors";
+import { createLogger } from "@/lib/logger";
 import { createSaleorApiUrl } from "@/modules/saleor/saleor-api-url";
 import { protectedClientProcedure } from "@/modules/trpc/protected-client-procedure";
 
 export class UpdateMappingTrpcHandler {
   baseProcedure = protectedClientProcedure;
+
+  private logger = createLogger("UpdateMappingTrpcHandler");
 
   getTrpcProcedure() {
     return this.baseProcedure
@@ -24,6 +27,10 @@ export class UpdateMappingTrpcHandler {
          * TODO: Extract such logic to be shared between handlers
          */
         if (saleorApiUrl.isErr()) {
+          this.logger.warn("Malformed Saleor API URL in TRPC request", {
+            error: saleorApiUrl.error,
+          });
+
           throw new TRPCError({
             code: "BAD_REQUEST",
             message: "Malformed request",
@@ -32,6 +39,8 @@ export class UpdateMappingTrpcHandler {
 
         if (!ctx.appUrl) {
           captureException(new BaseError("Missing appUrl in TRPC request"));
+
+          this.logger.error("Missing appUrl in TRPC request");
 
           throw new TRPCError({
             code: "INTERNAL_SERVER_ERROR",
@@ -51,6 +60,8 @@ export class UpdateMappingTrpcHandler {
         );
 
         if (saveResult.isErr()) {
+          this.logger.error("Failed to update mapping", { error: saveResult.error });
+
           // TODO Handle exact errors
           throw new TRPCError({
             code: "INTERNAL_SERVER_ERROR",
