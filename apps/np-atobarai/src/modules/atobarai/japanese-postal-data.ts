a69742e60a94313367591f717c8b22d73c096575
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-12-08 15:53:55 +0100

    extra formatting

--- apps/np-atobarai/src/modules/atobarai/japanese-postal-data.ts
+++ apps/np-atobarai/src/modules/atobarai/japanese-postal-data.ts
@@ -3,7 +3,7 @@ import { Oaza } from "jp-zipcode-lookup";
 export class JapanesePostalData {
   getNeighbourhood(postalCode: string) {
     // Lib accepts postal code without dash, so clean it first
-    const formattedCode = postalCode.replace("-", "");
+    const formattedCode = postalCode.replaceAll("-", "").trim();
 
     const hoods = Oaza.byZipcode(formattedCode);
 
