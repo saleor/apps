035887e0d49dcaa13014ab0b6d075250bfaa0da9
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-01-15 13:15:59 +0100

    fix migration script

--- apps/np-atobarai/src/lib/saleor-app.ts
+++ apps/np-atobarai/src/lib/saleor-app.ts
@@ -2,7 +2,7 @@ import { APL } from "@saleor/app-sdk/APL";
 import { DynamoAPL } from "@saleor/app-sdk/APL/dynamodb";
 import { FileAPL } from "@saleor/app-sdk/APL/file";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
-import { captureException } from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs";
 
 import { createLogger } from "@/lib/logger";
 import { dynamoMainTable } from "@/modules/dynamodb/dynamodb-main-table";
@@ -19,7 +19,7 @@ switch (env.APL) {
 
         if (level === "error") {
           logger.error(message);
-          captureException(message);
+          Sentry.captureException(message);
         } else {
           logger.debug(message);
         }
