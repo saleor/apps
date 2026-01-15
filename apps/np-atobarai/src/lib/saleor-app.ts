7b01ab93b4872edfefea87f5b278dbebac99f1f5
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-01-15 13:05:00 +0100

    extra log for apl

--- apps/np-atobarai/src/lib/saleor-app.ts
+++ apps/np-atobarai/src/lib/saleor-app.ts
@@ -2,7 +2,9 @@ import { APL } from "@saleor/app-sdk/APL";
 import { DynamoAPL } from "@saleor/app-sdk/APL/dynamodb";
 import { FileAPL } from "@saleor/app-sdk/APL/file";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
+import { captureException } from "@sentry/nextjs";
 
+import { createLogger } from "@/lib/logger";
 import { dynamoMainTable } from "@/modules/dynamodb/dynamodb-main-table";
 
 import { env } from "./env";
@@ -12,6 +14,16 @@ switch (env.APL) {
   case "dynamodb": {
     apl = DynamoAPL.create({
       table: dynamoMainTable,
+      externalLogger(message, level) {
+        const logger = createLogger("DynamoAPL");
+
+        if (level === "error") {
+          logger.error(message);
+          captureException(message);
+        } else {
+          logger.debug(message);
+        }
+      },
     });
 
     break;
