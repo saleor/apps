5d0e1ce4462d76e11545f858fccf6e9e8cdd9434
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-01-15 13:19:01 +0100

    remove sentry in APL logger

--- apps/np-atobarai/src/lib/saleor-app.ts
+++ apps/np-atobarai/src/lib/saleor-app.ts
@@ -2,7 +2,6 @@ import { APL } from "@saleor/app-sdk/APL";
 import { DynamoAPL } from "@saleor/app-sdk/APL/dynamodb";
 import { FileAPL } from "@saleor/app-sdk/APL/file";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
-import * as Sentry from "@sentry/nextjs";
 
 import { createLogger } from "@/lib/logger";
 import { dynamoMainTable } from "@/modules/dynamodb/dynamodb-main-table";
@@ -19,7 +18,6 @@ switch (env.APL) {
 
         if (level === "error") {
           logger.error(message);
-          Sentry.captureException(message);
         } else {
           logger.debug(message);
         }
