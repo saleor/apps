6eb71d91bcb27b5a5f92e26c083c64483f450866
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-01-26 10:29:31 +0100

    remove cloud apl (#2204)

--- apps/products-feed/src/saleor-app.ts
+++ apps/products-feed/src/saleor-app.ts
@@ -1,7 +1,6 @@
 import { APL } from "@saleor/app-sdk/APL";
 import { DynamoAPL } from "@saleor/app-sdk/APL/dynamodb";
 import { FileAPL } from "@saleor/app-sdk/APL/file";
-import { SaleorCloudAPL } from "@saleor/app-sdk/APL/saleor-cloud";
 import { UpstashAPL } from "@saleor/app-sdk/APL/upstash";
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
 
@@ -56,19 +55,6 @@ switch (aplType) {
     apl = new FileAPL();
 
     break;
-
-  case "saleor-cloud": {
-    if (!process.env.REST_APL_ENDPOINT || !process.env.REST_APL_TOKEN) {
-      throw new Error("Rest APL is not configured - missing env variables. Check saleor-app.ts");
-    }
-
-    apl = new SaleorCloudAPL({
-      resourceUrl: process.env.REST_APL_ENDPOINT,
-      token: process.env.REST_APL_TOKEN,
-    });
-
-    break;
-  }
 
   default: {
     throw new Error("Invalid APL config, ");
