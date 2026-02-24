4f64ff1e721c5132fd58b3b788bf8170842c235f
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-02-24 08:45:22 +0100

    add app-problems package

---
+++ .changeset/rare-worms-retire.md
@@ -0,0 +1,5 @@
+---
+"@saleor/app-problems": major
+---
+
+Added `app-problems` package which contains AppProblemsReporterError class, responsible of `appProblemCreate` and `appProblemDismiss` Saleor mutations
---
+++ packages/app-problems/src/app-problems-reporter.ts
@@ -0,0 +1,59 @@
+import { BaseError } from "@saleor/errors";
+import { err, ok, type Result } from "neverthrow";
+import { type Client } from "urql";
+
+import {
+  AppProblemCreateDocument,
+  type AppProblemCreateInput,
+  AppProblemDismissByKeyDocument,
+} from "../generated/graphql";
+
+export const AppProblemsReporterError = BaseError.subclass("AppProblemsReporterError", {
+  props: {
+    _brand: "AppProblemsReporterError" as const,
+  },
+});
+
+export class AppProblemsReporter {
+  constructor(private client: Client) {}
+
+  async reportProblem(
+    input: AppProblemCreateInput,
+  ): Promise<Result<void, InstanceType<typeof AppProblemsReporterError>>> {
+    const { data, error } = await this.client
+      .mutation(AppProblemCreateDocument, { input })
+      .toPromise();
+
+    if (error) {
+      return err(new AppProblemsReporterError(error.message, { cause: error }));
+    }
+
+    if (data?.appProblemCreate?.errors.length) {
+      return err(
+        new AppProblemsReporterError(data.appProblemCreate.errors[0].message ?? "Unknown error"),
+      );
+    }
+
+    return ok(undefined);
+  }
+
+  async clearProblems(
+    keys: string[],
+  ): Promise<Result<void, InstanceType<typeof AppProblemsReporterError>>> {
+    const { data, error } = await this.client
+      .mutation(AppProblemDismissByKeyDocument, { keys })
+      .toPromise();
+
+    if (error) {
+      return err(new AppProblemsReporterError(error.message, { cause: error }));
+    }
+
+    if (data?.appProblemDismiss?.errors.length) {
+      return err(
+        new AppProblemsReporterError(data.appProblemDismiss.errors[0].message ?? "Unknown error"),
+      );
+    }
+
+    return ok(undefined);
+  }
+}
