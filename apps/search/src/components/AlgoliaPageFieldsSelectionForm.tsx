6da4241003376105b78e3b5e69d75190a2b0502e
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-03-24 13:37:09 +0100

    memo

--- apps/search/src/components/AlgoliaPageFieldsSelectionForm.tsx
+++ apps/search/src/components/AlgoliaPageFieldsSelectionForm.tsx
@@ -1,7 +1,7 @@
 import { useDashboardNotification } from "@saleor/apps-shared/use-dashboard-notification";
 import { ButtonsBox, Layout } from "@saleor/apps-ui";
 import { Box, Button, Checkbox, Skeleton } from "@saleor/macaw-ui";
-import { useEffect } from "react";
+import { useMemo } from "react";
 import { Controller, useForm } from "react-hook-form";
 
 import {
@@ -11,40 +11,31 @@ import {
 } from "../lib/algolia-fields";
 import { trpcClient } from "../modules/trpc/trpc-client";
 
-export const AlgoliaPageFieldsSelectionForm = () => {
+interface AlgoliaPageFieldsSelectionFormInnerProps {
+  defaultValues: Record<AlgoliaPageFields, boolean>;
+}
+
+const AlgoliaPageFieldsSelectionFormInner = ({
+  defaultValues,
+}: AlgoliaPageFieldsSelectionFormInnerProps) => {
   const { notifySuccess } = useDashboardNotification();
 
-  const { setValue, control, handleSubmit } = useForm<Record<AlgoliaPageFields, boolean>>({});
+  const { control, handleSubmit } = useForm<Record<AlgoliaPageFields, boolean>>({
+    defaultValues,
+  });
 
-  const { data: config, isLoading } = trpcClient.configuration.getConfig.useQuery();
   const { mutate } = trpcClient.configuration.setPageFieldsMappingConfig.useMutation({
     onSuccess() {
       notifySuccess("Success", "Algolia will be updated only with selected page fields");
     },
   });
 
-  useEffect(() => {
-    if (config) {
-      const pageFields = config.pageFieldsMapping?.enabledAlgoliaFields ?? [
-        ...AlgoliaPageFieldsKeys,
-      ];
-
-      pageFields.forEach((field) => {
-        setValue(field as AlgoliaPageFields, true);
-      });
-    }
-  }, [config, setValue]);
-
-  if (isLoading || !config) {
-    return <Skeleton height={5} />;
-  }
-
   return (
     <Layout.AppSectionCard
       as="form"
       onSubmit={handleSubmit((values) => {
         const selectedValues = Object.entries(values)
-          .filter(([key, selected]) => selected)
+          .filter(([, selected]) => selected)
           .map(([key]) => key);
 
         mutate({
@@ -63,23 +54,45 @@ export const AlgoliaPageFieldsSelectionForm = () => {
             <Controller
               name={field}
               control={control}
-              render={({ field: { value, onChange } }) => {
-                return (
-                  <Checkbox
-                    onCheckedChange={(v) => {
-                      onChange(v);
-                    }}
-                    checked={value}
-                    name={field}
-                  >
-                    {AlgoliaPageFieldsLabelsMap[field]}
-                  </Checkbox>
-                );
-              }}
+              render={({ field: { value, onChange } }) => (
+                <Checkbox
+                  onCheckedChange={(v) => {
+                    onChange(v);
+                  }}
+                  checked={value}
+                  name={field}
+                >
+                  {AlgoliaPageFieldsLabelsMap[field]}
+                </Checkbox>
+              )}
             />
           </Box>
         ))}
       </Box>
     </Layout.AppSectionCard>
   );
+};
+
+export const AlgoliaPageFieldsSelectionForm = () => {
+  const { data: config, isLoading } = trpcClient.configuration.getConfig.useQuery();
+
+  const defaultValues = useMemo(() => {
+    if (!config) {
+      return null;
+    }
+
+    const enabledFields = config.pageFieldsMapping?.enabledAlgoliaFields ?? [
+      ...AlgoliaPageFieldsKeys,
+    ];
+
+    return Object.fromEntries(
+      AlgoliaPageFieldsKeys.map((field) => [field, enabledFields.includes(field)]),
+    ) as Record<AlgoliaPageFields, boolean>;
+  }, [config]);
+
+  if (isLoading || !defaultValues) {
+    return <Skeleton height={5} />;
+  }
+
+  return <AlgoliaPageFieldsSelectionFormInner defaultValues={defaultValues} />;
 };
