17cfde7b919ad6244b5b0d83a7e46fa3e9e75daa
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2026-04-10 14:48:27 +0200

    update all apps to use esm dynamic import instead of require

--- apps/klaviyo/src/logger.ts
+++ apps/klaviyo/src/logger.ts
@@ -1,32 +1,25 @@
 import { attachLoggerConsoleTransport, rootLogger } from "@saleor/apps-logger";
-import { createRequire } from "module";
 
 import packageJson from "../package.json";
 import { env } from "./env";
 
 rootLogger.settings.maskValuesOfKeys = ["metadata", "username", "password", "apiKey"];
 
-const require = createRequire(import.meta.url);
-
 if (env.NODE_ENV !== "production") {
   attachLoggerConsoleTransport(rootLogger);
 }
 
 if (typeof window === "undefined") {
-  // Don't remove require - it's necessary for proper logger initialization
-  const {
-    attachLoggerSentryTransport,
-    attachLoggerVercelRuntimeTransport,
-  } = require("@saleor/apps-logger/node");
+  const { attachLoggerSentryTransport, attachLoggerVercelRuntimeTransport } = await import(
+    "@saleor/apps-logger/node"
+  );
 
   attachLoggerSentryTransport(rootLogger);
 
   if (env.NODE_ENV === "production") {
-    attachLoggerVercelRuntimeTransport(
-      rootLogger,
-      packageJson.version,
-      require("./logger-context").loggerContext,
-    );
+    const { loggerContext } = await import("./logger-context");
+
+    attachLoggerVercelRuntimeTransport(rootLogger, packageJson.version, loggerContext);
   }
 }
 
