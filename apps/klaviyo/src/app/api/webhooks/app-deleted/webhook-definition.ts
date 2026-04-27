0923576975f13c46f7d040243821b240acfafbab
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-04-27 21:27:28 +0200

    Fix types

---
+++ apps/klaviyo/src/app/api/webhooks/app-deleted/webhook-definition.ts
@@ -0,0 +1,12 @@
+import { createAppDeletedHandler } from "@saleor/webhook-utils/app-deleted-handler";
+
+import { saleorApp } from "../../../../../saleor-app";
+import { createLogger } from "../../../../logger";
+
+const { handler, getWebhookManifest } = createAppDeletedHandler({
+  apl: saleorApp.apl,
+  logger: createLogger("APP_DELETED handler"),
+  webhookPath: "api/webhooks/app-deleted",
+});
+
+export { getWebhookManifest, handler };
